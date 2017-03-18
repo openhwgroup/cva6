d33b3cffd757815bab19586a4e3faeb81659bec4
Florian Zaruba <fl.zaruba@be4web.net>
2017-03-18 23:28:19 +0100

    Alu: Cleanup and first 64-bit version

--- alu.sv
+++ alu.sv
@@ -8,92 +8,75 @@
 // CONDITIONS OF ANY KIND, either express or implied. See the License for the
 // specific language governing permissions and limitations under the License.
 
-////////////////////////////////////////////////////////////////////////////////
-// Engineer:       Matthias Baer - baermatt@student.ethz.ch                   //
-//                                                                            //
-// Additional contributions by:                                               //
-//                 Igor Loi - igor.loi@unibo.it                               //
-//                 Andreas Traber - atraber@student.ethz.ch                   //
-//                 Lukas Mueller - lukasmue@student.ethz.ch                   //
-//                                                                            //
-// Design Name:    ALU                                                        //
-// Project Name:   RI5CY                                                      //
-// Language:       SystemVerilog                                              //
-//                                                                            //
-// Description:    Minimal version of the ALU to be used with shared DSP      //
-//                                                                            //
-////////////////////////////////////////////////////////////////////////////////
+//------------------------------------------------------------------------
+// Engineer:       Matthias Baer - baermatt@student.ethz.ch
+//
+// Additional contributions by:
+//                 Igor Loi - igor.loi@unibo.it
+//                 Andreas Traber - atraber@student.ethz.ch
+//                 Lukas Mueller - lukasmue@student.ethz.ch
+//                 Florian Zaruba - zaruabf@ethz.ch
+//
+// Design Name:    ALU
+// Project Name:   RI5CY
+// Language:       SystemVerilog
+//
+// Description:    Minimal version of the ALU, 64-bit compatible
+//------------------------------------------------------------------------
+
 
 import ariane_pkg::*;
 
 module alu
 (
-  input  logic                     clk,
-  input  logic                     rst_n,
+  input  logic          clk,
+  input  logic          rst_n,
 
-  input  alu_op                    operator_i,
-  input  logic [31:0]              operand_a_i,
-  input  logic [31:0]              operand_b_i,
-  input  logic [31:0]              operand_c_i,
+  input  alu_op         operator_i,
+  input  logic [63:0]   operand_a_i,
+  input  logic [63:0]   operand_b_i,
+  input  logic [63:0]   operand_c_i,
 
-  input  vec_mode                  vector_mode_i,
-  input  logic [ 4:0]              bmask_a_i,
-  input  logic [ 4:0]              bmask_b_i,
-  input  logic [ 1:0]              imm_vec_ext_i,
+  output logic [63:0]   result_o,
+  output logic          comparison_result_o,
 
-  output logic [31:0]              result_o,
-  output logic                     comparison_result_o,
-
-  output logic                     ready_o,
-  input  logic                     ex_ready_i
+  output logic          ready_o,
+  input  logic          ex_ready_i
 );
 
 
-  logic [31:0] operand_a_rev;
-  logic [31:0] operand_a_neg;
-  logic [31:0] operand_a_neg_rev;
+  logic [63:0] operand_a_rev;
+  logic [63:0] operand_a_neg;
+  logic [63:0] operand_a_neg_rev;
 
   assign operand_a_neg = ~operand_a_i;
 
   // bit reverse operand_a for left shifts and bit counting
   generate
     genvar k;
-    for(k = 0; k < 32; k++)
-    begin
-      assign operand_a_rev[k] = operand_a_i[31-k];
-    end
+    for(k = 0; k < 64; k++)
+      assign operand_a_rev[k] = operand_a_i[63-k];
   endgenerate
 
   // bit reverse operand_a_neg for left shifts and bit counting
   generate
     genvar m;
-    for(m = 0; m < 32; m++)
-    begin
-      assign operand_a_neg_rev[m] = operand_a_neg[31-m];
-    end
+    for(m = 0; m < 64; m++)
+      assign operand_a_neg_rev[m] = operand_a_neg[63-m];
   endgenerate
 
-  logic [31:0] operand_b_neg;
+  logic [63:0] operand_b_neg;
 
   assign operand_b_neg = ~operand_b_i;
 
+  logic [63:0] bmask;
 
-  logic [31:0] bmask;
-
-  //////////////////////////////////////////////////////////////////////////////////////////
-  //   ____            _   _ _   _                      _      _       _     _            //
-  //  |  _ \ __ _ _ __| |_(_) |_(_) ___  _ __   ___  __| |    / \   __| | __| | ___ _ __  //
-  //  | |_) / _` | '__| __| | __| |/ _ \| '_ \ / _ \/ _` |   / _ \ / _` |/ _` |/ _ \ '__| //
-  //  |  __/ (_| | |  | |_| | |_| | (_) | | | |  __/ (_| |  / ___ \ (_| | (_| |  __/ |    //
-  //  |_|   \__,_|_|   \__|_|\__|_|\___/|_| |_|\___|\__,_| /_/   \_\__,_|\__,_|\___|_|    //
-  //                                                                                      //
-  //////////////////////////////////////////////////////////////////////////////////////////
-
+  //--------------------
+  // Partitioned added
+  //--------------------
   logic        adder_op_b_negate;
-  logic [31:0] adder_op_a, adder_op_b;
-  logic [35:0] adder_in_a, adder_in_b;
-  logic [31:0] adder_result;
-  logic [35:0] adder_result_expanded;
+  logic [63:0] adder_op_a, adder_op_b;
+  logic [63:0] adder_result;
 
   assign adder_op_b_negate = (operator_i == sub) || (operator_i == subr) ||
                              (operator_i == subu) || (operator_i == subr);
@@ -104,33 +87,27 @@ module alu
   // prepare operand b
   assign adder_op_b = adder_op_b_negate ? operand_b_neg : operand_b_i;
 
-  assign adder_result = adder_op_a + adder_op_b + { 31'b0 , adder_op_b_negate };
+  assign adder_result = adder_op_a + adder_op_b + { 63'b0 , adder_op_b_negate };
 
-  ////////////////////////////////////////
-  //  ____  _   _ ___ _____ _____       //
-  // / ___|| | | |_ _|  ___|_   _|      //
-  // \___ \| |_| || || |_    | |        //
-  //  ___) |  _  || ||  _|   | |        //
-  // |____/|_| |_|___|_|     |_|        //
-  //                                    //
-  ////////////////////////////////////////
-
-  logic        shift_left;         // should we shift left
+  //----------
+  // Shifts
+  //----------
+  logic        shift_left;                                    // should we shift left
   logic        shift_arithmetic;
 
-  logic [31:0] shift_amt_left;                                // amount of shift, if to the left
-  logic [31:0] shift_amt;                                     // amount of shift, to the right
-  logic [31:0] shift_amt_int;                                 // amount of shift, used for the actual shifters
-  logic [31:0] shift_op_a;                                    // input of the shifter
-  logic [32:0] shift_op_a_ext, shift_right_sign_extended;     // sign extension
-  logic [31:0] shift_result;
-  logic [31:0] shift_right_result;
-  logic [31:0] shift_left_result;
+  logic [5:0]  shift_amt_left;                                // amount of shift, if to the left
+  logic [5:0]  shift_amt;                                     // amount of shift, to the right
+  logic [5:0]  shift_amt_int;                                 // amount of shift, used for the actual shifters
+  logic [63:0] shift_op_a;                                    // input of the shifter
+  logic [64:0] shift_op_a_ext, shift_right_sign_extended;     // sign extension
+  logic [63:0] shift_result;
+  logic [63:0] shift_right_result;
+  logic [63:0] shift_left_result;
 
-  assign shift_amt = operand_b_i;
+  assign shift_amt = operand_b_i[5:0];
 
   // by reversing the bits of the input, we also have to reverse the order of shift amounts
-  assign shift_amt_left[31: 0] = shift_amt[31: 0];
+  assign shift_amt_left[5:0] = shift_amt[5:0];
 
   // ALU_FL1 and ALU_CBL are used for the bit counting ops later
   assign shift_left = (operator_i == sll);
@@ -141,39 +118,29 @@ module alu
   assign shift_op_a    = shift_left ? operand_a_rev : operand_a_i;
   assign shift_amt_int = shift_left ? shift_amt_left : shift_amt;
 
-  assign shift_op_a_ext = shift_arithmetic ? {shift_op_a[31], shift_op_a} : {1'b0, shift_op_a};
+  assign shift_op_a_ext = shift_arithmetic ? {shift_op_a[63], shift_op_a} : {1'b0, shift_op_a};
 
   assign shift_right_sign_extended = $signed(shift_op_a_ext) >>> shift_amt_int;
-  assign shift_right_result = shift_right_sign_extended[31:0];
+  assign shift_right_result = shift_right_sign_extended[63:0];
 
   // bit reverse the shift_right_result for left shifts
-  genvar       j;
+  genvar j;
   generate
-    for(j = 0; j < 32; j++)
+    for(j = 0; j < 64; j++)
     begin
-      assign shift_left_result[j] = shift_right_result[31-j];
+      assign shift_left_result[j] = shift_right_result[63-j];
     end
   endgenerate
 
   assign shift_result = shift_left ? shift_left_result : shift_right_result;
 
-
-  //////////////////////////////////////////////////////////////////
-  //   ____ ___  __  __ ____   _    ____  ___ ____   ___  _   _   //
-  //  / ___/ _ \|  \/  |  _ \ / \  |  _ \|_ _/ ___| / _ \| \ | |  //
-  // | |  | | | | |\/| | |_) / _ \ | |_) || |\___ \| | | |  \| |  //
-  // | |__| |_| | |  | |  __/ ___ \|  _ < | | ___) | |_| | |\  |  //
-  //  \____\___/|_|  |_|_| /_/   \_\_| \_\___|____/ \___/|_| \_|  //
-  //                                                              //
-  //////////////////////////////////////////////////////////////////
-
+  //----------------
+  // Comparisons
+  //----------------
   logic [3:0] is_equal;
   logic [3:0] is_greater;  // handles both signed and unsigned forms
 
-  // 8-bit vector comparisons, basic building blocks
   logic [3:0] cmp_signed;
-  logic [3:0] is_equal_vec;
-  logic [3:0] is_greater_vec;
 
   always_comb
   begin
@@ -191,56 +158,10 @@ module alu
       abs,
       clip,
       clipu: begin
-        case (vector_mode_i)
-          mode8:  cmp_signed[3:0] = 4'b1111;
-          mode16: cmp_signed[3:0] = 4'b1010;
-          default:     cmp_signed[3:0] = 4'b1000;
-        endcase
+        cmp_signed[3:0] = 4'b1000;
       end
 
       default:;
-    endcase
-  end
-
-  // generate vector equal and greater than signals, cmp_signed decides if the
-  // comparison is done signed or unsigned
-  genvar i;
-  generate
-    for(i = 0; i < 4; i++)
-    begin
-      assign is_equal_vec[i]   = (operand_a_i[8*i+7:8*i] == operand_b_i[8*i+7:i*8]);
-      assign is_greater_vec[i] = $signed({operand_a_i[8*i+7] & cmp_signed[i], operand_a_i[8*i+7:8*i]})
-                                  >
-                                 $signed({operand_b_i[8*i+7] & cmp_signed[i], operand_b_i[8*i+7:i*8]});
-    end
-  endgenerate
-
-  // generate the real equal and greater than signals that take the vector
-  // mode into account
-  always_comb
-  begin
-    // 32-bit mode
-    is_equal[3:0]   = {4{is_equal_vec[3] & is_equal_vec[2] & is_equal_vec[1] & is_equal_vec[0]}};
-    is_greater[3:0] = {4{is_greater_vec[3] | (is_equal_vec[3] & (is_greater_vec[2]
-                                            | (is_equal_vec[2] & (is_greater_vec[1]
-                                             | (is_equal_vec[1] & (is_greater_vec[0]))))))}};
-
-    case(vector_mode_i)
-      mode16:
-      begin
-        is_equal[1:0]   = {2{is_equal_vec[0]   & is_equal_vec[1]}};
-        is_equal[3:2]   = {2{is_equal_vec[2]   & is_equal_vec[3]}};
-        is_greater[1:0] = {2{is_greater_vec[1] | (is_equal_vec[1] & is_greater_vec[0])}};
-        is_greater[3:2] = {2{is_greater_vec[3] | (is_equal_vec[3] & is_greater_vec[2])}};
-      end
-
-      mode8:
-      begin
-        is_equal[3:0]   = is_equal_vec[3:0];
-        is_greater[3:0] = is_greater_vec[3:0];
-      end
-
-      default:; // see default assignment
     endcase
   end
 
@@ -267,15 +188,9 @@ module alu
   end
 
   assign comparison_result_o = cmp_result[3];
-
-  ////////////////////////////////////////////////////////
-  //   ____                 _ _     __  __              //
-  //  |  _ \ ___  ___ _   _| | |_  |  \/  |_   ___  __  //
-  //  | |_) / _ \/ __| | | | | __| | |\/| | | | \ \/ /  //
-  //  |  _ <  __/\__ \ |_| | | |_  | |  | | |_| |>  <   //
-  //  |_| \_\___||___/\__,_|_|\__| |_|  |_|\__,_/_/\_\  //
-  //                                                    //
-  ////////////////////////////////////////////////////////
+  //----------------
+  // Result MUX
+  //----------------
 
   always_comb
   begin
@@ -306,7 +221,7 @@ module alu
           result_o[ 7: 0] = {8{cmp_result[0]}};
        end
       slts, sltu,
-      slets, sletu: result_o = {31'b0, comparison_result_o};
+      slets, sletu: result_o = {63'b0, comparison_result_o};
 
       default: $warning("instruction not supported in basic alu"); // default case to suppress unique warning
     endcase
