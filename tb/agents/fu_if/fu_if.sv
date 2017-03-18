51b55d1b06f3c63d0110e1c51deac97a8ff2b9f1
Florian Zaruba <fl.zaruba@be4web.net>
2017-03-18 23:59:07 +0100

    Added UVM test-bench structure for basic ALU

---
+++ tb/agents/fu_if/fu_if.sv
@@ -0,0 +1,40 @@
+// Author: Florian Zaruba, ETH Zurich
+// Date: 3/18/2017
+// Description: Generic functional unit interface (fu if)
+//              The interface can be used in Master or Slave mode.
+
+// Guard statement proposed by "Easier UVM" (doulos)
+`ifndef ALU_IF__SV
+`define ALU_IF__SV
+interface fu_if #(parameter int OPERATOR_SIZE = 8, parameter int OPERAND_SIZE = 64)(input clk);
+    wire [OPERATOR_SIZE-1:0] operator;          // FU operation
+    wire [OPERAND_SIZE-1:0]  operand_a;         // Operand A
+    wire [OPERAND_SIZE-1:0]  operand_b;         // Operand B
+    wire [OPERAND_SIZE-1:0]  operand_c;         // Operand C
+
+    wire [OPERAND_SIZE-1:0]  result;            // Result
+    wire                     comparison_result; // Comparison result
+    wire                     valid;             // Result is valid, ready to accept new request
+    wire                     ready;             // Sink is ready
+
+    // FU interface configured as master
+    clocking mck @(posedge clk);
+        input   operator, operand_a, operand_b, operand_c, ready;
+        output  result, comparison_result, valid;
+    endclocking
+    // FU interface configured as slave
+    clocking sck @(posedge clk);
+        output  operator, operand_a, operand_b, operand_c, ready;
+        input  result, comparison_result, valid;
+    endclocking
+    // FU interface configured in passive mode
+    clocking pck @(posedge clk);
+        input operator, operand_a, operand_b, operand_c, ready;
+    endclocking
+
+    modport master  (clocking mck);
+    modport slave   (clocking sck);
+    modport passive (clocking pck);
+
+endinterface
+`endif
