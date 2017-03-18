f38f31b1f37fff2d68095e9cb214cff10547a9cf
Florian Zaruba <fl.zaruba@be4web.net>
2017-03-18 21:02:00 +0100

    Added ALU operations, alu ops now enums

--- include/ariane_pkg.svh
+++ include/ariane_pkg.svh
@@ -1,4 +1,19 @@
 package ariane_pkg;
 
+// ---------------
+// ALU operations
+// ---------------
+typedef enum logic [7:0] { add, sub, addu, subu, addr, subr, addur, subug,  // basic ALU op
+                     lxor, lor, land,                                    // logic operations
+                     sra, srl, ror, sll,                              // shifts
+                     // bext, bextu, bins, bclr, bset,                // bit manipulation, currently not implemented
+                     ff1, fl1, cnt, clb,                              // bit counting
+                     exts, ext,                                       // sign-/zero-extensions
+                     lts, ltu, les, leu, gts, gtu, ges, geu, eq, ne,  // comparisons
+                     slts, sltu, slets, sletu,                        // set lower than operations
+                     abs, clip, clipu,                                // absolute value
+                     ins,                                             // insert/extract
+                     min, minu, max, maxu                             // min/max
+                   } alu_op;
 
 endpackage
