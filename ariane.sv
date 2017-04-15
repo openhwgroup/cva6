9f2c0be6afc5630a12b39dc73ab398b8b72b818b
Florian Zaruba <fl.zaruba@be4web.net>
2017-04-15 20:43:48 +0200

    Added IF and Commit stage instantiations

--- ariane.sv
+++ ariane.sv
@@ -10,8 +10,7 @@ import ariane_pkg::*;
 
 module ariane
     #(
-        parameter N_EXT_PERF_COUNTERS = 0,
-        parameter INSTR_RDATA_WIDTH   = 64
+        parameter N_EXT_PERF_COUNTERS = 0
     )
     (
         input  logic                           clk_i,
@@ -25,7 +24,7 @@ module ariane
         input  logic [N_EXT_PERF_COUNTERS-1:0] ext_perf_counters_i,
 
         // Core ID, Cluster ID and boot address are considered more or less static
-        input  logic [64:0]                    boot_addr_i,
+        input  logic [63:0]                    boot_addr_i,
         input  logic [ 3:0]                    core_id_i,
         input  logic [ 5:0]                    cluster_id_i,
         // Instruction memory interface
@@ -42,13 +41,7 @@ module ariane
         output logic                           sec_lvl_o,
 
         // Debug Interface
-        debug_if.Slave                         debug_if,
-
-        // synth stuff
-        input  logic [31:0]  instruction_i,
-        input  logic instruction_valid_i,
-        output scoreboard_entry commit_instr_o,
-        input  logic commit_ack_i
+        debug_if.Slave                         debug_if
     );
 
 
@@ -74,6 +67,51 @@ module ariane
     logic [63:0] alu_result;
     // synth stuff
     assign flush_i = 1'b0;
+
+    logic req_i;
+    logic if_busy_o;
+    logic id_ready_i;
+    logic halt_if_i;
+    logic instr_req_o;
+    logic [63:0] instr_addr_o;
+    logic instr_gnt_i;
+    logic instr_rvalid_i;
+    logic [31:0] instr_rdata_i;
+    logic instr_valid_id_o;
+    logic [31:0] instr_rdata_id_o;
+    logic is_compressed_id_o;
+    logic illegal_c_insn_id_o;
+    logic [63:0] pc_if_o;
+    logic [63:0] pc_id_o;
+    logic comparison_result_o;
+    logic lsu_ready_o;
+    logic lsu_valid_i;
+    logic mult_ready_o;
+    logic mult_valid_i;
+    priv_lvl_t priv_lvl_o;
+    exception exception_o;
+
+if_stage i_if_stage (
+    .clk_i               ( clk_i                   ),
+    .rst_ni              ( rst_ni                  ),
+    .req_i               ( req_i                   ),
+    .if_busy_o           ( if_busy_o               ),
+    .id_ready_i          ( id_ready_i              ),
+    .halt_if_i           ( halt_if_i               ),
+    .instr_req_o         ( instr_if.data_req       ),
+    .instr_addr_o        ( instr_if.address        ),
+    .instr_gnt_i         ( instr_if.data_gnt       ),
+    .instr_rvalid_i      ( instr_if.data_rvalid    ),
+    .instr_rdata_i       ( instr_if.data_rdata     ),
+    .instr_valid_id_o    ( instr_valid_id_o        ),
+    .instr_rdata_id_o    ( instr_rdata_id_o        ),
+    .is_compressed_id_o  ( is_compressed_id_o      ),
+    .illegal_c_insn_id_o ( illegal_c_insn_id_o     ),
+    .pc_if_o             ( pc_if_o                 ),
+    .pc_id_o             ( pc_id_o                 ),
+    .boot_addr_i         ( boot_addr_i             )
+);
+
     id_stage
     #(
         .NR_WB_PORTS         ( 1                   )
@@ -83,9 +121,9 @@ module ariane
         .rst_ni              ( rst_ni              ),
         .test_en_i           ( test_en_i           ),
         .flush_i             ( flush_i             ),
-        .instruction_i       ( instruction_i       ),
-        .instruction_valid_i ( instruction_valid_i ),
-        .pc_if_i             (                     ), // PC from if
+        .instruction_i       ( instr_rdata_id_o    ),
+        .instruction_valid_i ( instr_valid_id_o    ),
+        .pc_if_i             ( pc_if_o             ), // PC from if
         .ex_i                (                     ), // exception from if
         .ready_o             ( ready_o             ),
         .operator_o          ( operator_o          ),
@@ -110,32 +148,37 @@ module ariane
         .commit_ack_i        ( commit_ack_i        )
     );
 
-    logic comparison_result_o;
-    logic lsu_ready_o;
-    logic lsu_valid_i;
-    logic mult_ready_o;
-    logic mult_valid_i;
+    ex_stage ex_stage_i (
+        .clk_i               ( clk_i               ),
+        .rst_ni              ( rst_ni              ),
+        .operator_i          ( operator_o          ),
+        .operand_a_i         ( operand_a_o         ),
+        .operand_b_i         ( operand_b_o         ),
+        .trans_id_i          ( trans_id_o          ),
+        .comparison_result_o ( comparison_result_o ),
+        .alu_ready_o         ( alu_ready_i         ),
+        .alu_valid_i         ( alu_valid_i         ),
+        .alu_result_o        ( alu_result          ),
+        .alu_trans_id_o      ( alu_trans_id        ),
+        .alu_valid_o         ( alu_valid_o         ),
 
-ex_stage ex_stage_i (
-    .clk_i               ( clk_i               ),
-    .rst_ni              ( rst_ni              ),
-    .operator_i          ( operator_o          ),
-    .operand_a_i         ( operand_a_o         ),
-    .operand_b_i         ( operand_b_o         ),
-    .trans_id_i          ( trans_id_o          ),
-    .comparison_result_o ( comparison_result_o ),
-    .alu_ready_o         ( alu_ready_i         ),
-    .alu_valid_i         ( alu_valid_i         ),
-    .alu_result_o        ( alu_result          ),
-    .alu_trans_id_o      ( alu_trans_id        ),
-    .alu_valid_o         ( alu_valid_o         ),
+        .lsu_ready_o         ( lsu_ready_o         ),
+        .lsu_valid_i         ( lsu_valid_i         ),
 
-    .lsu_ready_o         ( lsu_ready_o         ),
-    .lsu_valid_i         ( lsu_valid_i         ),
+        .mult_ready_o        ( mult_ready_o        ),
+        .mult_valid_i        ( mult_valid_i        )
+    );
 
-    .mult_ready_o        ( mult_ready_o        ),
-    .mult_valid_i        ( mult_valid_i        )
-);
-
+    commit_stage i_commit_stage (
+        .clk_i           ( clk_i               ),
+        .rst_ni          ( rst_ni              ),
+        .priv_lvl_o      ( priv_lvl_o          ),
+        .exception_o     ( exception_o         ),
+        .commit_instr_i  ( commit_instr_o      ),
+        .commit_ack_o    ( commit_ack_i        ),
+        .waddr_a_o       ( waddr_a_i           ),
+        .wdata_a_o       ( wdata_a_i           ),
+        .we_a_o          ( we_a_i              )
+    );
 
 endmodule // ariane
