8e2688168651f2c9abca60fa16295ea3445dad18
Florian Zaruba <fl.zaruba@be4web.net>
2017-04-19 20:51:59 +0200

    Added PC generation stage stub

---
+++ pcgen.sv
@@ -0,0 +1,18 @@
+// Author: Florian Zaruba, ETH Zurich
+// Date: 20.04.2017
+// Description: PC generation stage
+//
+// Copyright (C) 2017 ETH Zurich, University of Bologna
+// All rights reserved.
+//
+// This code is under development and not yet released to the public.
+// Until it is released, the code is under the copyright of ETH Zurich and
+// the University of Bologna, and may contain confidential and/or unpublished
+// work. Any reuse/redistribution is strictly forbidden without written
+// permission from ETH Zurich.
+//
+// Bug fixes and contributions will eventually be released under the
+// SolderPad open hardware license in the context of the PULP platform
+// (http://www.pulp-platform.org), under the copyright of ETH Zurich and the
+// University of Bologna.
+//
