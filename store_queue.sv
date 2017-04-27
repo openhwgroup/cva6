5f69a4b2f6b6f154f7b700435478fcb543584731
Florian Zaruba <fl.zaruba@be4web.net>
2017-04-27 11:51:59 +0200

    Fixes issue #18
    Removed the redundant speculative entry as this can be
    achieved with a single bit anyway. So we only need to check a single
    entry.

--- store_queue.sv
+++ store_queue.sv
@@ -34,7 +34,6 @@ module store_queue (
     output logic         ready_o,  // the store queue is ready to accept a new request
                                    // it is only ready if it can unconditionally commit the instruction, e.g.:
                                    // the commit buffer needs to be empty
-                                   // it is easier to have an unconditional commit
     input  logic         valid_i,  // this is a valid store
     input  logic [63:0]  paddr_i,  // physical address of store which needs to be placed in the queue
     input  logic [63:0]  data_i,   // data which is placed in the queue
@@ -47,24 +46,26 @@ module store_queue (
     output logic         data_we_o,
     output logic [7:0]   data_be_o,
     input  logic         data_gnt_i,
-    input  logic         data_rvalid_i,
-    input  logic [63:0]  data_rdata_i
-);
+    input  logic         data_rvalid_i
+    );
     // the store queue has two parts:
     // 1. Speculative queue
     // 2. Commit queue which is non-speculative, e.g.: the store will definitely happen.
     // For simplicity reasons we just keep those two elements and not one real queue
     // should it turn out that this bottlenecks we can still increase the capacity here
     // potentially at the cost of increased area.
+    // In the current implementation this is represented by a single entry and
+    // differentiated by the is_speculative flag.
 
-    enum { IDLE, WAIT_RVALID } CS, NS;
+    enum logic { IDLE, WAIT_RVALID } CS, NS;
 
     struct packed {
         logic [63:0] address;
         logic [63:0] data;
         logic [7:0]  be;
-        logic        valid; // entry is valid
-    } commit_queue_n, commit_queue_q, store_queue_n, store_queue_q;
+        logic        valid;          // entry is valid
+        logic        is_speculative; // set if the entry isn't committed yet
+    } commit_queue_n, commit_queue_q;
 
     // we can directly output the commit entry since we just have one element in this "queue"
     assign paddr_o = commit_queue_q.address;
@@ -72,12 +73,19 @@ module store_queue (
     assign be_o    = commit_queue_q.be;
     assign valid_o = commit_queue_q.valid;
 
+    // those signals can directly be output to the memory if
+    assign address_o    = commit_queue_q.address;
+    assign data_wdata_o = commit_queue_q.data;
+    assign data_be_o    = commit_queue_q.be;
+
     // memory interface
     always_comb begin : store_if
-        // those signals can directly be output to the memory if
-        address_o    = commit_queue_q.address;
-        data_wdata_o = commit_queue_q.data;
-        data_be_o    = commit_queue_q.be;
+        // if there is no commit pending and the uncommitted queue is empty as well we can accept the request
+        automatic logic ready = ~commit_queue_q.valid;
+        ready_o       =  ready;
+
+        commit_queue_n = commit_queue_q;
+
         data_we_o    = 1'b1; // we will always write in the store queue
         data_req_o   = 1'b0;
         NS           = CS;
@@ -90,8 +98,7 @@ module store_queue (
                         data_req_o = 1'b1;
                         if (data_gnt_i) begin// advance to the next state if we received the grant
                             NS = WAIT_RVALID;
-                            // we can evict it from the commit buffer
-                            commit_queue_n.valid = 1'b0;
+
                         end
                     end
                 end
@@ -99,10 +106,12 @@ module store_queue (
                 WAIT_RVALID: begin
 
                     if (data_rvalid_i) begin
-                        // if we got the rvalid we can already perform a new store
-                        if (commit_queue_q.valid)
+                        // if we got the rvalid we can already perform a new store request
+                        if (commit_queue_q.valid) begin
+                            // we can evict it from the commit buffer
+                            commit_queue_n.valid = 1'b0;
                             data_req_o = 1'b1;
-                        else // if do not need to perform another
+                        end else // if do not need to perform another
                             NS = IDLE;
                     end else // wait here for the rvalid
                         NS = WAIT_RVALID;
@@ -115,43 +124,31 @@ module store_queue (
         // shift the store request from the speculative buffer
         // to the non-speculative
         if (commit_i) begin
-            commit_queue_n = store_queue_q;
+            commit_queue_n.is_speculative = 1'b0;
         end
-    end
 
-    // LSU interface
-    always_comb begin : lsu_if
-        // if there is no commit pending and the uncommitted queue is empty as well we can accept the request
-        automatic logic ready = ~commit_queue_q.valid & ~store_queue_q.valid;
-        ready_o       =  ready;
-        store_queue_n = store_queue_q;
+        // LSU interface
         // we are ready to accept a new entry and the input data is valid
         if (ready & valid_i & ~commit_i) begin
-            store_queue_n.address = paddr_i;
-            store_queue_n.data    = data_i;
-            store_queue_n.be      = be_i;
-            store_queue_n.valid   = 1'b1;
+            commit_queue_n.address        = paddr_i;
+            commit_queue_n.data           = data_i;
+            commit_queue_n.be             = be_i;
+            commit_queue_n.valid          = 1'b1;
+            commit_queue_n.is_speculative = 1'b1;
         end
-        // invalidate this result
-        // as it is moved to the non-speculative queue
-        if (~valid_i & commit_i) begin
-            store_queue_n.valid   = 1'b0;
-        end
+
     end
 
     // registers
     always_ff @(posedge clk_i or negedge rst_ni) begin : proc_
         if(~rst_ni) begin
-           commit_queue_q <= '{default: 0};
-           store_queue_q  <= '{default: 0};
-           CS             <= IDLE;
+           commit_queue_q  <= '{default: 0};
+           CS              <= IDLE;
         end else if (flush_i) begin // just empty the speculative queue
             commit_queue_q <= commit_queue_n;
-            store_queue_q  <= '{default: 0};
             CS             <= NS;
         end else begin
             commit_queue_q <= commit_queue_n;
-            store_queue_q  <= store_queue_n;
             CS             <= NS;
         end
      end
