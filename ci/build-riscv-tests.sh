ba415f21cff52d1f773ce720981217bfb990cc95
Florian Zaruba <zarubaf@iis.ee.ethz.ch>
2018-01-23 15:50:48 +0100

    Remove submodules, fix CI build

---
+++ ci/build-riscv-tests.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+set -e
+ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
+cd $ROOT/tmp
+
+[ -d $ROOT/tmp/riscv-tests ] || git clone https://github.com/riscv/riscv-tests.git
+cd riscv-tests
+git checkout ffa920340430f62e767fb2397f4ee41ffaf441ce
+git submodule update --init --recursive
+autoconf
+mkdir -p build
+cd build
+../configure --prefix=$ROOT/tmp/riscv-tests/build
+make isa -j
+make install
+
+cd isa
+# generate hex files
+for f in $(ls | grep -v '\.[dump|hex]'); do
+    # elf2hex $f
+    echo "elf2hex $f > $f.hex"
+    elf2hex 8 16384 $f 2147483648 > $f.hex
+done
