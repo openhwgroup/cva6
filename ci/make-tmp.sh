ca59a00ad283ef1df461cdd75160a0bffa519010
Florian Zaruba <zarubaf@iis.ee.ethz.ch>
2018-02-05 15:10:30 +0100

    Do not make tmp dir on tmpfs

--- ci/make-tmp.sh
+++ ci/make-tmp.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 set -e
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
-[ -d tmp ] || ln -s "$(mktemp -d -t ariane.XXXXXXXX)" tmp
+# [ -d tmp ] || ln -s "$(mktemp -d -t ariane.XXXXXXXX)" tmp
+[ -d tmp ] || mkdir -p tmp
