90ab062f9dcbd79c1cb7077abcc0bc2b2fa7cbe7
Florian Zaruba <zarubaf@iis.ee.ethz.ch>
2018-02-05 19:29:52 +0100

    Also check if bin directory exists

--- ci/build-riscv-gcc.sh
+++ ci/build-riscv-gcc.sh
@@ -3,7 +3,7 @@ set -e
 ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 cd $ROOT/tmp
 
-if ! [ -e $RISCV ]; then
+if ! [ -e $RISCV/bin ]; then
     [ -d $ROOT/tmp/riscv-gnu-toolchain ] || git clone https://github.com/riscv/riscv-gnu-toolchain.git
     cd riscv-gnu-toolchain
     git checkout 691e4e826251c7ec59f883cab18440c87baf45e7
