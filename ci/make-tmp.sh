20b03f7bf9d7c37b3511515535982bc0a0d297a0
Florian Zaruba <zarubaf@iis.ee.ethz.ch>
2018-02-05 15:19:50 +0100

    Remove tmp dir before installing

--- ci/make-tmp.sh
+++ ci/make-tmp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -e
 cd "$(dirname "${BASH_SOURCE[0]}")/.."
-# [ -d tmp ] || ln -s "$(mktemp -d -t ariane.XXXXXXXX)" tmp
-[ -d tmp ] || mkdir -p tmp
+[ -d tmp ] || rm -rf tmp
+mkdir -p tmp
