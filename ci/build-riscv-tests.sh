bd6c23294a10e0b4ddb14ce5eee0622ce8d0a112
Florian Zaruba <zarubaf@iis.ee.ethz.ch>
2018-07-25 11:38:49 -0700

    :arrow_up: Update riscv-tests dependencies

--- ci/build-riscv-tests.sh
+++ ci/build-riscv-tests.sh
@@ -5,7 +5,7 @@ cd $ROOT/tmp
 
 [ -d $ROOT/tmp/riscv-tests ] || git clone https://github.com/riscv/riscv-tests.git
 cd riscv-tests
-git checkout ffa920340430f62e767fb2397f4ee41ffaf441ce
+git checkout 294bfce8a1ca2fc501b8939292146e44f813a2b8
 git submodule update --init --recursive
 autoconf
 mkdir -p build
