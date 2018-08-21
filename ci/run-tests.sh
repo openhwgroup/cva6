8e89f6218106b1b5b1ba7f55fe356d1161c31807
Michael Schaffner <schaffner@iis.ee.ethz.ch>
2018-08-21 13:54:59 +0200

    :zap: restructure travis and gitlab-ci flow scripts and make targets
        * fix typo in signal naming and make axi_adapter questa-sim compliant

--- ci/run-tests.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -e
-ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
-# run the tests in parallel, 4 at a time
-printf "$(xargs printf '\n%s' < ${ROOT}/ci/test.list | cut -b 1-)" | xargs -n1 -P4 -I{} ${ROOT}/build/Variane_testharness tmp/riscv-tests/build/isa/{}
