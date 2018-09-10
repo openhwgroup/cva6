c1775be3981aa5bc31ead51965d064ccac2c3465
Luis Vitorio Cargnini <lcargnini@micron.com>
2018-09-10 14:14:56 -0700

    Changes to be committed: 	new file:   src/bht.sv 	new file:   src/btb.sv 	new file:   src/instr_scan.sv 	new file:   src/ras.sv

---
+++ src/btb.sv
@@ -0,0 +1,86 @@
+//Copyright (C) 2018 to present,
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 2.0 (the "License"); you may not use this file except in
+// compliance with the License.  You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-2.0. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//
+// Author: Florian Zaruba, ETH Zurich
+// Date: 08.02.2018
+// Migrated: Luis Vitorio Cargnini, IEEE
+// Date: 09.06.2018
+
+// ------------------------------
+// Branch Prediction
+// ------------------------------
+
+// branch target buffer
+module btb #(
+    parameter int NR_ENTRIES = 8
+)(
+    input  logic               clk_i,           // Clock
+    input  logic               rst_ni,          // Asynchronous reset active low
+    input  logic               flush_i,         // flush the btb
+
+    input  logic [63:0]        vpc_i,           // virtual PC from IF stage
+    input  btb_update_t        btb_update_i,    // update btb with this information
+    output btb_prediction_t    btb_prediction_o // prediction from btb
+);
+    // number of bits which are not used for indexing
+    localparam OFFSET = 1; // we are using compressed instructions so do use the lower 2 bits for prediction
+    localparam ANTIALIAS_BITS = 8;
+    // number of bits we should use for prediction
+    localparam PREDICTION_BITS = $clog2(NR_ENTRIES) + OFFSET;
+    // typedef for all branch target entries
+    // we may want to try to put a tag field that fills the rest of the PC in-order to mitigate aliasing effects
+    btb_prediction_t btb_d [NR_ENTRIES-1:0], btb_q [NR_ENTRIES-1:0];
+    logic [$clog2(NR_ENTRIES)-1:0]          index, update_pc;
+
+    assign index     = vpc_i[PREDICTION_BITS - 1:OFFSET];
+    assign update_pc = btb_update_i.pc[PREDICTION_BITS - 1:OFFSET];
+
+    // output matching prediction
+    assign btb_prediction_o = btb_q[index];
+
+    // -------------------------
+    // Update Branch Prediction
+    // -------------------------
+    // update on a mis-predict
+    always_comb begin : update_branch_predict
+        btb_d = btb_q;
+
+        if (btb_update_i.valid) begin
+            btb_d[update_pc].valid = 1'b1;
+            // the target address is simply updated
+            btb_d[update_pc].target_address = btb_update_i.target_address;
+            // as is the information whether this was a compressed branch
+            btb_d[update_pc].is_lower_16    = btb_update_i.is_lower_16;
+            // check if we should invalidate this entry, this happens in case we predicted a branch
+            // where actually none-is (aliasing)
+            if (btb_update_i.clear) begin
+                btb_d[update_pc].valid = 1'b0;
+            end
+        end
+    end
+
+    // sequential process
+    always_ff @(posedge clk_i or negedge rst_ni) begin
+        if (~rst_ni) begin
+            // Bias the branches to be taken upon first arrival
+            for (int i = 0; i < NR_ENTRIES; i++)
+                btb_q[i] <= '{default: 0};
+        end else begin
+            // evict all entries
+            if (flush_i) begin
+                for (int i = 0; i < NR_ENTRIES; i++) begin
+                    btb_q[i].valid <=  1'b0;
+                end
+            end else begin
+                btb_q <=  btb_d;
+            end
+        end
+    end
+endmodule
---
+++ src/instr_scan.sv
@@ -0,0 +1,56 @@
+//Copyright (C) 2018 to present,
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 2.0 (the "License"); you may not use this file except in
+// compliance with the License.  You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-2.0. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+// Author: Florian Zaruba, ETH Zurich
+// Date: 08.02.2018
+// Migrated: Luis Vitorio Cargnini, IEEE
+// Date: 09.06.2018
+
+// ------------------------------
+// Instruction Scanner
+// ------------------------------
+module instr_scan (
+    input  logic [31:0] instr_i,        // expect aligned instruction, compressed or not
+    output logic        is_rvc_o,
+    output logic        rvi_return_o,
+    output logic        rvi_call_o,
+    output logic        rvi_branch_o,
+    output logic        rvi_jalr_o,
+    output logic        rvi_jump_o,
+    output logic [63:0] rvi_imm_o,
+    output logic        rvc_branch_o,
+    output logic        rvc_jump_o,
+    output logic        rvc_jr_o,
+    output logic        rvc_return_o,
+    output logic        rvc_jalr_o,
+    output logic        rvc_call_o,
+    output logic [63:0] rvc_imm_o
+);
+    assign is_rvc_o     = (instr_i[1:0] != 2'b11);
+    // check that rs1 is either x1 or x5 and that rs1 is not x1 or x5, TODO: check the fact about bit 7
+    assign rvi_return_o = rvi_jalr_o & ~instr_i[7] & ~instr_i[19] & ~instr_i[18] & ~instr_i[16] & instr_i[15];
+    assign rvi_call_o   = (rvi_jalr_o | rvi_jump_o) & instr_i[7]; // TODO: check that this captures calls
+    // differentiates between JAL and BRANCH opcode, JALR comes from BHT
+    assign rvi_imm_o    = (instr_i[3]) ? uj_imm(instr_i) : sb_imm(instr_i);
+    assign rvi_branch_o = (instr_i[6:0] == riscv::OpcodeBranch) ? 1'b1 : 1'b0;
+    assign rvi_jalr_o   = (instr_i[6:0] == riscv::OpcodeJalr)   ? 1'b1 : 1'b0;
+    assign rvi_jump_o   = (instr_i[6:0] == riscv::OpcodeJal)    ? 1'b1 : 1'b0;
+    // opcode JAL
+    assign rvc_jump_o   = (instr_i[15:13] == riscv::OpcodeCJ) & is_rvc_o & (instr_i[1:0] == 2'b01);
+    assign rvc_jr_o     = (instr_i[15:12] == 4'b1000) & (instr_i[6:2] == 5'b00000) & is_rvc_o & (instr_i[1:0] == 2'b10);
+    assign rvc_branch_o = ((instr_i[15:13] == riscv::OpcodeCBeqz) | (instr_i[15:13] == riscv::OpcodeCBnez)) & is_rvc_o & (instr_i[1:0] == 2'b01);
+    // check that rs1 is x1 or x5
+    assign rvc_return_o = rvc_jr_o & ~instr_i[11] & ~instr_i[10] & ~instr_i[8] & instr_i[7];
+    assign rvc_jalr_o   = (instr_i[15:12] == 4'b1001) & (instr_i[6:2] == 5'b00000) & is_rvc_o;
+    assign rvc_call_o   = rvc_jalr_o;  // TODO: check that this captures calls
+
+    // // differentiates between JAL and BRANCH opcode, JALR comes from BHT
+    assign rvc_imm_o    = (instr_i[14]) ? {{56{instr_i[12]}}, instr_i[6:5], instr_i[2], instr_i[11:10], instr_i[4:3], 1'b0}
+                                       : {{53{instr_i[12]}}, instr_i[8], instr_i[10:9], instr_i[6], instr_i[7], instr_i[2], instr_i[11], instr_i[5:3], 1'b0};
+endmodule
