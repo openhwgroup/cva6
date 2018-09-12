7ddee4d0e79f3ff48c56850df1e693f14e36d8be
Michael Schaffner <schaffner@iis.ee.ethz.ch>
2018-09-12 15:15:01 +0200

    Bump GCC version to 7.2 in CI flows

--- ci/build-riscv-tests.sh
+++ ci/build-riscv-tests.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -e
 ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
+VERSION="294bfce8a1ca2fc501b8939292146e44f813a2b8"
+
 cd $ROOT/tmp
 
 if [ -z ${NUM_JOBS} ]; then
@@ -9,7 +11,7 @@ fi
 
 [ -d $ROOT/tmp/riscv-tests ] || git clone https://github.com/riscv/riscv-tests.git
 cd riscv-tests
-git checkout 294bfce8a1ca2fc501b8939292146e44f813a2b8
+git checkout $VERSION
 git submodule update --init --recursive
 autoconf
 mkdir -p build
