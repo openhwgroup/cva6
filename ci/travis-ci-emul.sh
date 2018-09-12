d2a2521bfddddb3820a6688c90e8e2f4547e03c8
Michael Schaffner <schaffner@iis.ee.ethz.ch>
2018-09-12 23:11:03 +0200

    Add torture test targets to Makefile and fix CI flows

--- ci/travis-ci-emul.sh
+++ ci/travis-ci-emul.sh
@@ -3,8 +3,8 @@
 # source this with a bash shell in the project root
 # comment out next command if you don't want to use sudo
 sudo apt install \
-    gcc-4.8 \
-    g++-4.8 \
+    gcc-7 \
+    g++-7 \
     gperf \
     autoconf \
     automake \
@@ -24,6 +24,7 @@ sudo apt install \
 # customize your paths here
 source ci/path-setup.sh
 
+# install the required tools
 git submodule update --init --recursive
 ci/make-tmp.sh
 ci/build-riscv-gcc.sh
@@ -32,10 +33,14 @@ ci/install-verilator.sh
 ci/build-riscv-tests.sh
 ci/install-dtc.sh
 ci/install-spike.sh
+ci/get-torture.sh
+
+# clean up and generate randomized test
 make clean
+make torture-gen
 
 # run asm tests on verilator
 make -j${NUM_JOBS} verilate                 verilator=$VERILATOR_ROOT/bin/verilator
 make -j${NUM_JOBS} run-asm-tests-verilator  verilator=$VERILATOR_ROOT/bin/verilator
 make -j${NUM_JOBS} run-benchmarks-verilator verilator=$VERILATOR_ROOT/bin/verilator
-make -j${NUM_JOBS} run-torture-verilator    verilator=$VERILATOR_ROOT/bin/verilator
+make -j${NUM_JOBS} torture-rtest-verilator    verilator=$VERILATOR_ROOT/bin/verilator
