44d0f28c4d2af95fbb45a471cdc929dc31474f2e
Moritz Schneider <scmoritz@student.ethz.ch>
2018-11-23 15:45:31 +0100

    Update the SPI perihperal

--- fpga/xilinx/xlnx_axi_quad_spi/tcl/run.tcl
+++ fpga/xilinx/xlnx_axi_quad_spi/tcl/run.tcl
@@ -7,7 +7,7 @@ create_project $ipName . -part $partNumber
 set_property board_part $boardName [current_project]
 
 create_ip -name axi_quad_spi -vendor xilinx.com -library ip -module_name $ipName
-set_property -dict [list CONFIG.C_USE_STARTUP {0} CONFIG.C_USE_STARTUP_INT {0} CONFIG.C_SCK_RATIO {4} CONFIG.C_TYPE_OF_AXI4_INTERFACE {1} CONFIG.C_S_AXI4_ID_WIDTH {0}] [get_ips $ipName]
+set_property -dict [list CONFIG.C_USE_STARTUP {0} CONFIG.C_SCK_RATIO {4} CONFIG.C_FIFO_DEPTH {256} CONFIG.C_TYPE_OF_AXI4_INTERFACE {1} CONFIG.C_S_AXI4_ID_WIDTH {0}] [get_ips $ipName]
 
 generate_target {instantiation_template} [get_files ./$ipName.srcs/sources_1/ip/$ipName/$ipName.xci]
 generate_target all [get_files  ./$ipName.srcs/sources_1/ip/$ipName/$ipName.xci]
