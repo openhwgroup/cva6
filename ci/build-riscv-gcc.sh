6286c867e86a61e79cf618b474b2627de998b475
Michael Schaffner <schaffner@iis.ee.ethz.ch>
2019-01-08 13:09:44 +0100

    Split GCC compilation and checkout into two travis targets.

--- ci/build-riscv-gcc.sh
+++ ci/build-riscv-gcc.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# call with first argument = 0 to checkout only
+
 set -e
 ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 VERSION="691e4e826251c7ec59f883cab18440c87baf45e7"
@@ -8,6 +10,7 @@ if [ -z ${NUM_JOBS} ]; then
     NUM_JOBS=1
 fi
 
+
 if ! [ -e $RISCV/bin ]; then
     [ -d $ROOT/tmp/riscv-gnu-toolchain ] || git clone https://github.com/riscv/riscv-gnu-toolchain.git
     cd riscv-gnu-toolchain
@@ -16,8 +19,10 @@ if ! [ -e $RISCV/bin ]; then
 
     mkdir -p $RISCV
 
-    echo "Compiling RISC-V Toolchain"
-    ./configure --prefix=$RISCV > /dev/null
-    make -j${NUM_JOBS} > /dev/null
-    echo "Compilation Finished"
+    if [[ $1 -ne "0" || -z ${1} ]]; then
+      echo "Compiling RISC-V Toolchain"
+      ./configure --prefix=$RISCV > /dev/null
+      make -j${NUM_JOBS} > /dev/null
+      echo "Compilation Finished"
+    fi
 fi
