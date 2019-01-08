5147435ba18318145f7a5461dc8f8ce9b1f2cf27
Michael Schaffner <schaffner@iis.ee.ethz.ch>
2019-01-08 13:41:39 +0100

    Fix travis cache path

--- ci/build-riscv-gcc.sh
+++ ci/build-riscv-gcc.sh
@@ -4,7 +4,10 @@
 set -e
 ROOT=$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)
 VERSION="691e4e826251c7ec59f883cab18440c87baf45e7"
-cd $ROOT/tmp
+
+mkdir -p $RISCV
+
+cd $RISCV
 
 if [ -z ${NUM_JOBS} ]; then
     NUM_JOBS=1
@@ -12,12 +15,13 @@ fi
 
 
 if ! [ -e $RISCV/bin ]; then
-    [ -d $ROOT/tmp/riscv-gnu-toolchain ] || git clone https://github.com/riscv/riscv-gnu-toolchain.git
+    if ! [ -e $RISCV/riscv-gnu-toolchain ]; then
+        git clone https://github.com/riscv/riscv-gnu-toolchain.git
+    fi
+
     cd riscv-gnu-toolchain
     git checkout $VERSION
     git submodule update --init --recursive
-
-    mkdir -p $RISCV
 
     if [[ $1 -ne "0" || -z ${1} ]]; then
       echo "Compiling RISC-V Toolchain"
