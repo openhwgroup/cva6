a0538a97b44d12fd0523e2604e389bd18e685190
Jonathan Balkind <jbalkind@princeton.edu>
2019-02-19 16:23:10 -0500

    Testing barrier-based synchronisation instead of CLINT-based

--- fpga/src/bootrom/src/smp.h
+++ fpga/src/bootrom/src/smp.h
@@ -6,11 +6,13 @@
 #endif
 
 // The maximum number of HARTs this code supports
-#define CLINT_CTRL_ADDR 0xFFF1020000
+//#define CLINT_CTRL_ADDR 0xFFF1020000
+// Barrier on the cacheline above the stack pointer
+#define SMP_BARRIER_ADDR (DRAM_BASE + 64)
 #ifndef MAX_HARTS
-#define MAX_HARTS 4
+#define MAX_HARTS 2
 #endif
-#define CLINT_END_HART_IPI CLINT_CTRL_ADDR + (MAX_HARTS * 4)
+//#define CLINT_END_HART_IPI CLINT_CTRL_ADDR + (MAX_HARTS * 4)
 
 /* If your test needs to temporarily block multiple-threads, do this:
  *    smp_pause(reg1, reg2)
@@ -20,33 +22,21 @@
  */
 
 #define smp_pause(reg1, reg2) \
-    li reg2, 0x8;             \
-    csrw mie, reg2;           \
     li reg1, NONSMP_HART;     \
     csrr reg2, mhartid;       \
     bne reg1, reg2, 42f
 
 #define smp_resume(reg1, reg2)   \
-    li reg1, CLINT_CTRL_ADDR;    \
-    41:;                         \
-    li reg2, 1;                  \
-    sw reg2, 0(reg1);            \
-    addi reg1, reg1, 4;          \
-    li reg2, CLINT_END_HART_IPI; \
-    blt reg1, reg2, 41b;         \
     42:;                         \
-    wfi;                         \
-    csrr reg2, mip;              \
-    andi reg2, reg2, 0x8;        \
-    beqz reg2, 42b;              \
-    li reg1, CLINT_CTRL_ADDR;    \
-    csrr reg2, mhartid;          \
-    slli reg2, reg2, 2;          \
-    add reg2, reg2, reg1;        \
-    sw zero, 0(reg2);            \
-    41:;                         \
+    li reg1, SMP_BARRIER_ADDR;   \
     lw reg2, 0(reg1);            \
-    bnez reg2, 41b;              \
-    addi reg1, reg1, 4;          \
-    li reg2, CLINT_END_HART_IPI; \
-    blt reg1, reg2, 41b
+    csrr reg1, mhartid;          \
+    bne reg1, reg2, 42b;         \
+    li reg1, SMP_BARRIER_ADDR;   \
+    addi reg2, reg2, 1;          \
+    amoswap.w reg1, reg2, (reg1);\
+    43:;                         \
+    li reg1, SMP_BARRIER_ADDR;   \
+    lw reg2, 0(reg1);            \
+    li reg1, MAX_HARTS;          \
+    bne reg1, reg2, 43b
