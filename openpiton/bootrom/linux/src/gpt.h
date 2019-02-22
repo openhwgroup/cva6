d997985d5b7bfdc20a1f8f450b3df98615582d29
msfschaffner <schaffner@iis.ee.ethz.ch>
2019-02-22 11:39:14 +0100

    Refactor serpent AXI adapter, bump dbg and atomics submodules, add separate bootrom for linux on OpenPiton (#190)
    * Refactor serpent AXI adapter
    
    * Disable FPU in OpenPiton by default
    
    * Bump dbg and atomics submodules
    
    * Fix cache testbenches (interface change)
    
    * FPGA bootrom changes for OpenPiton SDHC
    
    * Introduce two bootroms, one for baremetal apps (pitonstream), and one for linux boot from SD
    
    * Testing barrier-based synchronisation instead of CLINT-based
    
    * This bootrom works for 2 core on g2 and if you change MAX_HARTS to 4, then 4 cores on vc707
    
    * Add MAX_HARTS switch to makefile
    
    * Fix gitlab CI
    
    * Revert standard FPGA bootrom
    
    * Update Flist
    
    * Make UART_FREQ a parameter
    
    * Fix typo in tb.list and an error in define switch in ariane_pkg
    
    * Copy over SD-driver in bootloader from @leon575777642
    
    * Fix compilation issues of bootrom
    
    * Change signal name in serpent periph portlist
    
    * Correct generate statement in serpent dcache memory

---
+++ openpiton/bootrom/linux/src/gpt.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <stdint.h>
+
+// LBA 0: Protective MBR
+// ignored here
+
+// Partition Table Header (LBA 1)
+typedef struct gpt_pth
+{
+    uint64_t signature;
+    uint32_t revision;
+    uint32_t header_size; //! little endian, usually 0x5c = 92
+    uint32_t crc_header;
+    uint32_t reserved; //! must be 0
+    uint64_t current_lba;
+    uint64_t backup_lba;
+    uint64_t first_usable_lba;
+    uint64_t last_usable_lba;
+    uint8_t disk_guid[16];
+    uint64_t partition_entries_lba;
+    uint32_t nr_partition_entries;
+    uint32_t size_partition_entry; //! usually 0x80 = 128
+    uint32_t crc_partition_entry;
+} gpt_pth_t;
+
+// Partition Entries (LBA 2-33)
+typedef struct partition_entries
+{
+    uint8_t partition_type_guid[16];
+    uint8_t partition_guid[16];
+    uint64_t first_lba;
+    uint64_t last_lba; //! inclusive
+    uint64_t attributes;
+    uint8_t name[72]; //! utf16 encoded
+} partition_entries_t;
+
+// Find boot partition and load it to the destination
+int gpt_find_boot_partition(uint8_t* dest, uint32_t size);
---
+++ openpiton/bootrom/linux/src/main.c
@@ -0,0 +1,19 @@
+#include "uart.h"
+#include "spi.h"
+#include "sd.h"
+#include "gpt.h"
+
+int main()
+{
+    init_uart(UART_FREQ, 115200);
+    print_uart("Hello World!\r\n");
+
+    int res = gpt_find_boot_partition((uint8_t *)0x80000000UL, 2 * 16384);
+
+    return 0;
+}
+
+void handle_trap(void)
+{
+    // print_uart("trap\r\n");
+}
