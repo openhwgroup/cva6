8de5dd5eb82a976eb244add7d48b62ad3f85c191
Michael Schaffner <schaffner@iis.ee.ethz.ch>
2019-03-14 16:03:31 +0100

    Output more status info in zsbl for OpenPiton

--- openpiton/bootrom/linux/src/sd.c
+++ openpiton/bootrom/linux/src/sd.c
@@ -23,6 +23,15 @@ int sd_copy(void *dst, uint32_t src_lba, uint32_t size)
     uint64_t * addr = (uint64_t *)raw_addr;
     volatile uint64_t * p = (uint64_t *)dst;
     for (uint32_t blk = 0; blk < size; blk++) {
+        if(blk % 100 == 0) {
+          print_uart("copying block ");
+          print_uart_dec(blk, 1);
+          print_uart(" of ");
+          print_uart_dec(size, 1);
+          print_uart(" blocks (");
+          print_uart_dec((blk*100)/size, 1);
+          print_uart(" %)\r\n");
+        }
         for (uint32_t offset = 0; offset < 64; offset++) {
             *(p++) = *(addr++);
         }
