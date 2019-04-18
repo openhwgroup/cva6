67a7508ed6f3ebf7a7395ccb862379f6a947c64f
Michael Schaffner <schaffner@iis.ee.ethz.ch>
2019-04-18 16:41:24 +0200

    Update bootrom for OpenPiton to print some more info

--- openpiton/bootrom/linux/src/main.c
+++ openpiton/bootrom/linux/src/main.c
@@ -2,11 +2,14 @@
 #include "spi.h"
 #include "sd.h"
 #include "gpt.h"
+#include "info.h"
 
 int main()
 {
     init_uart(UART_FREQ, 115200);
-    print_uart("Hello World!\r\n");
+    print_uart(info);
+
+    print_uart("sd initialized!\r\n");
 
     int res = gpt_find_boot_partition((uint8_t *)0x80000000UL, 2 * 16384);
 
