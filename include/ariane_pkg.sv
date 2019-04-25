3e15d328c218c857e405916a74394fc8686ec69e
Michael Schaffner <schaffner@iis.ee.ethz.ch>
2019-04-25 17:22:45 +0200

    fpu: Add distributed pipe regs to ease FPGA timing

--- include/ariane_pkg.sv
+++ include/ariane_pkg.sv
@@ -161,12 +161,12 @@ package ariane_pkg;
 
 `ifdef PITON_ARIANE
     // Floating-point extensions configuration
-    localparam bit RVF = 1'b0; // Is F extension enabled
-    localparam bit RVD = 1'b0; // Is D extension enabled
+    localparam bit RVF = 1'b1; // Is F extension enabled
+    localparam bit RVD = 1'b1; // Is D extension enabled
 `else
     // Floating-point extensions configuration
-    localparam bit RVF = 1'b0; // Is F extension enabled
-    localparam bit RVD = 1'b0; // Is D extension enabled
+    localparam bit RVF = 1'b1; // Is F extension enabled
+    localparam bit RVD = 1'b1; // Is D extension enabled
 `endif
     localparam bit RVA = 1'b1; // Is A extension enabled
 
