6a7817aef4e041975e1be2a60bfee968886daa1f
Stefan Mach <smach@iis.ee.ethz.ch>
2019-04-25 19:27:27 +0200

    :arrow_up: Bump FPU and enable distributed pipeline

--- src/fpu_wrap.sv
+++ src/fpu_wrap.sv
@@ -71,7 +71,7 @@ generate
                    '{default: fpnew_pkg::MERGED},   // DIVSQRT
                    '{default: fpnew_pkg::PARALLEL}, // NONCOMP
                    '{default: fpnew_pkg::MERGED}},  // CONV
-      PipeConfig: fpnew_pkg::INSIDE
+      PipeConfig: fpnew_pkg::DISTRIBUTED
     };
 
     //-------------------------------------------------
