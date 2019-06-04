wallento/ariane

fpu: Add distributed pipe regs to ease FPGA timing

src/fpu_wrap.sv
@@ -71,7 +71,7 @@ generate
                    '{default: fpnew_pkg::MERGED},   // DIVSQRT
                    '{default: fpnew_pkg::PARALLEL}, // NONCOMP
                    '{default: fpnew_pkg::MERGED}},  // CONV
-      PipeConfig: fpnew_pkg::AFTER
+      PipeConfig: fpnew_pkg::INSIDE
     };
 
     //-------------------------------------------------

openhwgroup/cva6

src/fpu_wrap.sv
@@ -71,7 +71,7 @@ generate
                    '{default: fpnew_pkg::MERGED},   // DIVSQRT
                    '{default: fpnew_pkg::PARALLEL}, // NONCOMP
                    '{default: fpnew_pkg::MERGED}},  // CONV
-      PipeConfig: fpnew_pkg::AFTER
+      PipeConfig: fpnew_pkg::DISTRIBUTED
     };
 
     //-------------------------------------------------