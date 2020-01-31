0d063c0387e8fdc1bf8562d8c831b96484d34bc3
Wolfgang Rönninger <wroennin@student.ethz.ch>
2020-01-31 18:41:38 +0100

    gen_bender: Added script for bender vsim export

---
+++ gen_bender.sh
@@ -0,0 +1,2 @@
+bender script vsim --target="test" --vlog-arg="+cover=bcfst+/dut -64 -nologo -suppress 13262,2583 -permissive -pedanticerrors" --vcom-arg="-64 -nologo -2008" > ./scripts/compile_vsim_bender.tcl
+#-svinputport=compat -quiet
