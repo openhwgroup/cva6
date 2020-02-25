ab451d242c5eeec473c63e3266bfb9095d40e493
Wolfgang Rönninger <wroennin@student.ethz.ch>
2020-02-25 15:45:39 +0100

    fpga/scripts/run: Update script as include is handled by bender

--- fpga/scripts/run.tcl
+++ fpga/scripts/run.tcl
@@ -34,35 +34,35 @@ read_ip xilinx/xlnx_axi_quad_spi/ip/xlnx_axi_quad_spi.xci
 read_ip xilinx/xlnx_clk_gen/ip/xlnx_clk_gen.xci
 # read_ip xilinx/xlnx_protocol_checker/ip/xlnx_protocol_checker.xci
 
-set_property include_dirs { "src/axi_sd_bridge/include" "../src/common_cells/include" } [current_fileset]
+#set_property include_dirs { "src/axi_sd_bridge/include" "../src/common_cells/include" } [current_fileset]
 
 source scripts/add_sources.tcl
 
 set_property top ${project}_xilinx [current_fileset]
 
-if {$::env(BOARD) eq "genesys2"} {
-    read_verilog -sv {src/genesysii.svh ../src/common_cells/include/common_cells/registers.svh}
-    set file "src/genesysii.svh"
-    set registers "../src/common_cells/include/common_cells/registers.svh"
-} elseif {$::env(BOARD) eq "kc705"} {
-      read_verilog -sv {src/kc705.svh ../src/common_cells/include/common_cells/registers.svh}
-      set file "src/kc705.svh"
-      set registers "../src/common_cells/include/common_cells/registers.svh"
-} elseif {$::env(BOARD) eq "vc707"} {
-      read_verilog -sv {src/vc707.svh ../src/common_cells/include/common_cells/registers.svh}
-      set file "src/vc707.svh"
-      set registers "../src/common_cells/include/common_cells/registers.svh"
-} else {
-    exit 1
-}
-
-set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file" "$registers"]]
-set_property -dict { file_type {Verilog Header} is_global_include 1} -objects $file_obj
-
-update_compile_order -fileset sources_1
-
-add_files -fileset constrs_1 -norecurse constraints/$project.xdc
-
+#if {$::env(BOARD) eq "genesys2"} {
+#    read_verilog -sv {src/genesysii.svh ../src/common_cells/include/common_cells/registers.svh}
+#    set file "src/genesysii.svh"
+#    set registers "../src/common_cells/include/common_cells/registers.svh"
+#} elseif {$::env(BOARD) eq "kc705"} {
+#      read_verilog -sv {src/kc705.svh ../src/common_cells/include/common_cells/registers.svh}
+#      set file "src/kc705.svh"
+#      set registers "../src/common_cells/include/common_cells/registers.svh"
+#} elseif {$::env(BOARD) eq "vc707"} {
+#      read_verilog -sv {src/vc707.svh ../src/common_cells/include/common_cells/registers.svh}
+#      set file "src/vc707.svh"
+#      set registers "../src/common_cells/include/common_cells/registers.svh"
+#} else {
+#    exit 1
+#}
+#
+#set file_obj [get_files -of_objects [get_filesets sources_1] [list "*$file" "$registers"]]
+#set_property -dict { file_type {Verilog Header} is_global_include 1} -objects $file_obj
+#
+#update_compile_order -fileset sources_1
+#
+#add_files -fileset constrs_1 -norecurse constraints/$project.xdc
+#
 synth_design -rtl -name rtl_1
 
 set_property STEPS.SYNTH_DESIGN.ARGS.RETIMING true [get_runs synth_1]
