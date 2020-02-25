79485040367b6bc343b1054a058a9d3af80b5bdc
Wolfgang Rönninger <wroennin@student.ethz.ch>
2020-02-25 15:46:28 +0100

    bender generation, add svinputport vlog arg

--- gen_bender.sh
+++ gen_bender.sh
@@ -1,2 +1,2 @@
-bender script vsim --target="test" --vlog-arg="+cover=bcfst+/dut -64 -nologo -suppress 13262,2583 -permissive -pedanticerrors" --vcom-arg="-64 -nologo -2008" > ./scripts/compile_vsim_bender.tcl
+bender script vsim --target="test" --vlog-arg="+cover=bcfst+/dut -64 -nologo -suppress 13262,2583 -permissive -pedanticerrors -svinputport=compat" --vcom-arg="-64 -nologo -2008" > ./scripts/compile_vsim_bender.tcl
 #-svinputport=compat -quiet
