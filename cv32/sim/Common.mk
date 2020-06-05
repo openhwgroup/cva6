31d02092598af09f17ff5d0750f18e3f1ac28382
Craig Blackmore <craig.blackmore@embecosm.com>
2020-06-05 15:43:07 +0100

    Fix warnings in hello-world.c
    hello-world.c is now compiled with -Wall -pedantic rather than -w so
    that warnings are not hidden. The resulting warnings have now been
    fixed.
    
    The initialization of mvendorid_rval was commented out in a previous
    commit, so don't attempt to print its value.
    
    ++nonstd is now correctly guarded by `if ((misa_rval >> 23) & 0x1)`.
    
    Signed-off-by: Craig Blackmore <craig.blackmore@embecosm.com>

--- cv32/sim/Common.mk
+++ cv32/sim/Common.mk
@@ -214,7 +214,7 @@ $(CUSTOM_DIR)/$(CUSTOM_PROG).elf: $(CUSTOM_DIR)/$(CUSTOM_PROG).c
 
 # HELLO WORLD: custom/hello_world.elf: ../../tests/core/custom/hello_world.c
 $(CUSTOM)/hello_world.elf: $(CUSTOM)/hello_world.c
-	$(RISCV_EXE_PREFIX)gcc -mabi=ilp32 -march=rv32imc -o $@ -w -Os -g -nostdlib \
+	$(RISCV_EXE_PREFIX)gcc -mabi=ilp32 -march=rv32imc -o $@ -Wall -pedantic -Os -g -nostdlib \
 		-T $(CUSTOM)/link.ld  \
 		-static \
 		$(CUSTOM)/crt0.S \
