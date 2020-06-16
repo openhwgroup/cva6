81451ea7595a6bde2aec5362945c8ad993294b2c
Nursultan Kabylkas <kabylkas@gmail.com>
2020-06-15 20:34:02 -0700

    added license headers

--- tb/dpi/dromajo_cosim_dpi.cc
+++ tb/dpi/dromajo_cosim_dpi.cc
@@ -1,3 +1,17 @@
+// Copyright 2017-2020 ETH Zurich and University of Bologna.
+// Copyright and related rights are licensed under the Solderpad Hardware
+// License, Version 0.51 (the "License"); you may not use this file except in
+// compliance with the License.  You may obtain a copy of the License at
+// http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+// or agreed to in writing, software, hardware and materials distributed under
+// this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+// CONDITIONS OF ANY KIND, either express or implied. See the License for the
+// specific language governing permissions and limitations under the License.
+//
+// Author: Nursultan Kabylkas, UCSC
+// Date: Jun 15, 2020
+// Description: DPI wrappers to interface with Dromajo RISC-V emulator
+
 #include <svdpi.h>
 #include <iostream>
 #include "dromajo_cosim.h"
