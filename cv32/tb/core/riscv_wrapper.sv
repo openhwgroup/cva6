def656ca77deb6bcbd3d5f061804d1dbe6cf1069
Paul Zavalney <paul.zavalney@silabs.com>
2020-06-29 15:56:20 -0500

    fix core TB with dm_halt_addr connection to mm_ram
    Signed-off-by: Paul Zavalney <paul.zavalney@silabs.com>

--- cv32/tb/core/riscv_wrapper.sv
+++ cv32/tb/core/riscv_wrapper.sv
@@ -134,7 +134,7 @@ module riscv_wrapper
     ram_i
         (.clk_i          ( clk_i                          ),
          .rst_ni         ( rst_ni                         ),
-         .dm_halt_addr_i ( core_cntrl_if.dm_halt_addr     ),
+         .dm_halt_addr_i ( DM_HALTADDRESS                 ),
 
          .instr_req_i    ( instr_req                      ),
          .instr_addr_i   ( instr_addr[RAM_ADDR_WIDTH-1:0] ),
