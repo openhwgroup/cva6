0eb6d327fc6cdba93f18d01c62d5c6064e129f04
Ruige Lee <295054118@qq.com>
2020-08-27 14:57:31 +0800

    clean instr_tracer.sv
    Signed-off-by: Ruige Lee <295054118@qq.com>

--- src/util/instr_tracer.sv
+++ src/util/instr_tracer.sv
@@ -20,7 +20,7 @@ import uvm_pkg::*;
 `include "instr_trace_item.svh"
 //pragma translate_on
 
-module instr_tracer (
+module instr_tracer import ariane_pkg::*; (
   instr_tracer_if   tracer_if,
   input logic[63:0] hart_id_i
 );
@@ -30,10 +30,10 @@ module instr_tracer (
   // keep the issued instructions in a queue
   logic [31:0] issue_queue [$];
   // issue scoreboard entries
-  ariane_pkg::scoreboard_entry_t issue_sbe_queue [$];
-  ariane_pkg::scoreboard_entry_t issue_sbe;
+  scoreboard_entry_t issue_sbe_queue [$];
+  scoreboard_entry_t issue_sbe;
   // store resolved branches, get (mis-)predictions
-  ariane_pkg::bp_resolve_t bp [$];
+  bp_resolve_t bp [$];
   // shadow copy of the register files
   logic [63:0] gp_reg_file [32];
   logic [63:0] fp_reg_file [32];
@@ -53,18 +53,18 @@ module instr_tracer (
     $display("[TRACER] Output filename is: %s", fn);
 
     f = $fopen(fn,"w");
-    if (ariane_pkg::ENABLE_SPIKE_COMMIT_LOG) commit_log = $fopen(fn_commit_log, "w");
+    if (ENABLE_SPIKE_COMMIT_LOG) commit_log = $fopen(fn_commit_log, "w");
   endfunction : create_file
 
   task trace();
     automatic logic [31:0] decode_instruction, issue_instruction, issue_commit_instruction;
-    automatic ariane_pkg::scoreboard_entry_t commit_instruction;
+    automatic scoreboard_entry_t commit_instruction;
     // initialize register 0
     gp_reg_file  = '{default:0};
     fp_reg_file  = '{default:0};
 
     forever begin
-      automatic ariane_pkg::bp_resolve_t bp_instruction = '0;
+      automatic bp_resolve_t bp_instruction = '0;
       // new cycle, we are only interested if reset is de-asserted
       @(tracer_if.pck iff tracer_if.pck.rstn);
       // increment clock tick
@@ -87,7 +87,7 @@ module instr_tracer (
         issue_instruction = decode_queue.pop_front();
         issue_queue.push_back(issue_instruction);
         // also save the scoreboard entry to a separate issue queue
-        issue_sbe_queue.push_back(ariane_pkg::scoreboard_entry_t'(tracer_if.pck.issue_sbe));
+        issue_sbe_queue.push_back(scoreboard_entry_t'(tracer_if.pck.issue_sbe));
       end
 
       // --------------------
@@ -112,23 +112,23 @@ module instr_tracer (
       // we are committing an instruction
       for (int i = 0; i < 2; i++) begin
         if (tracer_if.pck.commit_ack[i]) begin
-          commit_instruction = ariane_pkg::scoreboard_entry_t'(tracer_if.pck.commit_instr[i]);
+          commit_instruction = scoreboard_entry_t'(tracer_if.pck.commit_instr[i]);
           issue_commit_instruction = issue_queue.pop_front();
           issue_sbe = issue_sbe_queue.pop_front();
           // check if the instruction retiring is a load or store, get the physical address accordingly
-          if (tracer_if.pck.commit_instr[i].fu == ariane_pkg::LOAD)
+          if (tracer_if.pck.commit_instr[i].fu == LOAD)
             address_mapping = load_mapping.pop_front();
-          else if (tracer_if.pck.commit_instr[i].fu == ariane_pkg::STORE)
+          else if (tracer_if.pck.commit_instr[i].fu == STORE)
             address_mapping = store_mapping.pop_front();
 
-          if (tracer_if.pck.commit_instr[i].fu == ariane_pkg::CTRL_FLOW)
+          if (tracer_if.pck.commit_instr[i].fu == CTRL_FLOW)
             bp_instruction = bp.pop_front();
           // the scoreboards issue entry still contains the immediate value as a result
           // check if the write back is valid, if not we need to source the result from the register file
           // as the most recent version of this register will be there.
           if (tracer_if.pck.we_gpr[i] || tracer_if.pck.we_fpr[i]) begin
             printInstr(issue_sbe, issue_commit_instruction, tracer_if.pck.wdata[i], address_mapping, tracer_if.pck.priv_lvl, tracer_if.pck.debug_mode, bp_instruction);
-          end else if (ariane_pkg::is_rd_fpr(commit_instruction.op)) begin
+          end else if (is_rd_fpr(commit_instruction.op)) begin
             printInstr(issue_sbe, issue_commit_instruction, fp_reg_file[commit_instruction.rd], address_mapping, tracer_if.pck.priv_lvl, tracer_if.pck.debug_mode, bp_instruction);
           end else begin
             printInstr(issue_sbe, issue_commit_instruction, gp_reg_file[commit_instruction.rd], address_mapping, tracer_if.pck.priv_lvl, tracer_if.pck.debug_mode, bp_instruction);
@@ -186,12 +186,12 @@ module instr_tracer (
   endfunction
 
   // pragma translate_off
-  function void printInstr(ariane_pkg::scoreboard_entry_t sbe, logic [31:0] instr, logic [63:0] result, logic [riscv::PLEN-1:0] paddr, riscv::priv_lvl_t priv_lvl, logic debug_mode, ariane_pkg::bp_resolve_t bp);
+  function void printInstr(scoreboard_entry_t sbe, logic [31:0] instr, logic [63:0] result, logic [riscv::PLEN-1:0] paddr, riscv::priv_lvl_t priv_lvl, logic debug_mode, bp_resolve_t bp);
     automatic instr_trace_item iti = new ($time, clk_ticks, sbe, instr, gp_reg_file, fp_reg_file, result, paddr, priv_lvl, debug_mode, bp);
     // print instruction to console
     automatic string print_instr = iti.printInstr();
-    if (ariane_pkg::ENABLE_SPIKE_COMMIT_LOG && !debug_mode) begin
-      $fwrite(commit_log, riscv::spikeCommitLog(sbe.pc, priv_lvl, instr, sbe.rd, result, ariane_pkg::is_rd_fpr(sbe.op)));
+    if (ENABLE_SPIKE_COMMIT_LOG && !debug_mode) begin
+      $fwrite(commit_log, riscv::spikeCommitLog(sbe.pc, priv_lvl, instr, sbe.rd, result, is_rd_fpr(sbe.op)));
     end
     uvm_report_info( "Tracer",  print_instr, UVM_HIGH);
     $fwrite(f, {print_instr, "\n"});
@@ -206,7 +206,7 @@ module instr_tracer (
 
   function void close();
     if (f) $fclose(f);
-    if (ariane_pkg::ENABLE_SPIKE_COMMIT_LOG && commit_log) $fclose(commit_log);
+    if (ENABLE_SPIKE_COMMIT_LOG && commit_log) $fclose(commit_log);
   endfunction
 
 
