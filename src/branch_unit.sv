13d938f65970f52b92bbcd38125535082fbd835e
Ruige Lee <295054118@qq.com>
2020-08-27 14:06:01 +0800

    clean branch_unit.sv
    Signed-off-by: Ruige Lee <295054118@qq.com>

--- src/branch_unit.sv
+++ src/branch_unit.sv
@@ -12,48 +12,48 @@
 // Date: 09.05.2017
 // Description: Branch target calculation and comparison
 
-module branch_unit (
+module branch_unit import riscv::*; import ariane_pkg::*; (
     input  logic                      clk_i,
     input  logic                      rst_ni,
     input  logic                      debug_mode_i,
-    input  ariane_pkg::fu_data_t      fu_data_i,
-    input  logic [riscv::VLEN-1:0]    pc_i,                   // PC of instruction
+    input  fu_data_t      fu_data_i,
+    input  logic [VLEN-1:0]    pc_i,                   // PC of instruction
     input  logic                      is_compressed_instr_i,
     input  logic                      fu_valid_i,             // any functional unit is valid, check that there is no accidental mis-predict
     input  logic                      branch_valid_i,
     input  logic                      branch_comp_res_i,      // branch comparison result from ALU
-    output logic [riscv::VLEN-1:0]    branch_result_o,
+    output logic [VLEN-1:0]    branch_result_o,
 
-    input  ariane_pkg::branchpredict_sbe_t        branch_predict_i,       // this is the address we predicted
-    output ariane_pkg::bp_resolve_t               resolved_branch_o,      // this is the actual address we are targeting
+    input  branchpredict_sbe_t        branch_predict_i,       // this is the address we predicted
+    output bp_resolve_t               resolved_branch_o,      // this is the actual address we are targeting
     output logic                      resolve_branch_o,       // to ID to clear that we resolved the branch and we can
                                                               // accept new entries to the scoreboard
-    output ariane_pkg::exception_t    branch_exception_o      // branch exception out
+    output exception_t    branch_exception_o      // branch exception out
 );
-    logic [riscv::VLEN-1:0] target_address;
-    logic [riscv::VLEN-1:0] next_pc;
+    logic [VLEN-1:0] target_address;
+    logic [VLEN-1:0] next_pc;
 
    // here we handle the various possibilities of mis-predicts
     always_comb begin : mispredict_handler
         // set the jump base, for JALR we need to look at the register, for all other control flow instructions we can take the current PC
-        automatic logic [riscv::VLEN-1:0] jump_base;
+        automatic logic [VLEN-1:0] jump_base;
         // TODO(zarubaf): The ALU can be used to calculate the branch target
-        jump_base = (fu_data_i.operator == ariane_pkg::JALR) ? fu_data_i.operand_a[riscv::VLEN-1:0] : pc_i;
+        jump_base = (fu_data_i.operator == JALR) ? fu_data_i.operand_a[VLEN-1:0] : pc_i;
 
-        target_address                   = {riscv::VLEN{1'b0}};
+        target_address                   = {VLEN{1'b0}};
         resolve_branch_o                 = 1'b0;
-        resolved_branch_o.target_address = {riscv::VLEN{1'b0}};
+        resolved_branch_o.target_address = {VLEN{1'b0}};
         resolved_branch_o.is_taken       = 1'b0;
         resolved_branch_o.valid          = branch_valid_i;
         resolved_branch_o.is_mispredict  = 1'b0;
         resolved_branch_o.cf_type        = branch_predict_i.cf;
         // calculate next PC, depending on whether the instruction is compressed or not this may be different
         // TODO(zarubaf): We already calculate this a couple of times, maybe re-use?
-        next_pc                          = pc_i + ((is_compressed_instr_i) ? {{riscv::VLEN-2{1'b0}}, 2'h2} : {{riscv::VLEN-3{1'b0}}, 3'h4});
+        next_pc                          = pc_i + ((is_compressed_instr_i) ? {{VLEN-2{1'b0}}, 2'h2} : {{VLEN-3{1'b0}}, 3'h4});
         // calculate target address simple 64 bit addition
-        target_address                   = $unsigned($signed(jump_base) + $signed(fu_data_i.imm[riscv::VLEN-1:0]));
+        target_address                   = $unsigned($signed(jump_base) + $signed(fu_data_i.imm[VLEN-1:0]));
         // on a JALR we are supposed to reset the LSB to 0 (according to the specification)
-        if (fu_data_i.operator == ariane_pkg::JALR) target_address[0] = 1'b0;
+        if (fu_data_i.operator == JALR) target_address[0] = 1'b0;
         // we need to put the branch target address into rd, this is the result of this unit
         branch_result_o = next_pc;
         resolved_branch_o.pc = pc_i;
@@ -65,18 +65,18 @@ module branch_unit (
             resolved_branch_o.target_address = (branch_comp_res_i) ? target_address : next_pc;
             resolved_branch_o.is_taken = branch_comp_res_i;
             // check the outcome of the branch speculation
-            if (ariane_pkg::op_is_branch(fu_data_i.operator) && branch_comp_res_i != (branch_predict_i.cf == ariane_pkg::Branch)) begin
+            if (op_is_branch(fu_data_i.operator) && branch_comp_res_i != (branch_predict_i.cf == Branch)) begin
                 // we mis-predicted the outcome
                 // if the outcome doesn't match we've got a mis-predict
                 resolved_branch_o.is_mispredict  = 1'b1;
-                resolved_branch_o.cf_type = ariane_pkg::Branch;
+                resolved_branch_o.cf_type = Branch;
             end
-            if (fu_data_i.operator == ariane_pkg::JALR
+            if (fu_data_i.operator == JALR
                 // check if the address of the jump register is correct and that we actually predicted
-                && (branch_predict_i.cf == ariane_pkg::NoCF || target_address != branch_predict_i.predict_address)) begin
+                && (branch_predict_i.cf == NoCF || target_address != branch_predict_i.predict_address)) begin
                 resolved_branch_o.is_mispredict  = 1'b1;
                 // update BTB only if this wasn't a return
-                if (branch_predict_i.cf != ariane_pkg::Return) resolved_branch_o.cf_type = ariane_pkg::JumpR;
+                if (branch_predict_i.cf != Return) resolved_branch_o.cf_type = JumpR;
             end
             // to resolve the branch in ID
             resolve_branch_o = 1'b1;
@@ -85,9 +85,9 @@ module branch_unit (
     // use ALU exception signal for storing instruction fetch exceptions if
     // the target address is not aligned to a 2 byte boundary
     always_comb begin : exception_handling
-        branch_exception_o.cause = riscv::INSTR_ADDR_MISALIGNED;
+        branch_exception_o.cause = INSTR_ADDR_MISALIGNED;
         branch_exception_o.valid = 1'b0;
-        branch_exception_o.tval  = {{64-riscv::VLEN{pc_i[riscv::VLEN-1]}}, pc_i};
+        branch_exception_o.tval  = {{64-VLEN{pc_i[VLEN-1]}}, pc_i};
         // only throw exception if this is indeed a branch
         if (branch_valid_i && target_address[0] != 1'b0) branch_exception_o.valid = 1'b1;
     end
