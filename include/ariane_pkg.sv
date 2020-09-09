c5bbc1d67599c6fe46f10d093780fb630f3568ff
jcoulon <jcoulon@gemalto.com>
2020-09-09 22:36:29 +0200

    ariane_pkg.sv: reduce ASID_WIDTH for 32bits configuration

--- include/ariane_pkg.sv
+++ include/ariane_pkg.sv
@@ -131,7 +131,7 @@ package ariane_pkg;
     localparam NR_SB_ENTRIES = 8; // number of scoreboard entries
     localparam TRANS_ID_BITS = $clog2(NR_SB_ENTRIES); // depending on the number of scoreboard entries we need that many bits
                                                       // to uniquely identify the entry in the scoreboard
-    localparam ASID_WIDTH    = 16;
+    localparam ASID_WIDTH    = (riscv::XLEN == 64) ? 16 : 1;
     localparam BITS_SATURATION_COUNTER = 2;
     localparam NR_COMMIT_PORTS = 2;
 
