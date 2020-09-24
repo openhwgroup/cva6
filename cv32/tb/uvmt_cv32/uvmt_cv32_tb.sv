f1eb8a9804fcbc84ab4415248263b1cb5b808ec4
Arjan Bink <Arjan.Bink@silabs.com>
2020-09-24 03:27:51 -0500

    Updated various files because the register file bist wrapper has been removed, pointed to latest RTL
    Signed-off-by: Arjan Bink <Arjan.Bink@silabs.com>

--- cv32/tb/uvmt_cv32/uvmt_cv32_tb.sv
+++ cv32/tb/uvmt_cv32/uvmt_cv32_tb.sv
@@ -107,10 +107,10 @@ module uvmt_cv32_tb;
 
       //always @(dut_wrap.cv32e40p_wrapper_i.core_i.tracer_i.retire) -> step_compare_if.riscv_retire;
       //assign step_compare_if.insn_pc   = dut_wrap.cv32e40p_wrapper_i.core_i.tracer_i.insn_pc;
-      //assign step_compare_if.riscy_GPR = dut_wrap.cv32e40p_wrapper_i.core_i.id_stage_i.registers_i.register_file_i.mem;
+      //assign step_compare_if.riscy_GPR = dut_wrap.cv32e40p_wrapper_i.core_i.id_stage_i.register_file_i.mem;
       always @(dut_wrap.cv32e40p_wrapper_i.tracer_i.retire) -> step_compare_if.riscv_retire;
       assign step_compare_if.insn_pc   = dut_wrap.cv32e40p_wrapper_i.tracer_i.insn_pc;
-      assign step_compare_if.riscy_GPR = dut_wrap.cv32e40p_wrapper_i.core_i.id_stage_i.registers_i.register_file_i.mem;
+      assign step_compare_if.riscy_GPR = dut_wrap.cv32e40p_wrapper_i.core_i.id_stage_i.register_file_i.mem;
       assign clknrst_if_iss.reset_n = clknrst_if.reset_n;
     
       wire [31:0] irq_enabled;
