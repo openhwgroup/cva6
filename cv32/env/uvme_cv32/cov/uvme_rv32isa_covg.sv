40135b3c5bb7373265111c36ed95c6e4643341c8
Mike Thompson <mike@openhwgroup.org>
2020-10-09 22:44:33 -0500

    Yet another nop_cg fix
    Signed-off-by: Mike Thompson <mike@openhwgroup.org>

--- cv32/env/uvme_cv32/cov/uvme_rv32isa_covg.sv
+++ cv32/env/uvme_cv32/cov/uvme_rv32isa_covg.sv
@@ -712,7 +712,7 @@ class uvme_rv32isa_covg extends uvm_component;
 // FIXME: DONE
     covergroup nop_cg with function sample(ins_t ins);
         option.per_instance = 1;
-        cp_asm   : coverpoint (ins.asm == NOP); {
+        cp_asm   : coverpoint (ins.asm == NOP) {
             ignore_bins zero = {0};
             bins        one  = {1};
         }
