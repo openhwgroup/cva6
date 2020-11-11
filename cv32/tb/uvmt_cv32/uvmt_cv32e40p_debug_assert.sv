4254478839f119439d97482654bf68145373087b
Oystein Knauserud <Oystein.Knauserud@silabs.com>
2020-11-11 07:37:07 -0600

    Added single stepping c/ebreak with dcsr.ebreak=0
    Signed-off-by: Oystein Knauserud <Oystein.Knauserud@silabs.com>

--- cv32/tb/uvmt_cv32/uvmt_cv32e40p_debug_assert.sv
+++ cv32/tb/uvmt_cv32/uvmt_cv32e40p_debug_assert.sv
@@ -122,7 +122,7 @@ module uvmt_cv32e40p_debug_assert
     property p_cebreak_exception;
         disable iff(cov_assert_if.debug_req_i | cov_assert_if.rst_ni)
         $rose(cov_assert_if.is_cebreak) && cov_assert_if.dcsr_q[15] == 1'b0 && !cov_assert_if.debug_mode_q  && cov_assert_if.is_decoding && cov_assert_if.id_valid &&
-        !cov_assert_if.debug_req_i 
+        !cov_assert_if.debug_req_i && !cov_assert_if.dcsr_q[2]
         |-> (decode_valid & cov_assert_if.id_valid) [->2] ##0  !cov_assert_if.debug_mode_q && (cov_assert_if.mcause_q[5:0] === cv32e40p_pkg::EXC_CAUSE_BREAKPOINT) 
                                                                 && (cov_assert_if.mepc_q == pc_at_ebreak) &&
                                                                    (cov_assert_if.id_stage_pc == cov_assert_if.mtvec);
@@ -133,9 +133,10 @@ module uvmt_cv32e40p_debug_assert
             `uvm_error(info_tag,$sformatf("Exception not entered correctly after c.ebreak with dcsr.ebreak=0"));
 
     // ebreak without dcsr.ebreakm results in exception at mtvec
+    // Exclude single stepping as the sequence gets very complicated
     property p_ebreak_exception;
         $rose(cov_assert_if.is_ebreak) && cov_assert_if.dcsr_q[15] == 1'b0 && !cov_assert_if.debug_mode_q  && cov_assert_if.is_decoding && cov_assert_if.id_valid &&
-        !cov_assert_if.debug_req_i 
+        !cov_assert_if.debug_req_i && !cov_assert_if.dcsr_q[2] 
         |-> (decode_valid & cov_assert_if.id_valid) [->2] ##0  !cov_assert_if.debug_mode_q && (cov_assert_if.mcause_q[5:0] === cv32e40p_pkg::EXC_CAUSE_BREAKPOINT) 
                                                                 && (cov_assert_if.mepc_q == pc_at_ebreak) &&
                                                                    (cov_assert_if.id_stage_pc == cov_assert_if.mtvec);
