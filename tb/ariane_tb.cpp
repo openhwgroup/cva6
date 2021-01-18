eaeef7da1ad5d19f0fa76fe3e09c968a15e5ed9e
Florian Zaruba <florian@openhwgroup.org>
2021-01-18 14:08:06 +0100

    verilator: Dot reference compilation issue (fix #583) (#585)
    Signed-off-by: Florian Zaruba <florian@openhwgroup.org>

--- tb/ariane_tb.cpp
+++ tb/ariane_tb.cpp
@@ -320,7 +320,7 @@ done_processing:
 
   // Preload memory.
   size_t mem_size = 0x100000;
-  memif.read(0x80000000, mem_size, (void *) top->ariane_testharness__DOT__i_sram__DOT__genblk1__BRA__0__KET____DOT__genblk2__DOT__i_ram__DOT__Mem_DP);
+  memif.read(0x80000000, mem_size, (void *)top->ariane_testharness__DOT__i_sram__DOT__gen_cut__BRA__0__KET____DOT__gen_mem__DOT__i_ram__DOT__Mem_DP);
 
 #ifndef DROMAJO
   while (!dtm->done() && !jtag->done()) {
