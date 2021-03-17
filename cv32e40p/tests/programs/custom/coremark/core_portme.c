d0ded71d4c0847bfa761adb7253960865b4c8df2
Robin Pedersen <Robin.Pedersen@silabs.com>
2021-03-17 13:43:35 +0100

    add spdx identifiers to license headers
    Signed-off-by: Robin Pedersen <Robin.Pedersen@silabs.com>

--- cv32e40p/tests/programs/custom/coremark/core_portme.c
+++ cv32e40p/tests/programs/custom/coremark/core_portme.c
@@ -30,6 +30,8 @@ Original Author: Shay Gal-on
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// SPDX-License-Identifier:Apache-2.0 WITH SHL-2.0
 
 #include "coremark.h"
 
