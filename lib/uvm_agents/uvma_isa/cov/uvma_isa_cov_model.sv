c9490f6c4a93139eed4ff591d3153326b3d0ce95
Robin Pedersen <Robin.Pedersen@silabs.com>
2021-03-29 09:21:31 +0200

    add spdx license ids
    Signed-off-by: Robin Pedersen <Robin.Pedersen@silabs.com>

--- lib/uvm_agents/uvma_isa/cov/uvma_isa_cov_model.sv
+++ lib/uvm_agents/uvma_isa/cov/uvma_isa_cov_model.sv
@@ -12,6 +12,8 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0
 
 
 covergroup cg_rtype(string name) with function sample (instr_c instr);
