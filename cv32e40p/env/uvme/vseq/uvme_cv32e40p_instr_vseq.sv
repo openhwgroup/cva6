d6b5112af7ab9273a18a2b234a6dffc220ab4b9c
Mike Thompson <mike@openhwgroup.org>
2021-05-26 15:30:33 -0400

    Hackathon with DP
    Signed-off-by: Mike Thompson <mike@openhwgroup.org>

--- cv32e40p/env/uvme/vseq/uvme_cv32e40p_instr_vseq.sv
+++ cv32e40p/env/uvme/vseq/uvme_cv32e40p_instr_vseq.sv
@@ -73,10 +73,10 @@ task uvme_cv32e40p_instr_vseq_c::body();
    
    forever begin
       // Wait for the monitor to send us the mstr's "req" with an access request
-      p_sequencer.obi_memory_instr_sqr.mon_trn_fifo.get(mon_trn);
+      p_sequencer.obi_memory_sqr.mon_trn_fifo.get(mon_trn);
       `uvm_info("OBI_MEMORY_SLV_SEQ", $sformatf("Got mon_trn:\n%s", mon_trn.sprint()), UVM_HIGH)
       
-      error  = mon_trn.__error;
+      error  = mon_trn.err;
       error |= (mon_trn.address > (2**8));
       
       `uvm_create(slv_rsp)
@@ -109,7 +109,9 @@ task uvme_cv32e40p_instr_vseq_c::body();
          end
       end
       
-      slv_rsp.start(p_sequencer.obi_memory_sequencer);
+      //slv_rsp.start(p_sequencer.obi_memory_sequencer);
+      slv_rsp.set_sequencer(p_sequencer.obi_memory_sequencer);
+      `uvm_send(slv_rsp)
    end
    
 endtask : body
