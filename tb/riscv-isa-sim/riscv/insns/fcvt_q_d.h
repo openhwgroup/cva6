8f68ae32c6f577ee962d486a1bdd92fab2a7feec
Luca Valente <luca.valente2@unibo.it>
2021-05-27 12:57:39 +0200

    Removed original folders

--- tb/riscv-isa-sim/riscv/insns/fcvt_q_d.h
+++
@@ -1,5 +0,0 @@
-require_extension('Q');
-require_fp;
-softfloat_roundingMode = RM;
-WRITE_FRD(f64_to_f128(f64(FRS1)));
-set_fp_exceptions;
--- tb/riscv-isa-sim/riscv/insns/fsqrt_q.h
+++
@@ -1,5 +0,0 @@
-require_extension('Q');
-require_fp;
-softfloat_roundingMode = RM;
-WRITE_FRD(f128_sqrt(f128(FRS1)));
-set_fp_exceptions;
