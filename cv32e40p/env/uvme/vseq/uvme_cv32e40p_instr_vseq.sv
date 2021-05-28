25beeef982007027942e56e9360276f13f03f667
David Poulin <dpoulin@datumtc.ca>
2021-05-27 20:45:14 -0400

    Compilation error fixes and better error handling. From Mike Thompson.
    Signed-off-by: David Poulin <dpoulin@datumtc.ca>

--- cv32e40p/env/uvme/vseq/uvme_cv32e40p_instr_vseq.sv
+++ cv32e40p/env/uvme/vseq/uvme_cv32e40p_instr_vseq.sv
@@ -67,13 +67,23 @@ task uvme_cv32e40p_instr_vseq_c::body();
    uvma_obi_memory_mon_trn_c       mon_trn;
    uvma_obi_memory_slv_seq_item_c  slv_rsp;
    bit                             error = 0;
+   integer                         file;
+   integer                         errno;
+   string                          error_dscr;
    
-   `uvm_info("OBI_MEMORY_SLV_SEQ", $sformatf("Loading memory contents from %s", mem_contents_location), UVM_LOW)
-   $readmemh(mem_contents_location, cntxt.mem);
+   file = $fopen(mem_contents_location, "r");
+   errno = $ferror(file, error_dscr);
+   if (errno != 0) begin
+      `uvm_warning("OBI_MEMORY_SLV_SEQ", $sformatf("Cannot open %s for reading (error description: %s).", mem_contents_location, error_dscr))
+   end
+   else begin 
+      `uvm_info("OBI_MEMORY_SLV_SEQ", $sformatf("Loading memory contents from %s", mem_contents_location), UVM_LOW)
+      $readmemh(mem_contents_location, cntxt.mem);
+   end
    
    forever begin
       // Wait for the monitor to send us the mstr's "req" with an access request
-      p_sequencer.obi_memory_sqr.mon_trn_fifo.get(mon_trn);
+      p_sequencer.obi_memory_instr_sequencer.mon_trn_fifo.get(mon_trn);
       `uvm_info("OBI_MEMORY_SLV_SEQ", $sformatf("Got mon_trn:\n%s", mon_trn.sprint()), UVM_HIGH)
       
       error  = mon_trn.err;
@@ -110,9 +120,9 @@ task uvme_cv32e40p_instr_vseq_c::body();
       end
       
       //slv_rsp.start(p_sequencer.obi_memory_instr_sqr);
-      slv_rsp.set_sequencer(p_sequencer.obi_memory_instr_sqr);
+      slv_rsp.set_sequencer(p_sequencer.obi_memory_instr_sequencer);
       `uvm_send(slv_rsp)
-   end
+   end //forever
    
 endtask : body
 
