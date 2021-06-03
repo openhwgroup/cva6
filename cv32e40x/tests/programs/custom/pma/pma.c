94c5455769875e6028bd7c950a78a586556a3364
Robin Pedersen <Robin.Pedersen@silabs.com>
2021-06-03 15:53:44 +0200

    write pma test for exec in io region
    Signed-off-by: Robin Pedersen <Robin.Pedersen@silabs.com>

--- cv32e40x/tests/programs/custom/pma/pma.c
+++ cv32e40x/tests/programs/custom/pma/pma.c
@@ -18,17 +18,40 @@
 // TODO should license be just Apache, or Solderpad?
 
 #include <stdio.h>
+#include <stdlib.h>
+
+#define  EXCEPTION_INSN_ACCESS_FAULT  1
+
+static volatile int iaf_occured = 0;
+
+void u_sw_irq_handler(void) {  // overrides a "weak" symbol in the bsp
+  unsigned int mcause = -1;
+
+  __asm__ volatile("csrr %0, mcause" : "=r"(mcause));
+
+  if (mcause == EXCEPTION_INSN_ACCESS_FAULT) {
+    // TODO check "iaf_expected"?
+    iaf_occured = 1;
+    printf("in exception handler for instruction access fault\n");
+    return;  // should continue test, assuming no intermediary ABI function call
+  } else {
+    printf("error: pma test unexpected mcause value\n");
+    while (1)
+      ;
+  }
+}
 
 int main(void) {
-  printf("\n");
-  printf("Hello, PMA test!\n");
-  printf("\n");
+  printf("\nHello, PMA test!\n\n");
 
-  void (*pma_fault_function)(void) = (void (*)(void))0x10000;
+  void (*pma_fault_function)(void) = (void (*)(void))0x10000;  // TODO magicnum
+  iaf_occured = 0;
   pma_fault_function();
+  if (!iaf_occured) {
+    printf("error: pma test expected instruction access fault\n");
+    return EXIT_FAILURE;
+  }
 
-  printf("\n");
-  printf("Goodbye, PMA test!\n");
-  printf("\n");
-  return 0;
+  printf("\nGoodbye, PMA test!\n\n");
+  return EXIT_SUCCESS;
 }
