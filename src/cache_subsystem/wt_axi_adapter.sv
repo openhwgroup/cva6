3df6fe7be1912a5c0328c865f970cc308b6f21bd
Emeric Poulin <emeric.poulin@thalesgroup.com>
2021-06-25 12:01:49 +0200

    Fix cache_inval_t to pass Travis
    Signed-off-by: Emeric Poulin <emeric.poulin@thalesgroup.com>

--- src/cache_subsystem/wt_axi_adapter.sv
+++ src/cache_subsystem/wt_axi_adapter.sv
@@ -356,7 +356,7 @@ module wt_axi_adapter import ariane_pkg::*; import wt_cache_pkg::*; #(
   logic [ICACHE_LINE_WIDTH/64-1:0][63:0] icache_rd_shift_d, icache_rd_shift_q;
   logic [DCACHE_LINE_WIDTH/64-1:0][63:0] dcache_rd_shift_d, dcache_rd_shift_q;
   wt_cache_pkg::dcache_in_t dcache_rtrn_type_d, dcache_rtrn_type_q;
-  wt_cache_pkg::cache_inval_t dcache_rtrn_inv_d, dcache_rtrn_inv_q;
+  wt_cache_pkg::dcache_inval_t dcache_rtrn_inv_d, dcache_rtrn_inv_q;
   logic dcache_sc_rtrn, axi_rd_last;
 
   always_comb begin : p_axi_rtrn_shift
