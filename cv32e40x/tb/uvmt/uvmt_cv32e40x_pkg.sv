cef38f268590bbe30506436675df8d975c833385
Steve Richmond <Steve.Richmond@silabs.com>
2021-07-15 10:38:59 -0500

    move the ovpsim mem variable configuration to an HDL path
    Signed-off-by: Steve Richmond <Steve.Richmond@silabs.com>

--- cv32e40x/tb/uvmt/uvmt_cv32e40x_pkg.sv
+++ cv32e40x/tb/uvmt/uvmt_cv32e40x_pkg.sv
@@ -38,7 +38,8 @@ package uvmt_cv32e40x_pkg;
    import uvme_cv32e40x_pkg::*;
    import uvml_hrtbt_pkg::*;
    import uvml_logs_pkg::*;
-   //import uvma_debug_pkg::*;
+   import uvma_rvvi_ovpsim_pkg::*;
+   
    
    // Constants / Parameters / Structs / Enums
    `include "uvmt_cv32e40x_constants.sv"
