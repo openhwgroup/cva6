054289ed269dd6146cc9206282df4e78ca4c2c4f
Luca Valente <luca.valente2@unibo.it>
2021-07-19 15:44:42 +0200

    Revert "Update boot-rom fpga."
    This reverts commit 4b152ca26eb61b83c425a152ba3bb26a36aa15bf.

--- hardware/fpga/run.tcl
+++ hardware/fpga/run.tcl
@@ -3,7 +3,7 @@ create_project alsaqr /scratch/lvalente/project/arianissimo2/cva6/hardware/fpga/
 source ./alsaqr/tcl/generated/compile.tcl
 set_property top alsaqr_xilinx [current_fileset]
 read_ip ./alsaqr/tcl/ips/clk_mngr/ip/xilinx_clk_mngr.xci
-read_ip ./alsaqr/tcl/ips/boot_rom/ip/xilinx_rom_bank_1024x64.xci
+read_ip ./alsaqr/tcl/ips/boot_rom/ip/xilinx_rom_bank_256x64.xci
 add_files -fileset constrs_1 -norecurse "alsaqr/tcl/fmc_board_$::env(BOARD).xdc"
 update_compile_order -fileset sources_1
 auto_detect_xpm
@@ -13,5 +13,4 @@ update_compile_order -fileset sources_1
 launch_runs impl_1 -to_step write_bitstream -jobs 64
 wait_on_run impl_1
 report_clocks
-report_utilization > util.rpt
-report_timing > timing.rpt
+report_utilization
