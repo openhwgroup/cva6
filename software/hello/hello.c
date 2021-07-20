e05860752f0f0cb4facc471e83c1a426ef876849
Luca Valente <luca.valente2@unibo.it>
2021-07-20 13:39:44 +0200

    [fpga] Hello! uart sw fix.
    The apb_uart module has a 16x clock divider. Hence, @ 10MHz
    9600 is the only baudrate that can correctly work.

--- software/hello/hello.c
+++ software/hello/hello.c
@@ -2,14 +2,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "utils.h"
+//#define FPGA_EMULATION
 
 int main(int argc, char const *argv[]) {
 
+  #ifdef FPGA_EMULATION
+  int baud_rate = 9600;
+  int test_freq = 10000000;
+  #else
   int baud_rate = 115200;
-  int test_freq = 50000000; 
-  uart_set_cfg(0,(test_freq/baud_rate)/16);
+  int test_freq = 50000000;
+  #endif  
+  uart_set_cfg(0,(test_freq/baud_rate)>>4);
   printf("Hello CVA6!\n");
   uart_wait_tx_done();
   return 0;
 }
  
+
+
