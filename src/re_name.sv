5fd9bec35498bf6c981c06f99005d48ff65c2539
Florian Zaruba <florian@openhwgroup.org>
2021-07-20 14:36:09 +0200

    Update re-namer license (#695)
    * Update re-namer license
    
    * Update src/re_name.sv

--- src/re_name.sv
+++ src/re_name.sv
@@ -1,23 +1,13 @@
+// Copyright (C) 2017 ETH Zurich, University of Bologna
+//
+// Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0
+// You may obtain a copy of the License at https://solderpad.org/licenses/
+//
 // Author: Florian Zaruba, ETH Zurich
 // Date: 03.10.2017
 // Description: Re-name registers
-//
-//
-// Copyright (C) 2017 ETH Zurich, University of Bologna
-// All rights reserved.
-//
-// This code is under development and not yet released to the public.
-// Until it is released, the code is under the copyright of ETH Zurich and
-// the University of Bologna, and may contain confidential and/or unpublished
-// work. Any reuse/redistribution is strictly forbidden without written
-// permission from ETH Zurich.
-//
-// Bug fixes and contributions will eventually be released under the
-// SolderPad open hardware license in the context of the PULP platform
-// (http://www.pulp-platform.org), under the copyright of ETH Zurich and the
-// University of Bologna.
-//
-
 
 module re_name import ariane_pkg::*; (
     input  logic                                   clk_i,    // Clock
