31c9cb4bd60f62d3809baceb0a5f34b66123cd7a
André Sintzoff <andre.sintzoff@thalesgroup.com>
2021-09-03 08:37:52 +0200

    cva6_tb.sv: stop using ariane_wrapper module
    Signed-off-by: André Sintzoff <andre.sintzoff@thalesgroup.com>

--- cva6/tb/core/cva6_tb.sv
+++ cva6/tb/core/cva6_tb.sv
@@ -51,18 +51,20 @@ module cva6_core_only_tb #(
   // Core:
   //      The AXI REQ and RESP ports are implemented as structs which are
   //      defined in two packages:
-  //            - package "ariane_axi_soc" defined in tb/ariane_axi_soc_pkg.sv
-  //            - package "axi_pkg"        defined in src/axi/src/axi_pkg.sv
+  //            - package "ariane_axi" defined in core/include/ariane_axi_pkg.sv
+  //            - package "axi_pkg"    defined in corev_apu/axi/src/axi_pkg.sv
   //      TODO: extract these package definitions into a single file that is
   //            completely independ of the RTL.
   //----------------------------------------------------------------------------
-  ariane_axi_soc::req_t    axi_ariane_req;
-  ariane_axi_soc::resp_t   axi_ariane_resp;
-  rvfi_pkg::rvfi_port_t    rvfi;
+  ariane_axi::req_t    axi_ariane_req;
+  ariane_axi::resp_t   axi_ariane_resp;
 
-  ariane_wrapper #(
+  typedef rvfi_pkg::rvfi_instr_t [ariane_pkg::NR_COMMIT_PORTS-1:0] rvfi_port_t;
+  rvfi_port_t    rvfi;
+
+  ariane #(
     .ArianeCfg  ( ariane_soc::ArianeSocCfg )
-  ) i_cva6_wrapper (
+  ) i_cva6 (
     .clk_i                ( tb_clk                    ),
     .rst_ni               ( tb_rstn                   ),
     .boot_addr_i          ( 64'h0000_0000_8000_0000   ), //ariane_soc::ROMBase
@@ -71,95 +73,9 @@ module cva6_core_only_tb #(
     .ipi_i                ( 1'b0  /*ipi*/             ),
     .time_irq_i           ( 1'b0  /*timer_irq*/       ),
     .debug_req_i          ( 1'b0                      ),
-
-    .rvfi_valid_1         ( rvfi[1].valid             ),
-    .rvfi_order_1         ( rvfi[1].order             ),
-    .rvfi_insn_1          ( rvfi[1].insn              ),
-    .rvfi_trap_1          ( rvfi[1].trap              ),
-    .rvfi_halt_1          ( rvfi[1].halt              ),
-    .rvfi_intr_1          ( rvfi[1].intr              ),
-    .rvfi_mode_1          ( rvfi[1].mode              ),
-    .rvfi_ixl_1           ( rvfi[1].ixl               ),
-    .rvfi_rs1_addr_1      ( rvfi[1].rs1_addr          ),
-    .rvfi_rs2_addr_1      ( rvfi[1].rs2_addr          ),
-    .rvfi_rs1_rdata_1     ( rvfi[1].rs1_rdata         ),
-    .rvfi_rs2_rdata_1     ( rvfi[1].rs2_rdata         ),
-    .rvfi_rd_addr_1       ( rvfi[1].rd_addr           ),
-    .rvfi_rd_wdata_1      ( rvfi[1].rd_wdata          ),
-    .rvfi_pc_rdata_1      ( rvfi[1].pc_rdata          ),
-    .rvfi_pc_wdata_1      ( rvfi[1].pc_wdata          ),
-    .rvfi_mem_addr_1      ( rvfi[1].mem_addr          ),
-    .rvfi_mem_rmask_1     ( rvfi[1].mem_rmask         ),
-    .rvfi_mem_wmask_1     ( rvfi[1].mem_wmask         ),
-    .rvfi_mem_rdata_1     ( rvfi[1].mem_rdata         ),
-    .rvfi_mem_wdata_1     ( rvfi[1].mem_wdata         ),
-    .rvfi_valid_0         ( rvfi[0].valid             ),
-    .rvfi_order_0         ( rvfi[0].order             ),
-    .rvfi_insn_0          ( rvfi[0].insn              ),
-    .rvfi_trap_0          ( rvfi[0].trap              ),
-    .rvfi_halt_0          ( rvfi[0].halt              ),
-    .rvfi_intr_0          ( rvfi[0].intr              ),
-    .rvfi_mode_0          ( rvfi[0].mode              ),
-    .rvfi_ixl_0           ( rvfi[0].ixl               ),
-    .rvfi_rs1_addr_0      ( rvfi[0].rs1_addr          ),
-    .rvfi_rs2_addr_0      ( rvfi[0].rs2_addr          ),
-    .rvfi_rs1_rdata_0     ( rvfi[0].rs1_rdata         ),
-    .rvfi_rs2_rdata_0     ( rvfi[0].rs2_rdata         ),
-    .rvfi_rd_addr_0       ( rvfi[0].rd_addr           ),
-    .rvfi_rd_wdata_0      ( rvfi[0].rd_wdata          ),
-    .rvfi_pc_rdata_0      ( rvfi[0].pc_rdata          ),
-    .rvfi_pc_wdata_0      ( rvfi[0].pc_wdata          ),
-    .rvfi_mem_addr_0      ( rvfi[0].mem_addr          ),
-    .rvfi_mem_rmask_0     ( rvfi[0].mem_rmask         ),
-    .rvfi_mem_wmask_0     ( rvfi[0].mem_wmask         ),
-    .rvfi_mem_rdata_0     ( rvfi[0].mem_rdata         ),
-    .rvfi_mem_wdata_0     ( rvfi[0].mem_wdata         ),
-
-    .aw_id_o              ( axi_ariane_req.aw.id     ),
-    .aw_addr_o            ( axi_ariane_req.aw.addr   ),
-    .aw_len_o             ( axi_ariane_req.aw.len    ),
-    .aw_size_o            ( axi_ariane_req.aw.size   ),
-    .aw_burst_o           ( axi_ariane_req.aw.burst  ),
-    .aw_lock_o            ( axi_ariane_req.aw.lock   ),
-    .aw_cache_o           ( axi_ariane_req.aw.cache  ),
-    .aw_prot_o            ( axi_ariane_req.aw.prot   ),
-    .aw_qos_o             ( axi_ariane_req.aw.qos    ),
-    .aw_region_o          ( axi_ariane_req.aw.region ),
-    .aw_atop_o            ( axi_ariane_req.aw.atop   ),
-    .aw_user_o            ( axi_ariane_req.aw.user   ),
-    .aw_valid_o           ( axi_ariane_req.aw_valid  ),
-    .w_data_o             ( axi_ariane_req.w.data    ),
-    .w_strb_o             ( axi_ariane_req.w.strb    ),
-    .w_last_o             ( axi_ariane_req.w.last    ),
-    .w_user_o             ( axi_ariane_req.w.user    ),
-    .w_valid_o            ( axi_ariane_req.w_valid   ),
-    .b_ready_o            ( axi_ariane_req.b_ready   ),
-    .ar_id_o              ( axi_ariane_req.ar.id     ),
-    .ar_addr_o            ( axi_ariane_req.ar.addr   ),
-    .ar_len_o             ( axi_ariane_req.ar.len    ),
-    .ar_size_o            ( axi_ariane_req.ar.size   ),
-    .ar_burst_o           ( axi_ariane_req.ar.burst  ),
-    .ar_lock_o            ( axi_ariane_req.ar.lock   ),
-    .ar_cache_o           ( axi_ariane_req.ar.cache  ),
-    .ar_prot_o            ( axi_ariane_req.ar.prot   ),
-    .ar_qos_o             ( axi_ariane_req.ar.qos    ),
-    .ar_region_o          ( axi_ariane_req.ar.region ),
-    .ar_user_o            ( axi_ariane_req.ar.user   ),
-    .ar_valid_o           ( axi_ariane_req.ar_valid  ),
-    .r_ready_o            ( axi_ariane_req.r_ready   ),
-    .aw_ready_i           ( axi_ariane_resp.aw_ready ),
-    .ar_ready_i           ( axi_ariane_resp.ar_ready ),
-    .w_ready_i            ( axi_ariane_resp.w_ready  ),
-    .b_valid_i            ( axi_ariane_resp.b_valid  ),
-    .b_id_i               ( axi_ariane_resp.b.id     ),
-    .b_resp_i             ( axi_ariane_resp.b.resp   ),
-    .b_user_i             ( axi_ariane_resp.b.user   ),
-    .r_valid_i            ( axi_ariane_resp.r_valid  ),
-    .r_id_i               ( axi_ariane_resp.r.id     ),
-    .r_data_i             ( axi_ariane_resp.r.data   ),
-    .r_resp_i             ( axi_ariane_resp.r.resp   ),
-    .r_last_i             ( axi_ariane_resp.r.last   ),
-    .r_user_i             ( axi_ariane_resp.r.user   )
+    .rvfi_o               ( rvfi                      ),
+    .axi_req_o            ( axi_ariane_req            ),
+    .axi_resp_i           ( axi_ariane_resp           )
   );
 
   //----------------------------------------------------------------------------
@@ -170,7 +86,8 @@ module cva6_core_only_tb #(
     .SIM_FINISH(2000000),
     .HART_ID(8'h0),
     .DEBUG_START(0),
-    .DEBUG_STOP(0)
+    .DEBUG_STOP(0),
+    .NR_COMMIT_PORTS(ariane_pkg::NR_COMMIT_PORTS)
   ) rvfi_tracer_i (
     .clk_i(tb_clk),
     .rst_ni(tb_rstn),
