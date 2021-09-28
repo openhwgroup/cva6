d5299235d3b4cdd7a5cf9c884061401112cc6d1f
Robin Pedersen <Robin.Pedersen@silabs.com>
2021-09-28 11:20:52 +0200

    connect exceptions_covg to rvfi_agent instr port
    Signed-off-by: Robin Pedersen <Robin.Pedersen@silabs.com>

--- cv32e40x/env/uvme/cov/uvme_exceptions_covg.sv
+++ cv32e40x/env/uvme/cov/uvme_exceptions_covg.sv
@@ -16,11 +16,17 @@
 // SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0
 
 
+`uvm_analysis_imp_decl(_rvfi)
+
+
 class uvme_exceptions_covg extends uvm_component;
+
+  uvm_analysis_imp_rvfi#(uvma_rvfi_instr_seq_item_c#(ILEN, XLEN), uvme_exceptions_covg) rvfi_mon_export;
 
   `uvm_component_utils(uvme_exceptions_covg);
 
   extern function new(string name = "exceptions_covg", uvm_component parent = null);
+  extern function void write_rvfi(uvma_rvfi_instr_seq_item_c#(ILEN, XLEN) trn);
 
 endclass : uvme_exceptions_covg
 
@@ -29,4 +35,13 @@ function uvme_exceptions_covg::new(string name = "exceptions_covg", uvm_componen
 
   super.new(name, parent);
 
+  rvfi_mon_export = new("rvfi_mon_export", this);
+
 endfunction : new
+
+
+function void uvme_exceptions_covg::write_rvfi(uvma_rvfi_instr_seq_item_c#(ILEN, XLEN) trn);
+
+  $display("TODO WRITE RVFI HAPPENED");
+
+endfunction : write_rvfi
