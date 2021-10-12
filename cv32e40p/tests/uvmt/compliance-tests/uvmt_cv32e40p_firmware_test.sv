09db9403fea3ea6b61a22620de7a1e943d4fdb7b
Steve Richmond <Steve.Richmond@silabs.com>
2021-10-12 16:35:51 -0500

    lint checks: avoid method override that only calls super
    Signed-off-by: Steve Richmond <Steve.Richmond@silabs.com>

--- cv32e40p/tests/uvmt/compliance-tests/uvmt_cv32e40p_firmware_test.sv
+++ cv32e40p/tests/uvmt/compliance-tests/uvmt_cv32e40p_firmware_test.sv
@@ -2,19 +2,19 @@
 // Copyright 2020 OpenHW Group
 // Copyright 2020 Datum Technology Corporation
 // Copyright 2020 Silicon Labs, Inc.
-// 
+//
 // Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     https://solderpad.org/licenses/
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-// 
+//
 
 
 
@@ -23,7 +23,7 @@
 
 
 /**
- *  CV32E40PE40P "firmware" test.  
+ *  CV32E40PE40P "firmware" test.
  *  This class relies on a pre-existing "firmware" file written in C and/or
  *  RISC-V assembly code.  This class will invoke the riscv-gcc-toolchain to
  *  translate the firmware into a "hexfile" that is read into the CV32E40PE40P
@@ -33,7 +33,7 @@
  *
  */
 class uvmt_cv32e40p_firmware_test_c extends uvmt_cv32e40p_base_test_c;
-   
+
    //constraint env_cfg_cons {
    //   env_cfg.enabled         == 1;
    //   env_cfg.is_active       == UVM_ACTIVE;
@@ -43,24 +43,14 @@ class uvmt_cv32e40p_firmware_test_c extends uvmt_cv32e40p_base_test_c;
    constraint test_type_cons {
      test_cfg.tpt == PREEXISTING_SELFCHECKING;
    }
-   
-   
+
+
    `uvm_component_utils(uvmt_cv32e40p_firmware_test_c)
-   
+
    /**
     */
    extern function new(string name="uvmt_cv32e40p_firmware_test", uvm_component parent=null);
-   
-   /**
-    * Runs reset_vseq.
-    */
-   extern virtual task reset_phase(uvm_phase phase);
-   
-   /**
-    * Loads the test program (the "firmware") into memory.
-    */
-   extern virtual task configure_phase(uvm_phase phase);
-   
+
    /**
     *  Enable program execution, wait for completion.
     */
@@ -88,54 +78,17 @@ endclass : uvmt_cv32e40p_firmware_test_c
 
 
 function uvmt_cv32e40p_firmware_test_c::new(string name="uvmt_cv32e40p_firmware_test", uvm_component parent=null);
-   
+
    super.new(name, parent);
    `uvm_info("TEST", "This is the FIRMWARE TEST", UVM_NONE)
-   
+
 endfunction : new
 
-
-task uvmt_cv32e40p_firmware_test_c::reset_phase(uvm_phase phase);
-   super.reset_phase(phase);
-  
-endtask : reset_phase
-
-
-task uvmt_cv32e40p_firmware_test_c::configure_phase(uvm_phase phase);
-   
-   //string firmware;
-   //int    fd;
-   
-   super.configure_phase(phase);
-
-   /*
-   ** Moved to uvmt_cv32e40p_dut_wrap.sv to avoid XMRs across packages.
-   ** TODO: delete all this once you are confident of the approach.
-   **
-    // Load the pre-compiled firmware
-    if($value$plusargs("firmware=%s", firmware)) begin
-      // First, check if it exists...
-      fd = $fopen (firmware, "r");   
-      if (fd)  `uvm_info("TEST", $sformatf("%s was opened successfully : (fd=%0d)", firmware, fd), UVM_NONE)
-      else     `uvm_fatal("TEST", $sformatf("%s was NOT opened successfully : (fd=%0d)", firmware, fd))
-      $fclose(fd);
-      // Now load it...
-      `uvm_info("TEST", $sformatf("loading firmware %0s", firmware), UVM_NONE)
-      $readmemh(firmware, uvmt_cv32e40p_tb.dut_wrap.ram_i.dp_ram_i.mem);
-    end
-    else begin
-      `uvm_error("TEST", "No firmware specified!")
-    end
-   */
-
-endtask : configure_phase
-
-
 task uvmt_cv32e40p_firmware_test_c::run_phase(uvm_phase phase);
-   
+
    // start_clk() and watchdog_timer() are called in the base_test
    super.run_phase(phase);
-   
+
    if ($test$plusargs("gen_random_debug")) begin
     fork
       random_debug();
@@ -143,7 +96,7 @@ task uvmt_cv32e40p_firmware_test_c::run_phase(uvm_phase phase);
    end
 
    if ($test$plusargs("gen_irq_noise")) begin
-    fork    
+    fork
       irq_noise();
     join_none
    end
@@ -180,7 +133,7 @@ task uvmt_cv32e40p_firmware_test_c::run_phase(uvm_phase phase);
    //TODO: exit_value will not be valid - need to add a latch in the vp_status_vif
    `uvm_info("TEST", $sformatf("Finished RUN: exit status is %0h", vp_status_vif.exit_value), UVM_NONE)
    phase.drop_objection(this);
-   
+
 endtask : run_phase
 
 task uvmt_cv32e40p_firmware_test_c::reset_debug();
@@ -202,7 +155,7 @@ task uvmt_cv32e40p_firmware_test_c::bootset_debug();
 
     // Delay debug_req_i by up to 35 cycles.Should hit BOOT_SET
     repeat($urandom_range(35,1)) @(posedge env_cntxt.clknrst_cntxt.vif.clk);
-  
+
     void'(debug_vseq.randomize());
     debug_vseq.start(vsequencer);
 
@@ -253,14 +206,14 @@ task uvmt_cv32e40p_firmware_test_c::random_fetch_toggle();
     core_cntrl_vif.stop_fetch();
 
     // Randomly dessert for a random number of cycles
-    randcase    
+    randcase
       3: fetch_deassert_cycles = $urandom_range(100, 1);
       1: fetch_deassert_cycles = $urandom_range(3, 1);
     endcase
     repeat (fetch_deassert_cycles) @(core_cntrl_vif.drv_cb);
     core_cntrl_vif.go_fetch();
   end
-  
+
 endtask : random_fetch_toggle
 
 `endif // __UVMT_CV32E40P_FIRMWARE_TEST_SV__
