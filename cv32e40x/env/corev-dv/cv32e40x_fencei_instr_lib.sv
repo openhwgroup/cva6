b5c0a7182d47f36ec41709a117bb370e40e69c0e
Robin Pedersen <Robin.Pedersen@silabs.com>
2021-10-14 11:17:02 +0200

    begin new instr stream for fencei
    Signed-off-by: Robin Pedersen <Robin.Pedersen@silabs.com>

---
+++ cv32e40x/env/corev-dv/cv32e40x_fencei_instr_lib.sv
@@ -0,0 +1,27 @@
+// Copyright 2021 OpenHW Group
+// Copyright 2021 Silicon Labs, Inc.
+//
+// Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://solderpad.org/licenses/
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0
+
+
+class corev_store_fencei_load_instr_stream extends riscv_load_store_rand_instr_stream;
+
+  `uvm_object_utils(corev_store_fencei_load_instr_stream)
+
+  function new(string name = "");
+    super.new(name);
+  endfunction : new
+
+endclass : corev_store_fencei_load_instr_stream
