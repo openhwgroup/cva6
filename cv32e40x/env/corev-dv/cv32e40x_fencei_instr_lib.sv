9a9ae223e1e327d7efeb1622f5137f2ff5169e74
Robin Pedersen <Robin.Pedersen@silabs.com>
2021-10-14 15:28:30 +0200

    add store_fencei_load instr stream
    Signed-off-by: Robin Pedersen <Robin.Pedersen@silabs.com>

--- cv32e40x/env/corev-dv/cv32e40x_fencei_instr_lib.sv
+++ cv32e40x/env/corev-dv/cv32e40x_fencei_instr_lib.sv
@@ -18,10 +18,44 @@
 
 class corev_store_fencei_load_instr_stream extends riscv_load_store_rand_instr_stream;
 
+  rand riscv_reg_t addr_reg;
+  rand bit [31:0]  addr;
+
   `uvm_object_utils(corev_store_fencei_load_instr_stream)
 
   function new(string name = "");
     super.new(name);
   endfunction : new
+
+  function void post_randomize();
+    riscv_instr instr;
+
+    // Store
+    instr = riscv_instr::get_rand_instr(.include_instr({SW, SH, SB}));
+    `DV_CHECK_RANDOMIZE_WITH_FATAL(instr,
+      instr_name inside {SW, SH, SB};
+      rs1 == addr_reg;
+      imm == addr[31:20];
+      , "TODO"
+    )
+    instr.comment = "store_fencei_load: store";
+    insert_instr(instr, 0);
+
+    // Fence.i
+    instr = riscv_instr::get_instr(FENCE_I);
+    instr.comment = "store_fencei_load: fencei";
+    insert_instr(instr, 1);
+
+    // Load
+    instr = riscv_instr::get_rand_instr(.include_instr({LW, LH, LHU, LB, LBU}));
+    `DV_CHECK_RANDOMIZE_WITH_FATAL(instr,
+      instr_name inside {LW, LH, LHU, LB, LBU};
+      rs1 == addr_reg;
+      imm == addr[31:20];
+      , "TODO"
+    )
+    instr.comment = "store_fencei_load: load";
+    insert_instr(instr, 2);
+  endfunction : post_randomize
 
 endclass : corev_store_fencei_load_instr_stream
