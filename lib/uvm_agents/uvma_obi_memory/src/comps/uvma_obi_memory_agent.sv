13284922c026cfe5560277ba1a85e203f96d01ff
dpoulin <dpoulin@localhost>
2021-10-14 16:20:40 -0400

    Changes for #928

--- lib/uvm_agents/uvma_obi_memory/src/comps/uvma_obi_memory_agent.sv
+++ lib/uvm_agents/uvma_obi_memory/src/comps/uvma_obi_memory_agent.sv
@@ -157,7 +157,7 @@ endfunction: connect_phase
 function void uvma_obi_memory_agent_c::get_and_set_cfg();
    
    void'(uvm_config_db#(uvma_obi_memory_cfg_c)::get(this, "", "cfg", cfg));
-   if (!cfg) begin
+   if (cfg == null) begin
       `uvm_fatal("CFG", "Configuration handle is null")
    end
    else begin
@@ -171,7 +171,7 @@ endfunction : get_and_set_cfg
 function void uvma_obi_memory_agent_c::get_and_set_cntxt();
    
    void'(uvm_config_db#(uvma_obi_memory_cntxt_c)::get(this, "", "cntxt", cntxt));
-   if (!cntxt) begin
+   if (cntxt == null) begin
       `uvm_info("CNTXT", "Context handle is null; creating.", UVM_DEBUG)
       cntxt = uvma_obi_memory_cntxt_c::type_id::create("cntxt");
    end
