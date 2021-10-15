016d57c076001d11581b66f3a45e5fd43fd759fd
Robin Pedersen <Robin.Pedersen@silabs.com>
2021-10-15 13:51:45 +0200

    add store_fencei_exec instr stream
    Signed-off-by: Robin Pedersen <Robin.Pedersen@silabs.com>

--- cv32e40x/env/corev-dv/cv32e40x_fencei_instr_lib.sv
+++ cv32e40x/env/corev-dv/cv32e40x_fencei_instr_lib.sv
@@ -39,12 +39,12 @@ class corev_store_fencei_load_instr_stream extends riscv_load_store_rand_instr_s
       , "TODO"
     )
     instr.comment = "store_fencei_load: store";
-    insert_instr(instr, 0);
+    instr_list.push_back(instr);
 
     // Fence.i
     instr = riscv_instr::get_instr(FENCE_I);
     instr.comment = "store_fencei_load: fencei";
-    insert_instr(instr, 1);
+    instr_list.push_back(instr);
 
     // Load
     instr = riscv_instr::get_rand_instr(.include_instr({LW, LH, LHU, LB, LBU}));
@@ -55,7 +55,112 @@ class corev_store_fencei_load_instr_stream extends riscv_load_store_rand_instr_s
       , "TODO"
     )
     instr.comment = "store_fencei_load: load";
-    insert_instr(instr, 2);
+    instr_list.push_back(instr);
   endfunction : post_randomize
 
 endclass : corev_store_fencei_load_instr_stream
+
+
+class corev_store_fencei_exec_instr_stream extends riscv_load_store_rand_instr_stream;
+
+  rand riscv_reg_t addr_reg;
+  rand riscv_reg_t data_reg;
+
+  static int       idx_label;
+
+  `uvm_object_utils(corev_store_fencei_exec_instr_stream)
+
+  function new(string name = "");
+    super.new(name);
+  endfunction : new
+
+  function void post_randomize();
+    riscv_instr        instr;
+    riscv_pseudo_instr pseudo;
+    string             label_exec;
+    string             label_dummy;
+
+    // Calculate labels with right index
+    label_exec = $sformatf("store_fencei_exec__exec_%0d", idx_label);
+    label_dummy = $sformatf("store_fencei_exec__dummy_%0d", idx_label);
+    idx_label++;
+
+    // Load address of dummy instruction
+    pseudo = riscv_pseudo_instr::type_id::create("LA");
+    `DV_CHECK_RANDOMIZE_WITH_FATAL(pseudo,
+      pseudo_instr_name == LA;
+      rd == addr_reg;
+      , "TODO"
+    )
+    pseudo.imm_str = label_dummy;
+    pseudo.comment = "store_fencei_exec: la dummy";
+    instr_list.push_back(pseudo);
+
+    // Load data of dummy instruction
+    instr = riscv_instr::get_instr(LW);
+    `DV_CHECK_RANDOMIZE_WITH_FATAL(instr,
+      instr_name == LW;
+      rs1 == addr_reg;
+      imm == 0;
+      rd == data_reg;
+      , "TODO"
+    )
+    instr.comment = "store_fencei_exec: lw dummy";
+    instr_list.push_back(instr);
+
+    // Load address of exec instruction
+    pseudo = riscv_pseudo_instr::type_id::create("LA");
+    `DV_CHECK_RANDOMIZE_WITH_FATAL(pseudo,
+      pseudo_instr_name == LA;
+      rd == addr_reg;
+      , "TODO"
+    )
+    pseudo.imm_str = label_exec;
+    pseudo.comment = "store_fencei_exec: la exec";
+    instr_list.push_back(pseudo);
+
+    // Store
+    instr = riscv_instr::get_instr(SW);
+    `DV_CHECK_RANDOMIZE_WITH_FATAL(instr,
+      instr_name == SW;
+      rs1 == addr_reg;
+      imm == 0;
+      rs2 == data_reg;
+      , "TODO"
+    )
+    instr.comment = "store_fencei_exec: store";
+    instr_list.push_back(instr);
+
+    // Fencei
+    instr = riscv_instr::get_instr(FENCE_I);
+    instr.comment = "store_fencei_exec: fencei";
+    instr_list.push_back(instr);
+
+    // Exec
+    instr = riscv_instr::get_rand_instr();
+    `DV_CHECK_RANDOMIZE_FATAL(instr, "TODO");
+    instr.comment = "store_fencei_exec: exec";
+    instr.label = label_exec;
+    instr_list.push_back(instr);
+
+    // Dummy, for replacing exec
+    instr = riscv_instr::get_rand_instr(.exclude_category({JUMP, BRANCH}));
+    `DV_CHECK_RANDOMIZE_WITH_FATAL(instr,
+      (category != JUMP);
+      (category != BRANCH);
+      // Note: Could allow JUMP/BRANCH, but just not backwards jumps
+      , "TODO"
+    )
+    instr.comment = "store_fencei_exec: dummy";
+    instr.label = label_dummy;
+    instr_list.push_back(instr);
+
+    foreach (instr_list[i]) begin
+      instr_list[i].atomic = 1;
+      if (instr_list[i].label == "") begin
+        instr_list[i].label = $sformatf("%0d", i);
+      end
+    end
+  endfunction : post_randomize
+
+endclass : corev_store_fencei_exec_instr_stream
