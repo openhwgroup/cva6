e1629f829361d670fc92ac8c423d2ca54f31894d
Robin Pedersen <Robin.Pedersen@silabs.com>
2021-11-04 16:49:23 +0100

    buserr_sb: count d-side err transactions
    Signed-off-by: Robin Pedersen <Robin.Pedersen@silabs.com>

--- cv32e40x/env/uvme/uvme_cv32e40x_buserr_sb.sv
+++ cv32e40x/env/uvme/uvme_cv32e40x_buserr_sb.sv
@@ -26,12 +26,14 @@
 
 class uvme_cv32e40x_buserr_sb_c extends uvm_scoreboard;
 
+  string info_tag = "BUSERRSB";
+
+  int cnt_obid_trn;  // Count of all obi d-side transactions
+  int cnt_obid_err;  // Count of all d-side "err" transactions
+  int cnt_obid_first;  // Count of all first d-side "err", in case of multiple "err" before handler "taken"
+
   uvm_analysis_imp_obid#(uvma_obi_memory_mon_trn_c, uvme_cv32e40x_buserr_sb_c)  obid;
   uvm_analysis_imp_rvfi#(uvma_rvfi_instr_seq_item_c#(ILEN,XLEN), uvme_cv32e40x_buserr_sb_c)  rvfi;
-
-  // TODO:ropeders count "err" transactions on d-side
-  // TODO:ropeders count "first err" transactions on d-side (using flags)
-  // TODO:ropeders count transactions on d-side?
 
   `uvm_component_utils(uvme_cv32e40x_buserr_sb_c)
 
@@ -39,6 +41,7 @@ class uvme_cv32e40x_buserr_sb_c extends uvm_scoreboard;
   extern virtual function void write_obid(uvma_obi_memory_mon_trn_c trn);
   extern virtual function void write_rvfi(uvma_rvfi_instr_seq_item_c#(ILEN,XLEN) trn);
   extern virtual function void build_phase(uvm_phase phase);
+  extern virtual function void check_phase(uvm_phase phase);
 
 endclass : uvme_cv32e40x_buserr_sb_c
 
@@ -52,14 +55,19 @@ endfunction : new
 
 function void uvme_cv32e40x_buserr_sb_c::write_obid(uvma_obi_memory_mon_trn_c trn);
 
-  $display("TODO write_obid");
+  cnt_obid_trn++;
+
+  if (trn.err) begin
+    cnt_obid_err++;
+    // TODO:ropeders store in queue for later comparison?
+  end
+
+  // TODO:ropeders count "first" errs
 
 endfunction : write_obid
 
 
 function void uvme_cv32e40x_buserr_sb_c::write_rvfi(uvma_rvfi_instr_seq_item_c#(ILEN,XLEN) trn);
-
-  $display("TODO write_rvfi");
 
 endfunction : write_rvfi
 
@@ -72,6 +80,19 @@ function void uvme_cv32e40x_buserr_sb_c::build_phase(uvm_phase phase);
   rvfi = new("rvfi", this);
 
 endfunction : build_phase
+
+
+function void uvme_cv32e40x_buserr_sb_c::check_phase(uvm_phase phase);
+
+  super.check_phase(phase);
+
+  assert (cnt_obid_trn > 0) else `uvm_warning(info_tag, "Zero D-side transactions checked");
+  assert (cnt_obid_trn >= cnt_obid_err) else `uvm_error(info_tag, "'err' transactions counted wrong");
+  assert (cnt_obid_err >= cnt_obid_first) else `uvm_error(info_tag, "'first' transactions counted wrong");
+
+  `uvm_info(info_tag, $sformatf("TODO checked %0d D-side 'err' transactions", cnt_obid_err), UVM_NONE)
+
+endfunction : check_phase
 
 
 `endif  // __UVME_CV32E40X_BUSERR_SB_SV__
