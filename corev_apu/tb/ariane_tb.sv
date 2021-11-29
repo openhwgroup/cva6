601329bf7c1ac657f299a23f25d5806645ef6a1a
pawelku <pawelku@aldec.com.pl>
2021-11-29 16:21:45 +0100

    Explicit void type should be used for read_section function.

--- corev_apu/tb/ariane_tb.sv
+++ corev_apu/tb/ariane_tb.sv
@@ -23,7 +23,7 @@ import uvm_pkg::*;
 
 import "DPI-C" function read_elf(input string filename);
 import "DPI-C" function byte get_section(output longint address, output longint len);
-import "DPI-C" context function read_section(input longint address, inout byte buffer[]);
+import "DPI-C" context function void read_section(input longint address, inout byte buffer[]);
 
 module ariane_tb;
 
