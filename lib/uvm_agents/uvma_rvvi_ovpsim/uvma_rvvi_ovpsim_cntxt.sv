17bfbb44d878e1c48b2b13eddf0c05ab2920eabe
Steve Richmond <Steve.Richmond@silabs.com>
2021-12-07 16:57:09 -0600

    fixme/todo cleanup in rvfi and rvvi agents
    Signed-off-by: Steve Richmond <Steve.Richmond@silabs.com>

--- lib/uvm_agents/uvma_rvvi_ovpsim/uvma_rvvi_ovpsim_cntxt.sv
+++ lib/uvm_agents/uvma_rvvi_ovpsim/uvma_rvvi_ovpsim_cntxt.sv
@@ -5,9 +5,9 @@
 // Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     https://solderpad.org/licenses/
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,9 +23,9 @@
  * Object encapsulating all state variables for all Rvvi agent
  * (uvma_rvvi_ovpsim_agent_c) components.
  */
-class uvma_rvvi_ovpsim_cntxt_c#(int ILEN=uvma_rvvi_pkg::DEFAULT_ILEN, 
+class uvma_rvvi_ovpsim_cntxt_c#(int ILEN=uvma_rvvi_pkg::DEFAULT_ILEN,
                                 int XLEN=uvma_rvvi_pkg::DEFAULT_XLEN) extends uvma_rvvi_cntxt_c#(ILEN,XLEN);
-   
+
    virtual RVVI_bus                     ovpsim_bus_vif;
    virtual RVVI_io                      ovpsim_io_vif;
    virtual RVVI_memory                  ovpsim_mem_vif;
@@ -37,27 +37,25 @@ class uvma_rvvi_ovpsim_cntxt_c#(int ILEN=uvma_rvvi_pkg::DEFAULT_ILEN,
     * Builds events.
     */
    extern function new(string name="uvma_rvvi_ovpsim_cntxt");
-   
+
    /**
     * TODO Describe uvma_rvvi_ovpsim_cntxt_c::reset()
     */
    extern function void reset();
-   
+
 endclass : uvma_rvvi_ovpsim_cntxt_c
 
 
 `pragma protect begin
 
 function uvma_rvvi_ovpsim_cntxt_c::new(string name="uvma_rvvi_ovpsim_cntxt");
-   
+
    super.new(name);
-      
+
 endfunction : new
 
 function void uvma_rvvi_ovpsim_cntxt_c::reset();
-   
-   // TODO Implement uvma_rvvi_ovpsim_cntxt_c::reset()
-   
+
 endfunction : reset
 
 
