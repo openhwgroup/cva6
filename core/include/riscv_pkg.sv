cf8d3c451289b2e8f31c690faa6d78a8671037ba
Jean-Roch Coulon <jean-roch.coulon@thalesgroup.com>
2021-12-12 22:04:59 +0100

    rename CVA6ConfigFpuen into CVA6ConfigFpuEn
    Signed-off-by: Jean-Roch Coulon <jean-roch.coulon@thalesgroup.com>

--- core/include/riscv_pkg.sv
+++ core/include/riscv_pkg.sv
@@ -21,7 +21,7 @@ package riscv;
     // Import cva6 config from cva6_config_pkg
     // ----------------------
     localparam XLEN = cva6_config_pkg::CVA6ConfigXlen;
-    localparam FPU_EN = cva6_config_pkg::CVA6ConfigFpuen;
+    localparam FPU_EN = cva6_config_pkg::CVA6ConfigFpuEn;
 
     // ----------------------
     // Data and Address length
