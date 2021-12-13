9d7af487dce5958099de0be196c17a84fcc6290d
Robin Pedersen <Robin.Pedersen@silabs.com>
2021-12-13 16:37:24 +0100

    isacov: reflect that addi16sp has rdrs1 tied to x2
    Signed-off-by: Robin Pedersen <Robin.Pedersen@silabs.com>

--- lib/uvm_agents/uvma_isacov/cov/uvma_isacov_cov_model.sv
+++ lib/uvm_agents/uvma_isacov/cov/uvma_isacov_cov_model.sv
@@ -999,7 +999,8 @@ covergroup cg_ci(
     bit imm_is_signed,
     bit rd_is_signed,
     bit imm_is_nonzero,
-    bit has_rs1
+    bit has_rs1,
+    bit tie_rdrs1_x2
 ) with function sample (
     uvma_isacov_instr_c instr
 );
@@ -1007,10 +1008,10 @@ covergroup cg_ci(
   option.name = name;
 
   cp_rs1_value: coverpoint instr.rs1_value_type {
-    ignore_bins OFF     = cp_rs1_value    with (!has_rs1);
-    ignore_bins POS_OFF = {POSITIVE}      with (!rs1_is_signed);
-    ignore_bins NEG_OFF = {NEGATIVE}      with (!rs1_is_signed);
-    ignore_bins NON_ZERO_OFF = {NON_ZERO} with ( rs1_is_signed);
+    ignore_bins  OFF     = cp_rs1_value    with (!has_rs1);
+    illegal_bins POS_OFF = {POSITIVE}      with (!rs1_is_signed);
+    illegal_bins NEG_OFF = {NEGATIVE}      with (!rs1_is_signed);
+    illegal_bins NON_ZERO_OFF = {NON_ZERO} with ( rs1_is_signed);
   }
 
   cp_imm_value: coverpoint instr.c_imm_value_type {
@@ -1021,14 +1022,14 @@ covergroup cg_ci(
   }
 
   cp_rd_value: coverpoint instr.rd_value_type {
-    ignore_bins POS_OFF = {POSITIVE} with (!rd_is_signed);
-    ignore_bins NEG_OFF = {NEGATIVE} with (!rd_is_signed);
-    ignore_bins NON_ZERO_OFF = {NON_ZERO} with (rd_is_signed);
+    illegal_bins POS_OFF = {POSITIVE} with (!rd_is_signed);
+    illegal_bins NEG_OFF = {NEGATIVE} with (!rd_is_signed);
+    illegal_bins NON_ZERO_OFF = {NON_ZERO} with (rd_is_signed);
   }
 
   cp_rdrs1: coverpoint instr.c_rdrs1 {
-    ignore_bins RD_NOT_ZERO = {0};
-    // TODO rs1_tied_x2
+    illegal_bins RD_NOT_ZERO = {0};
+    illegal_bins NON_X2      = cp_rdrs1 with ((item != 2) && tie_rdrs1_x2);
   }
 
   `ISACOV_CP_BITWISE(cp_rd_toggle, instr.rd_value, 1)
@@ -1942,13 +1943,15 @@ function void uvma_isacov_cov_model_c::build_phase(uvm_phase phase);
                               .imm_is_signed(c_imm_is_signed[C_ADDI]),
                               .rd_is_signed(rd_is_signed[C_ADDI]),
                               .imm_is_nonzero(c_imm_is_nonzero[C_ADDI]),
-                              .has_rs1(c_has_rs1[C_ADDI]));
+                              .has_rs1(c_has_rs1[C_ADDI]),
+                              .tie_rdrs1_x2(0));
       rv32c_addi16sp_cg = new("rv32c_addi16sp_cg",
                               .rs1_is_signed(rs1_is_signed[C_ADDI16SP]),
                               .imm_is_signed(c_imm_is_signed[C_ADDI16SP]),
                               .rd_is_signed(rd_is_signed[C_ADDI16SP]),
                               .imm_is_nonzero(c_imm_is_nonzero[C_ADDI16SP]),
-                              .has_rs1(c_has_rs1[C_ADDI16SP]));
+                              .has_rs1(c_has_rs1[C_ADDI16SP]),
+                              .tie_rdrs1_x2(1));
       rv32c_slli_cg     = new("rv32c_slli_cg",
                               .rs1_is_signed(rs1_is_signed[C_SLLI]),
                               .rd_is_signed(rd_is_signed[C_SLLI]));
@@ -1957,7 +1960,8 @@ function void uvma_isacov_cov_model_c::build_phase(uvm_phase phase);
                               .imm_is_signed (c_imm_is_signed [C_LWSP]),
                               .rd_is_signed  (rd_is_signed    [C_LWSP]),
                               .imm_is_nonzero(c_imm_is_nonzero[C_LWSP]),
-                              .has_rs1       (c_has_rs1       [C_LWSP]));
+                              .has_rs1       (c_has_rs1       [C_LWSP]),
+                              .tie_rdrs1_x2(0));
       rv32c_li_cg       = new("rv32c_li_cg");
       rv32c_lui_cg      = new("rv32c_lui_cg");
 
