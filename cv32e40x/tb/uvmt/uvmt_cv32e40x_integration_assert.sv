6c09c88b71b67ec858c10fd9076f03a4f21d598d
Robin Pedersen <Robin.Pedersen@silabs.com>
2021-12-23 11:52:53 +0100

    integration_assert: finish assert module for core integration checks
    Signed-off-by: Robin Pedersen <Robin.Pedersen@silabs.com>

--- cv32e40x/tb/uvmt/uvmt_cv32e40x_integration_assert.sv
+++ cv32e40x/tb/uvmt/uvmt_cv32e40x_integration_assert.sv
@@ -16,14 +16,22 @@
 // SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0
 
 
+// This module contains assertions relating to the "Core Integration" chapter
+// of the user manual
+
 module uvmt_cv32e40x_integration_assert
   import uvm_pkg::*;
 (
   input clk_i,
   input rst_ni,
 
-  input        fetch_enable_i,
-  input [31:0] dm_halt_addr_i
+  input fetch_enable_i,
+
+  input [31:0] boot_addr_i,
+  input [31:0] dm_exception_addr_i,
+  input [31:0] dm_halt_addr_i,
+  input [31:0] mtvec_addr_i,
+  input [31:0] nmi_addr_i
 );
 
   default clocking cb @(posedge clk_i); endclocking
@@ -38,16 +46,47 @@ module uvmt_cv32e40x_integration_assert
     end
   end
 
-  // Check that "dm_halt_addr_i" is stable after "fetch_enable_i"
-  a_dmhaltaddr_stable : assert property (
-    fetch_enable_i_sticky
-    |->
-    $stable(dm_halt_addr_i)
-    ) else `uvm_error(info_tag, "dm_halt_addr_i changed after fetch_enable_i");
 
-  // Check that "dm_halt_addr_i" is word-aligned
-  a_dmhaltaddr_aligned : assert property (
-    dm_halt_addr_i[1:0] == 2'b00
-    ) else `uvm_error(info_tag, "dm_halt_addr_i not word-aligned");
+  // Check that addresses are stable after "fetch_enable_i"
+
+  property p_stable_addr(addr);
+    fetch_enable_i_sticky |-> $stable(addr);
+  endproperty
+
+  a_stable_bootaddr : assert property (p_stable_addr(boot_addr_i))
+    else `uvm_error(info_tag, "boot_addr_i changed after fetch_enable_i");
+
+  a_stable_dmexceptionaddr : assert property (p_stable_addr(dm_exception_addr_i))
+    else `uvm_error(info_tag, "dm_exception_addr_i changed after fetch_enable_i");
+
+  a_stable_dmhaltaddr : assert property (p_stable_addr(dm_halt_addr_i))
+    else `uvm_error(info_tag, "dm_halt_addr_i changed after fetch_enable_i");
+
+  a_stable_mtvecaddr : assert property (p_stable_addr(mtvec_addr_i))
+    else `uvm_error(info_tag, "mtvec_addr_i changed after fetch_enable_i");
+
+  a_stable_nmiaddr : assert property (p_stable_addr(nmi_addr_i))
+    else `uvm_error(info_tag, "nmi_addr_i changed after fetch_enable_i");
+
+
+  // Check that addresses are word-aligned
+
+  property p_aligned_addr(addr);
+    addr[1:0] == 2'b00;
+  endproperty
+
+  a_aligned_bootaddr : assert property (p_aligned_addr(boot_addr_i))
+    else `uvm_error(info_tag, "boot_addr_i not word-aligned");
+
+  a_aligned_dmexceptionaddr : assert property (p_aligned_addr(dm_exception_addr_i))
+    else `uvm_error(info_tag, "dm_exception_addr_i not word-aligned");
+
+  a_aligned_dmhaltaddr : assert property (p_aligned_addr(dm_halt_addr_i))
+    else `uvm_error(info_tag, "dm_halt_addr_i not word-aligned");
+
+  //a_aligned_mtvecaddr is not required by the user manual as per now
+
+  a_aligned_nmiaddr : assert property (p_aligned_addr(nmi_addr_i))
+    else `uvm_error(info_tag, "nmi_addr_i not word-aligned");
 
 endmodule : uvmt_cv32e40x_integration_assert
