670467ac7870a7dedcaf8cba62ffe033c670fe40
Robin Pedersen <Robin.Pedersen@silabs.com>
2022-01-19 16:09:01 +0100

    cv32e40s: copy 'tests/programs' from 40x
    Signed-off-by: Robin Pedersen <Robin.Pedersen@silabs.com>

--- cv32e40s/tests/programs/custom/modeled_csr_por/modeled_csr_por.c
+++ cv32e40s/tests/programs/custom/modeled_csr_por/modeled_csr_por.c
@@ -1,19 +1,19 @@
 /*
 **
 ** Copyright 2020 OpenHW Group
-** 
+**
 ** Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
 ** you may not use this file except in compliance with the License.
 ** You may obtain a copy of the License at
-** 
+**
 **     https://solderpad.org/licenses/
-** 
+**
 ** Unless required by applicable law or agreed to in writing, software
 ** distributed under the License is distributed on an "AS IS" BASIS,
 ** WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 ** See the License for the specific language governing permissions and
 ** limitations under the License.
-** 
+**
 *******************************************************************************
 **
 ** Modeled CSR power-on-reset test:
@@ -89,7 +89,7 @@ int main(int argc, char *argv[])
   __asm__ volatile("csrr %0, 0x7C4" : "=r"(lpstart1_rval));
   __asm__ volatile("csrr %0, 0x7C5" : "=r"(lpend1_rval));
   __asm__ volatile("csrr %0, 0x7C6" : "=r"(lpcount1_rval));
- 
+
   if (lpstart0_rval != 0x0) {
     printf("ERROR: CSR LPSTART0 not zero!\n\n");
     ++err_cnt;
@@ -158,7 +158,7 @@ int main(int argc, char *argv[])
   }
 
 	//__asm__ volatile("csrr %0, 0x306" : "=r"(mcounteren_rval));    // Not currently modeled
-  //__asm__ volatile("csrr %0, 0x320" : "=r"(mcountinhibit_rval)); // Modeled, but cannot override PoR 
+  //__asm__ volatile("csrr %0, 0x320" : "=r"(mcountinhibit_rval)); // Modeled, but cannot override PoR
 
   //if (mcounteren_rval != 0x0) {
   //  printf("ERROR: CSR MCOUNTEREN not 0x0!\n\n");
