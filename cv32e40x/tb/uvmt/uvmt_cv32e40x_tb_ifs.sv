60a50c6b733ae6654ff09ea5c8d0a9a07e33613b
silabs-mateilga <59358563+silabs-mateilga@users.noreply.github.com>
2022-02-10 10:38:21 +0100

    Fixed signal with of rvfi_dbg

--- cv32e40x/tb/uvmt/uvmt_cv32e40x_tb_ifs.sv
+++ cv32e40x/tb/uvmt/uvmt_cv32e40x_tb_ifs.sv
@@ -146,7 +146,7 @@ interface uvmt_cv32e40x_debug_cov_assert_if
     input         rvfi_valid,
     input  [31:0] rvfi_insn,
     input         rvfi_intr,
-    input         rvfi_dbg,
+    input  [2:0]  rvfi_dbg,
     input         rvfi_dbg_mode,
     input  [31:0] rvfi_pc_wdata,
     input  [31:0] rvfi_pc_rdata,
