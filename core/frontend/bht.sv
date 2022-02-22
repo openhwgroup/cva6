8f87167608016b363ead4e59f4c720729311ca87
sjthales <sebastien.jacq@thalesgroup.com>
2022-02-22 13:36:25 +0100

    C extension optional

--- core/frontend/bht.sv
+++ core/frontend/bht.sv
@@ -27,11 +27,12 @@ module bht #(
     output ariane_pkg::bht_prediction_t [ariane_pkg::INSTR_PER_FETCH-1:0] bht_prediction_o
 );
     // the last bit is always zero, we don't need it for indexing
-    localparam OFFSET = 1;
+    localparam OFFSET = ariane_pkg::RVC == 1'b1 ? 1 : 2;
     // re-shape the branch history table
     localparam NR_ROWS = NR_ENTRIES / ariane_pkg::INSTR_PER_FETCH;
     // number of bits needed to index the row
     localparam ROW_ADDR_BITS = $clog2(ariane_pkg::INSTR_PER_FETCH);
+    localparam ROW_INDEX_BITS = ariane_pkg::RVC == 1'b1 ? $clog2(ariane_pkg::INSTR_PER_FETCH) : 1;   // 1    1
     // number of bits we should use for prediction
     localparam PREDICTION_BITS = $clog2(NR_ROWS) + OFFSET + ROW_ADDR_BITS;
     // we are not interested in all bits of the address
@@ -43,12 +44,16 @@ module bht #(
     } bht_d[NR_ROWS-1:0][ariane_pkg::INSTR_PER_FETCH-1:0], bht_q[NR_ROWS-1:0][ariane_pkg::INSTR_PER_FETCH-1:0];
 
     logic [$clog2(NR_ROWS)-1:0]  index, update_pc;
-    logic [ROW_ADDR_BITS-1:0]    update_row_index;
+    logic [ROW_INDEX_BITS-1:0]    update_row_index;
     logic [1:0]                  saturation_counter;
 
     assign index     = vpc_i[PREDICTION_BITS - 1:ROW_ADDR_BITS + OFFSET];
     assign update_pc = bht_update_i.pc[PREDICTION_BITS - 1:ROW_ADDR_BITS + OFFSET];
-    assign update_row_index = bht_update_i.pc[ROW_ADDR_BITS + OFFSET - 1:OFFSET];
+    if (ariane_pkg::RVC == 1'b1) begin : gen_update_row_index
+      assign update_row_index = bht_update_i.pc[ROW_ADDR_BITS + OFFSET - 1:OFFSET];
+    end else begin
+      assign update_row_index = '0;
+    end
 
     // prediction assignment
     for (genvar i = 0; i < ariane_pkg::INSTR_PER_FETCH; i++) begin : gen_bht_output
