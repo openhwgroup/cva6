5857b3654ee6acb70c345cf93dff8e3779a1e8e2
ninolomata <bruno.vilaca.sa@gmail.com>
2022-03-21 13:49:30 +0000

    hyp-mmu: add auxiliar functions
    - Add functions to check if the final translation is 2M, as 1G;
    - Add function to compute the guest physical address;
    - Add function to compute the guest physical page number;
    
    Signed-off-by: Bruno Sa <bruno.vilaca.sa@gmail.com>

--- core/include/ariane_pkg.sv
+++ core/include/ariane_pkg.sv
@@ -1021,4 +1021,53 @@ package ariane_pkg;
             default:     return 2'b11;
         endcase
     endfunction
+
+    // ----------------------
+    // MMU Functions
+    // ----------------------
+
+    // checks if final translation page size is 1G when H-extension is enabled
+    function logic is_trans_1G(input logic s_st_enbl, input logic g_st_enbl, input logic is_s_1G, input logic is_g_1G);
+      return (((is_s_1G && s_st_enbl) || !s_st_enbl) && ((is_g_1G && g_st_enbl) || !g_st_enbl));
+    endfunction : is_trans_1G
+
+    // checks if final translation page size is 2M when H-extension is enabled
+    function logic is_trans_2M(input logic s_st_enbl, input logic g_st_enbl, input logic is_s_1G, input logic is_s_2M, input logic is_g_1G, input logic is_g_2M);
+      return  (s_st_enbl && g_st_enbl) ? 
+                ((is_s_2M && (is_g_1G || is_g_2M)) || (is_g_2M && (is_s_1G || is_s_2M))) :
+                ((is_s_2M && s_st_enbl) || (is_g_2M && g_st_enbl));
+    endfunction : is_trans_2M
+
+    // computes the paddr based on the page size, ppn and offset
+    function logic [(riscv::GPLEN-1):0] make_gpaddr(input logic s_st_enbl, input logic is_1G, input logic is_2M, input logic [(riscv::VLEN-1):0] vaddr, input riscv::pte_t pte);
+        logic [(riscv::GPLEN-1):0] gpaddr;
+        if (s_st_enbl) begin
+            gpaddr = {pte.ppn[(riscv::GPPNW-1):0], vaddr[11:0]};
+            // Giga page
+            if (is_1G)
+                gpaddr[29:12] = vaddr[29:12];
+            // Mega page
+            if (is_2M)
+                gpaddr[20:12] = vaddr[20:12];
+        end else begin
+            gpaddr = vaddr[(riscv::GPLEN-1):0];
+        end
+        return gpaddr;
+    endfunction : make_gpaddr
+
+     // computes the final gppn based on the guest physical address
+    function logic [(riscv::GPPNW-1):0] make_gppn(input logic s_st_enbl, input logic is_2M, input logic is_1G, input logic [28:0] vpn, input riscv::pte_t pte);
+        logic [(riscv::GPPNW-1):0] gppn;
+        if (s_st_enbl) begin
+            gppn = pte.ppn[(riscv::GPPNW-1):0];
+            if(is_2M)
+                gppn[8:0] = vpn[8:0];
+            if(is_1G)
+                gppn[17:0] = vpn[17:0];
+        end else begin
+            gppn = vpn;
+        end
+        return gppn;
+    endfunction : make_gppn
+
 endpackage
