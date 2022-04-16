c8d6690d22e4d801f2723d6328fea6ab4b403ca2
M-Ijaz-10x <muhammad.ijaz@10xengineers.ai>
2022-04-16 15:25:26 +0500

    Adding the updated ariane_pkg file

--- core/include/ariane_pkg.sv
+++ core/include/ariane_pkg.sv
@@ -457,6 +457,8 @@ package ariane_pkg;
     // ---------------
     // EX Stage
     // ---------------
+
+`ifndef BITMANIP
     typedef enum logic [6:0] { // basic ALU op
                                ADD, SUB, ADDW, SUBW,
                                // logic operations
@@ -496,6 +498,69 @@ package ariane_pkg;
                                // Offload Instructions to be directed into cv_x_if
                                OFFLOAD
                              } fu_op;
+`else
+    typedef enum logic [6:0] { // basic ALU op
+                               ADD, SUB, ADDW, SUBW,
+                               // logic operations
+                               XORL, ORL, ANDL,
+                               // shifts
+                               SRA, SRL, SLL, SRLW, SLLW, SRAW,
+                               // comparisons
+                               LTS, LTU, GES, GEU, EQ, NE,
+                               // jumps
+                               JALR, BRANCH,
+                               // set lower than operations
+                               SLTS, SLTU,
+                               // CSR functions
+                               MRET, SRET, DRET, ECALL, WFI, FENCE, FENCE_I, SFENCE_VMA, CSR_WRITE, CSR_READ, CSR_SET, CSR_CLEAR,
+                               // LSU functions
+                               LD, SD, LW, LWU, SW, LH, LHU, SH, LB, SB, LBU,
+                               // Atomic Memory Operations
+                               AMO_LRW, AMO_LRD, AMO_SCW, AMO_SCD,
+                               AMO_SWAPW, AMO_ADDW, AMO_ANDW, AMO_ORW, AMO_XORW, AMO_MAXW, AMO_MAXWU, AMO_MINW, AMO_MINWU,
+                               AMO_SWAPD, AMO_ADDD, AMO_ANDD, AMO_ORD, AMO_XORD, AMO_MAXD, AMO_MAXDU, AMO_MIND, AMO_MINDU,
+                               // Multiplications
+                               MUL, MULH, MULHU, MULHSU, MULW,
+                               // Divisions
+                               DIV, DIVU, DIVW, DIVUW, REM, REMU, REMW, REMUW,
+                               // Floating-Point Load and Store Instructions
+                               FLD, FLW, FLH, FLB, FSD, FSW, FSH, FSB,
+                               // Floating-Point Computational Instructions
+                               FADD, FSUB, FMUL, FDIV, FMIN_MAX, FSQRT, FMADD, FMSUB, FNMSUB, FNMADD,
+                               // Floating-Point Conversion and Move Instructions
+                               FCVT_F2I, FCVT_I2F, FCVT_F2F, FSGNJ, FMV_F2X, FMV_X2F,
+                               // Floating-Point Compare Instructions
+                               FCMP,
+                               // Floating-Point Classify Instruction
+                               FCLASS,
+                               // Vectorial Floating-Point Instructions that don't directly map onto the scalar ones
+                               VFMIN, VFMAX, VFSGNJ, VFSGNJN, VFSGNJX, VFEQ, VFNE, VFLT, VFGE, VFLE, VFGT, VFCPKAB_S, VFCPKCD_S, VFCPKAB_D, VFCPKCD_D,
+                               // Offload Instructions to be directed into cv_x_if
+                               OFFLOAD,
+                               // Or-Combine and REV8
+                               ORCB, REV8,
+                               // Bitwise Rotation
+                               ROL, ROLW, ROR, RORI, RORIW, RORW,
+                               // Sign and Zero Extend
+                               SEXTB, SEXTH, ZEXTH,
+                               // Count population
+                               CPOP, CPOPW,
+                               // Count Leading/Training Zeros
+                               CLZ, CLZW, CTZ, CTZW,
+                               // Carry less multiplication Op's
+                               CLMUL, CLMULH, CLMULR,
+                               // Single bit instructions Op's
+                               BCLR, BCLRI, BEXT, BEXTI, BINV, BINVI, BSET, BSETI,
+                               // Integer minimum/maximum
+                               MAX, MAXU, MIN, MINU,
+                               // Shift with Add Unsigned Word and Unsigned Word Op's (Bitmanip)
+                               SH1ADDUW, SH2ADDUW, SH3ADDUW, ADDUW, SLLIUW,
+                               // Shift with Add (Bitmanip)
+                               SH1ADD, SH2ADD, SH3ADD,
+                               // Bitmanip Logical with negate op (Bitmanip)
+                               ANDN, ORN, XNOR
+                             } fu_op;
+`endif
 
     typedef struct packed {
         fu_t                      fu;
