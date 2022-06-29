15800de3a6617a82ee2e6bd775c600d1d1f101ca
Guillaume Chauvon <guillaume.chauvon@thalesgroup.com>
2022-06-29 11:16:54 +0200

    cv32a60x_pkg.sv: set CVXIFEn to 1
    Signed-off-by: Guillaume Chauvon <guillaume.chauvon@thalesgroup.com>

--- core/include/cv32a60x_config_pkg.sv
+++ core/include/cv32a60x_config_pkg.sv
@@ -18,7 +18,7 @@ package cva6_config_pkg;
     localparam CVA6ConfigF8En = 0;
     localparam CVA6ConfigFVecEn = 0;
 
-    localparam CVA6ConfigCvxifEn = 0;
+    localparam CVA6ConfigCvxifEn = 1;
     localparam CVA6ConfigCExtEn = 1;
     localparam CVA6ConfigAExtEn = 0;
 
