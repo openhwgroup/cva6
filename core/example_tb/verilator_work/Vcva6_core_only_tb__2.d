d0748ce6a15104d95b8fce8bf6e8e8478b36c09d
Jean-Roch Coulon <jean-roch.coulon@thalesgroup.com>
2022-10-11 18:22:50 +0200

    Remove verilator_work unwanted and unused files
    Signed-off-by: Jean-Roch Coulon <jean-roch.coulon@thalesgroup.com>

--- core/example_tb/verilator_work/Vcva6_core_only_tb__2.d
+++
@@ -1,6 +0,0 @@
-Vcva6_core_only_tb__2.o: Vcva6_core_only_tb__2.cpp Vcva6_core_only_tb.h \
- /usr/local/share/verilator/include/verilated_heavy.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- Vcva6_core_only_tb__Syms.h Vcva6_core_only_tb___024unit.h \
- Vcva6_core_only_tb_AXI_BUS__A40_AB40_AC5_AD1.h
--- core/example_tb/verilator_work/Vcva6_core_only_tb__5__Slow.d
+++
@@ -1,7 +0,0 @@
-Vcva6_core_only_tb__5__Slow.o: Vcva6_core_only_tb__5__Slow.cpp \
- Vcva6_core_only_tb.h \
- /usr/local/share/verilator/include/verilated_heavy.h \
- /usr/local/share/verilator/include/verilated.h \
- /usr/local/share/verilator/include/verilatedos.h \
- Vcva6_core_only_tb__Syms.h Vcva6_core_only_tb___024unit.h \
- Vcva6_core_only_tb_AXI_BUS__A40_AB40_AC5_AD1.h
