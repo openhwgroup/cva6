df920cda7e4a8a0db7b6bcd0d2270a1b5a7521d2
JeanRochCoulon <jean-roch.coulon@thalesgroup.com>
2022-10-13 08:02:01 +0200

    Remove verilator_work files (#975)

--- core/example_tb/verilator_work/Vcva6_core_only_tb_AXI_BUS__A40_AB40_AC5_AD1.cpp
+++
@@ -1,8 +0,0 @@
-// Verilated -*- C++ -*-
-// DESCRIPTION: Verilator output: Design implementation internals
-// See Vcva6_core_only_tb.h for the primary calling header
-
-#include "Vcva6_core_only_tb_AXI_BUS__A40_AB40_AC5_AD1.h"
-#include "Vcva6_core_only_tb__Syms.h"
-
-//==========
