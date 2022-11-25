b277c728d4ff9ef7544bdb63eade9d55fcba9363
sjthales <sebastien.jacq@thalesgroup.com>
2022-11-25 09:17:51 +0100

    code cleaning
    Signed-off-by: sjthales <sebastien.jacq@thalesgroup.com>

--- core/include/cv32a6_ima_sv32_fpga_config_pkg.sv
+++ core/include/cv32a6_ima_sv32_fpga_config_pkg.sv
@@ -1,11 +1,11 @@
-// Copyright 2022 Thales Research and Technology
+// Copyright 2021 Thales DIS design services SAS
 //
 // Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0
 // You may obtain a copy of the License at https://solderpad.org/licenses/
 //
-// Original Author: Sébastien Jacq
+// Original Author: Jean-Roch COULON (jean-roch.coulon@thalesgroup.com)
 
 
 package cva6_config_pkg;
