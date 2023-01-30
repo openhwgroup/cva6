95b7e4bde137fe8e0c54cd3db454d88a252d6923
sjthales <sebastien.jacq@thalesgroup.com>
2023-01-30 11:35:27 +0100

    fix bug
    Signed-off-by: sjthales <sebastien.jacq@thalesgroup.com>

--- core/include/ariane_pkg.sv
+++ core/include/ariane_pkg.sv
@@ -378,6 +378,11 @@ package ariane_pkg;
         logic       valid;
         logic       taken;
     } bht_prediction_t;
+    
+    typedef struct packed {
+        logic       valid;
+        logic [1:0] saturation_counter;
+    } bht_t;
 
     typedef enum logic[3:0] {
         NONE,      // 0
