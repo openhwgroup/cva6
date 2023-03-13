d9933a5f86aa305a0255fe5eab59003b00e28581
Nils Wistoff <nwistoff@iis.ee.ethz.ch>
2023-03-13 10:57:47 +0100

    Make AXI transactions modifiable (#948)
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>
    Co-authored-by: Florian Zaruba <zarubaf@iis.ee.ethz.ch>

--- core/axi_shim.sv
+++ core/axi_shim.sv
@@ -93,7 +93,7 @@ module axi_shim #(
   assign axi_req_o.aw.prot   = 3'b0;
   assign axi_req_o.aw.region = 4'b0;
   assign axi_req_o.aw.lock   = wr_lock_i;
-  assign axi_req_o.aw.cache  = 4'b0;
+  assign axi_req_o.aw.cache  = axi_pkg::CACHE_MODIFIABLE;
   assign axi_req_o.aw.qos    = 4'b0;
   assign axi_req_o.aw.atop   = wr_atop_i;
   assign axi_req_o.aw.user   = '0;
@@ -252,7 +252,7 @@ module axi_shim #(
   assign axi_req_o.ar.prot   = 3'b0;
   assign axi_req_o.ar.region = 4'b0;
   assign axi_req_o.ar.lock   = rd_lock_i;
-  assign axi_req_o.ar.cache  = 4'b0;
+  assign axi_req_o.ar.cache  = axi_pkg::CACHE_MODIFIABLE;
   assign axi_req_o.ar.qos    = 4'b0;
   assign axi_req_o.ar.user   = '0;
 
