69bd76366459b4bf10b784ab877f2a5e64dd9e08
Zbigniew Chamski <zbigniew.chamski@thalesgroup.com>
2023-03-13 16:40:48 +0100

    Test termination: Streamline the definitions of termination sequences.
    * cva6/regress/install-riscv-arch-test.sh: Complain if Spike is not
      installed alongside its source code.  Copy Spike target definitions to
      corresponding riscv-arch-test target directory.
    * cva6/regress/riscv-compoliance.patch: Update to use write-into-tohost
       termination.
    * cva6/regress/riscv-tests-env.patch: Do not set up ECALL arguments.
    * cva6/regress/smoke-tests.sh: Add cv32a60x version of hello_world to check
      sanity of 32b custom BSP support.
    * cva6/tests/custom/common/crt.S (handle_trap): Add default overrideable
      definition.
      (_init): Likewise.
      (exit): Likewise.
      (_exit): New.
    * cva6/tests/testlist_riscv-arch-test-cv32a60x.yaml: Use target definitions
      copied from Spike tree upon arch-test installation.
    * cva6/tests/testlist_riscv-arch-test-cv64a6_imafdc_sv39.yaml: Likewise.(_exit): New.

--- cva6/regress/smoke-tests.sh
+++ cva6/regress/smoke-tests.sh
@@ -38,6 +38,8 @@ make clean_all
 python3 cva6.py --testlist=../tests/testlist_riscv-compliance-cv32a60x.yaml --test rv32i-I-ADD-01 --iss_yaml cva6.yaml --target cv32a60x --iss=$DV_SIMULATORS $DV_OPTS
 python3 cva6.py --testlist=../tests/testlist_riscv-tests-cv32a60x-p.yaml --test rv32ui-p-add --iss_yaml cva6.yaml --target cv32a60x --iss=$DV_SIMULATORS $DV_OPTS
 python3 cva6.py --testlist=../tests/testlist_riscv-arch-test-cv32a60x.yaml --test rv32im-cadd-01 --iss_yaml cva6.yaml --target cv32a60x --iss=$DV_SIMULATORS $DV_OPTS  --linker=../tests/riscv-isa-sim/arch_test_target/spike/link.ld
+python3 cva6.py --target cv32a60x --iss=$DV_SIMULATORS --iss_yaml=cva6.yaml --c_tests ../tests/custom/hello_world/hello_world.c\
+  --gcc_opts "-g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc -I../tests/custom/env -I../tests/custom/common -T ../tests/custom/common/test.ld"
 make -C ../../core-v-cores/cva6 clean
 make clean_all
 
