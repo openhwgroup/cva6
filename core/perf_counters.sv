80fbf28ef81b7c8addfab60dba3c331f92216ff9
Tulika Sinha <tulika.sinha@thalesgroup.com>
2023-03-27 11:32:28 +0530

    Factorised Code

--- core/perf_counters.sv
+++ core/perf_counters.sv
@@ -63,172 +63,39 @@ module perf_counters import ariane_pkg::*; #(
 
   //Multiplexer   
    always_comb begin : Mux
-        events[6:1]='{default:0}; 
- 
-        case(mhpmevent_q[1])
-           5'b00000 : events[1] = 0;
-           5'b00001 : events[1] = l1_icache_miss_i;//L1 I-Cache misses
-           5'b00010 : events[1] = l1_dcache_miss_i;//L1 D-Cache misses
-           5'b00011 : events[1] = itlb_miss_i;//ITLB misses
-           5'b00100 : events[1] = dtlb_miss_i;//DTLB misses
-           5'b00101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[1] = commit_instr_i[i].fu == LOAD;//Load accesses							 
-           5'b00110 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[1] = commit_instr_i[i].fu == STORE;//Store accesses
-           5'b00111 : events[1] = ex_i.valid;//Exceptions
-           5'b01000 : events[1] = eret_i;//Exception handler returns
-           5'b01001 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[1] = commit_instr_i[i].fu == CTRL_FLOW;//Branch instructions 
-           5'b01010 : events[1] = resolved_branch_i.valid && resolved_branch_i.is_mispredict;//Branch mispredicts
-           5'b01011 : events[1] = branch_exceptions_i.valid;//Branch exceptions
+        events[6:1]='{default:0};
+         
+      for(int unsigned i = 1; i <= 6; i++) begin
+        case(mhpmevent_q[i])
+           5'b00000 : events[i] = 0;
+           5'b00001 : events[i] = l1_icache_miss_i;//L1 I-Cache misses
+           5'b00010 : events[i] = l1_dcache_miss_i;//L1 D-Cache misses
+           5'b00011 : events[i] = itlb_miss_i;//ITLB misses
+           5'b00100 : events[i] = dtlb_miss_i;//DTLB misses
+           5'b00101 : for (int unsigned j = 0; j < NR_COMMIT_PORTS; j++) if (commit_ack_i[j]) events[i] = commit_instr_i[j].fu == LOAD;//Load accesses							 
+           5'b00110 : for (int unsigned j = 0; j < NR_COMMIT_PORTS; j++) if (commit_ack_i[j]) events[i] = commit_instr_i[j].fu == STORE;//Store accesses
+           5'b00111 : events[i] = ex_i.valid;//Exceptions
+           5'b01000 : events[i] = eret_i;//Exception handler returns
+           5'b01001 : for (int unsigned j = 0; j < NR_COMMIT_PORTS; j++) if (commit_ack_i[j]) events[i] = commit_instr_i[j].fu == CTRL_FLOW;//Branch instructions 
+           5'b01010 : events[i] = resolved_branch_i.valid && resolved_branch_i.is_mispredict;//Branch mispredicts
+           5'b01011 : events[i] = branch_exceptions_i.valid;//Branch exceptions
                    // The standard software calling convention uses register x1 to hold the return address on a call
                    // the unconditional jump is decoded as ADD op
-           5'b01100 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[1] = commit_instr_i[i].fu == CTRL_FLOW && (commit_instr_i[i].op == ADD || commit_instr_i[i].op == JALR) && (commit_instr_i[i].rd == 'd1 || commit_instr_i[i].rd == 'd5);//Call
-           5'b01101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[1] = commit_instr_i[i].op == JALR && commit_instr_i[i].rd == 'd0;//Return
-           5'b01110 : events[1] = sb_full_i;//MSB Full
-           5'b01111 : events[1] = if_empty_i;//Instruction fetch Empty
-           5'b10000 : events[1] = l1_icache_access_i.req;//L1 I-Cache accesses
-           5'b10001 : events[1] = l1_dcache_access_i[0].data_req || l1_dcache_access_i[1].data_req || l1_dcache_access_i[2].data_req;//L1 D-Cache accesses
-           5'b10010 : events[1] = (l1_dcache_miss_i && miss_vld_bits_i[0] == 8'hFF) || (l1_dcache_miss_i && miss_vld_bits_i[1] == 8'hFF) || (l1_dcache_miss_i && miss_vld_bits_i[2] == 8'hFF);//eviction 
-           5'b10011 : events[1] = i_tlb_flush_i;//I-TLB flush
-           5'b10100 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[1] = commit_instr_i[i].fu == ALU || commit_instr_i[i].fu == MULT;//Integer instructions
-           5'b10101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[1] = commit_instr_i[i].fu == FPU || commit_instr_i[i].fu == FPU_VEC;//Floating Point Instructions
-           5'b10110 : events[1] = stall_issue_i;//Pipeline bubbles
-           default:   events[1] = 0;
+           5'b01100 : for (int unsigned j = 0; j < NR_COMMIT_PORTS; j++) if (commit_ack_i[j]) events[i] = commit_instr_i[j].fu == CTRL_FLOW && (commit_instr_i[j].op == ADD || commit_instr_i[j].op == JALR) && (commit_instr_i[j].rd == 'd1 || commit_instr_i[j].rd == 'd5);//Call
+           5'b01101 : for (int unsigned j = 0; j < NR_COMMIT_PORTS; j++) if (commit_ack_i[j]) events[i] = commit_instr_i[j].op == JALR && commit_instr_i[j].rd == 'd0;//Return
+           5'b01110 : events[i] = sb_full_i;//MSB Full
+           5'b01111 : events[i] = if_empty_i;//Instruction fetch Empty
+           5'b10000 : events[i] = l1_icache_access_i.req;//L1 I-Cache accesses
+           5'b10001 : events[i] = l1_dcache_access_i[0].data_req || l1_dcache_access_i[1].data_req || l1_dcache_access_i[2].data_req;//L1 D-Cache accesses
+           5'b10010 : events[i] = (l1_dcache_miss_i && miss_vld_bits_i[0] == 8'hFF) || (l1_dcache_miss_i && miss_vld_bits_i[1] == 8'hFF) || (l1_dcache_miss_i && miss_vld_bits_i[2] == 8'hFF);//eviction 
+           5'b10011 : events[i] = i_tlb_flush_i;//I-TLB flush
+           5'b10100 : for (int unsigned j = 0; j < NR_COMMIT_PORTS; j++) if (commit_ack_i[j]) events[i] = commit_instr_i[j].fu == ALU || commit_instr_i[j].fu == MULT;//Integer instructions
+           5'b10101 : for (int unsigned j = 0; j < NR_COMMIT_PORTS; j++) if (commit_ack_i[j]) events[i] = commit_instr_i[j].fu == FPU || commit_instr_i[j].fu == FPU_VEC;//Floating Point Instructions
+           5'b10110 : events[i] = stall_issue_i;//Pipeline bubbles
+           default:   events[i] = 0;
          endcase
+       end
        
-        case(mhpmevent_q[2])
-           5'b00000 : events[2] = 0;
-           5'b00001 : events[2] = l1_icache_miss_i;
-           5'b00010 : events[2] = l1_dcache_miss_i;
-           5'b00011 : events[2] = itlb_miss_i;
-           5'b00100 : events[2] = dtlb_miss_i;
-           5'b00101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[2] = commit_instr_i[i].fu == LOAD; 							 
-           5'b00110 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[2] = commit_instr_i[i].fu == STORE;
-           5'b00111 : events[2] = ex_i.valid;
-           5'b01000 : events[2] = eret_i;
-           5'b01001 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[2] = commit_instr_i[i].fu == CTRL_FLOW;
-           5'b01010 : events[2] = resolved_branch_i.valid && resolved_branch_i.is_mispredict;
-           5'b01011 : events[2] = branch_exceptions_i.valid;
-           5'b01100 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[2] = commit_instr_i[i].fu == CTRL_FLOW && (commit_instr_i[i].op == ADD || commit_instr_i[i].op == JALR) && (commit_instr_i[i].rd == 'd1 || commit_instr_i[i].rd == 'd5);
-           5'b01101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[2] = commit_instr_i[i].op == JALR && commit_instr_i[i].rd == 'd0;
-           5'b01110 : events[2] = sb_full_i;
-           5'b01111 : events[2] = if_empty_i;
-           5'b10000 : events[2] = l1_icache_access_i.req;
-           5'b10001 : events[2] = l1_dcache_access_i[0].data_req || l1_dcache_access_i[1].data_req || l1_dcache_access_i[2].data_req;
-           5'b10010 : events[2] = (l1_dcache_miss_i && miss_vld_bits_i[0] == 8'hFF) || (l1_dcache_miss_i && miss_vld_bits_i[1] == 8'hFF) || (l1_dcache_miss_i && miss_vld_bits_i[2] == 8'hFF); 
-           5'b10011 : events[2] = i_tlb_flush_i;
-           5'b10100 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[2] = commit_instr_i[i].fu == ALU || commit_instr_i[i].fu == MULT;
-           5'b10101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[2] = commit_instr_i[i].fu == FPU || commit_instr_i[i].fu == FPU_VEC;
-           5'b10110 : events[2] = stall_issue_i;
-           default:   events[2] = 0;
-         endcase
-       
-         case(mhpmevent_q[3])
-           5'b00000 : events[3] = 0;
-           5'b00001 : events[3] = l1_icache_miss_i;
-           5'b00010 : events[3] = l1_dcache_miss_i;
-           5'b00011 : events[3] = itlb_miss_i;
-           5'b00100 : events[3] = dtlb_miss_i;
-           5'b00101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[3] = commit_instr_i[i].fu == LOAD; 							 
-           5'b00110 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[3] = commit_instr_i[i].fu == STORE;
-           5'b00111 : events[3] = ex_i.valid;
-           5'b01000 : events[3] = eret_i;
-           5'b01001 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[3] = commit_instr_i[i].fu == CTRL_FLOW; 
-           5'b01010 : events[3] = resolved_branch_i.valid && resolved_branch_i.is_mispredict;
-           5'b01011 : events[3] = branch_exceptions_i.valid;
-           5'b01100 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[3] = commit_instr_i[i].fu == CTRL_FLOW && (commit_instr_i[i].op == ADD || commit_instr_i[i].op == JALR) && (commit_instr_i[i].rd == 'd1 || commit_instr_i[i].rd == 'd5);
-           5'b01101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[3] = commit_instr_i[i].op == JALR && commit_instr_i[i].rd == 'd0;
-           5'b01110 : events[3] = sb_full_i;
-           5'b01111 : events[3] = if_empty_i;
-           5'b10000 : events[3] = l1_icache_access_i.req;
-           5'b10001 : events[3] = l1_dcache_access_i[0].data_req || l1_dcache_access_i[1].data_req || l1_dcache_access_i[2].data_req;
-           5'b10010 : events[3] = (l1_dcache_miss_i && miss_vld_bits_i[0] == 8'hFF) || (l1_dcache_miss_i && miss_vld_bits_i[1] == 8'hFF) || (l1_dcache_miss_i && miss_vld_bits_i[2] == 8'hFF); 
-           5'b10011 : events[3] = i_tlb_flush_i;
-           5'b10100 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[3] = commit_instr_i[i].fu == ALU || commit_instr_i[i].fu == MULT;
-           5'b10101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[3] = commit_instr_i[i].fu == FPU || commit_instr_i[i].fu == FPU_VEC;
-           5'b10110 : events[3] = stall_issue_i;
-           default:   events[3] = 0;
-         endcase
-
-         case(mhpmevent_q[4])
-           5'b00000 : events[4] = 0;
-           5'b00001 : events[4] = l1_icache_miss_i;
-           5'b00010 : events[4] = l1_dcache_miss_i;
-           5'b00011 : events[4] = itlb_miss_i;
-           5'b00100 : events[4] = dtlb_miss_i;
-           5'b00101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[4] = commit_instr_i[i].fu == LOAD; 							 
-           5'b00110 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[4] = commit_instr_i[i].fu == STORE;
-           5'b00111 : events[4] = ex_i.valid;
-           5'b01000 : events[4] = eret_i;
-           5'b01001 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[4] = commit_instr_i[i].fu == CTRL_FLOW;
-           5'b01010 : events[4] = resolved_branch_i.valid && resolved_branch_i.is_mispredict;
-           5'b01011 : events[4] = branch_exceptions_i.valid;
-           5'b01100 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[4] = commit_instr_i[i].fu == CTRL_FLOW && (commit_instr_i[i].op == ADD || commit_instr_i[i].op == JALR) && (commit_instr_i[i].rd == 'd1 || commit_instr_i[i].rd == 'd5);
-           5'b01101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[4] = commit_instr_i[i].op == JALR && commit_instr_i[i].rd == 'd0;
-           5'b01110 : events[4] = sb_full_i;
-           5'b01111 : events[4] = if_empty_i;
-           5'b10000 : events[4] = l1_icache_access_i.req;
-           5'b10001 : events[4] = l1_dcache_access_i[0].data_req || l1_dcache_access_i[1].data_req || l1_dcache_access_i[2].data_req;
-           5'b10010 : events[4] = (l1_dcache_miss_i && miss_vld_bits_i[0] == 8'hFF) || (l1_dcache_miss_i && miss_vld_bits_i[1] == 8'hFF) || (l1_dcache_miss_i && miss_vld_bits_i[2] == 8'hFF); 
-           5'b10011 : events[4] = i_tlb_flush_i;
-           5'b10100 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[4] = commit_instr_i[i].fu == ALU || commit_instr_i[i].fu == MULT;
-           5'b10101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[4] = commit_instr_i[i].fu == FPU || commit_instr_i[i].fu == FPU_VEC;
-           5'b10110 : events[4] = stall_issue_i;
-           default:   events[4] = 0;
-         endcase
-       
-         case(mhpmevent_q[5])
-           5'b00000 : events[5] = 0;
-           5'b00001 : events[5] = l1_icache_miss_i;
-           5'b00010 : events[5] = l1_dcache_miss_i;
-           5'b00011 : events[5] = itlb_miss_i;
-           5'b00100 : events[5] = dtlb_miss_i;
-           5'b00101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[5] = commit_instr_i[i].fu == LOAD; 							 
-           5'b00110 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[5] = commit_instr_i[i].fu == STORE;
-           5'b00111 : events[5] = ex_i.valid;
-           5'b01000 : events[5] = eret_i;
-           5'b01001 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[5] = commit_instr_i[i].fu == CTRL_FLOW;
-           5'b01010 : events[5] = resolved_branch_i.valid && resolved_branch_i.is_mispredict;
-           5'b01011 : events[5] = branch_exceptions_i.valid;
-           5'b01100 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[5] = commit_instr_i[i].fu == CTRL_FLOW && (commit_instr_i[i].op == ADD || commit_instr_i[i].op == JALR) && (commit_instr_i[i].rd == 'd1 || commit_instr_i[i].rd == 'd5);
-           5'b01101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[5] = commit_instr_i[i].op == JALR && commit_instr_i[i].rd == 'd0;
-           5'b01110 : events[5] = sb_full_i;
-           5'b01111 : events[5] = if_empty_i;
-           5'b10000 : events[5] = l1_icache_access_i.req;
-           5'b10001 : events[5] = l1_dcache_access_i[0].data_req || l1_dcache_access_i[1].data_req || l1_dcache_access_i[2].data_req;
-           5'b10010 : events[5] = (l1_dcache_miss_i && miss_vld_bits_i[0] == 8'hFF) || (l1_dcache_miss_i && miss_vld_bits_i[1] == 8'hFF) || (l1_dcache_miss_i && miss_vld_bits_i[2] == 8'hFF);
-           5'b10011 : events[5] = i_tlb_flush_i;
-           5'b10100 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[5] = commit_instr_i[i].fu == ALU || commit_instr_i[i].fu == MULT;
-           5'b10101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[5] = commit_instr_i[i].fu == FPU || commit_instr_i[i].fu == FPU_VEC;
-           5'b10110 : events[5] = stall_issue_i;
-           default:   events[5] = 0;
-         endcase
-
-         case(mhpmevent_q[6])
-            5'b00000 : events[6] = 0;
-            5'b00001 : events[6] = l1_icache_miss_i;
-            5'b00010 : events[6] = l1_dcache_miss_i;
-            5'b00011 : events[6] = itlb_miss_i;
-            5'b00100 : events[6] = dtlb_miss_i;
-            5'b00101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[6] = commit_instr_i[i].fu == LOAD; 							 
-            5'b00110 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[6] = commit_instr_i[i].fu == STORE;
-            5'b00111 : events[6] = ex_i.valid;
-            5'b01000 : events[6] = eret_i;
-            5'b01001 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[6] = commit_instr_i[i].fu == CTRL_FLOW; 
-            5'b01010 : events[6] = resolved_branch_i.valid && resolved_branch_i.is_mispredict;
-            5'b01011 : events[6] = branch_exceptions_i.valid;
-            5'b01100 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[6] = commit_instr_i[i].fu == CTRL_FLOW && (commit_instr_i[i].op == ADD || commit_instr_i[i].op == JALR) && (commit_instr_i[i].rd == 'd1 || commit_instr_i[i].rd == 'd5);
-            5'b01101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[6] = commit_instr_i[i].op == JALR && commit_instr_i[i].rd == 'd0;
-            5'b01110 : events[6] = sb_full_i;
-            5'b01111 : events[6] = if_empty_i;
-            5'b10000 : events[6] = l1_icache_access_i.req;
-            5'b10001 : events[6] = l1_dcache_access_i[0].data_req || l1_dcache_access_i[1].data_req || l1_dcache_access_i[2].data_req;
-            5'b10010 : events[6] = (l1_dcache_miss_i && miss_vld_bits_i[0] == 8'hFF) || (l1_dcache_miss_i && miss_vld_bits_i[1] == 8'hFF) || (l1_dcache_miss_i && miss_vld_bits_i[2] == 8'hFF);
-            5'b10011 : events[6] = i_tlb_flush_i;
-            5'b10100 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[6] = commit_instr_i[i].fu == ALU || commit_instr_i[i].fu == MULT;
-            5'b10101 : for (int unsigned i = 0; i < NR_COMMIT_PORTS; i++) if (commit_ack_i[i]) events[6] = commit_instr_i[i].fu == FPU || commit_instr_i[i].fu == FPU_VEC;
-            5'b10110 : events[6] = stall_issue_i;
-            default:   events[6] = 0;
-         endcase
-         
     end     
             
     always_comb begin : generic_counter
