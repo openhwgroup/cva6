2afb29916b841a42ad5627798d40b1f836ff8248
Zbigniew Chamski <zbigniew.chamski@thalesgroup.com>
2023-04-05 17:15:52 +0200

    Provide way of disabling Verilator and/or Spike setup for specific CI jobs.
    * cva6/regress/install-cva6.sh: Disable Verilator setup if the install dir
      variable has value 'NO'.  Refactor the flow of variable definitions in
      presence of Verilator.  Add informative messages.
    * cva6/regress/install-spike.sh: Disable Spike setup if variable SPIKE_ROOT
      has value 'NO'.  Update message printed upon reusing an existing Spike
      installation.

--- cva6/regress/install-cva6.sh
+++ cva6/regress/install-cva6.sh
@@ -9,7 +9,7 @@
 
 # Customise this to a fast local disk
 export ROOT_PROJECT=$(cd "$(dirname "${BASH_SOURCE[0]}")/../../" && pwd)
-export TOP=$ROOT_PROJECT/tools
+export TOP="$ROOT_PROJECT/tools"
 
 # where to install the tools
 if ! [ -n "$RISCV" ]; then
@@ -17,26 +17,38 @@ if ! [ -n "$RISCV" ]; then
   return
 fi
 
+# Set up tool-related variables.
+export PATH="$RISCV/bin:$PATH"
+export LIBRARY_PATH="$RISCV/lib"
+export LD_LIBRARY_PATH="$RISCV/lib:$LD_LIBRARY_PATH"
+export C_INCLUDE_PATH="$RISCV/include"
+export CPLUS_INCLUDE_PATH="$RISCV/include"
+
 # Install Verilator v5.
-cva6/regress/install-verilator.sh
+# Set VERILATOR_INSTALL_DIR to 'NO' to skip installation and checks
+# of Verilator (useful for CI jobs not depending on Verilator in any way).
+if [ "$VERILATOR_INSTALL_DIR" != "NO" ]; then
+  cva6/regress/install-verilator.sh
 
-# Complain if the installation directory of Verilator still is not set
-# after running the installer.
-if [ -z "$VERILATOR_INSTALL_DIR" ]; then
-  echo "Error: VERILATOR_INSTALL_DIR variable still undefined after running Verilator installer."
-  return
+  # Complain if the installation directory of Verilator still is not set
+  # after running the installer.
+  if [ -z "$VERILATOR_INSTALL_DIR" ]; then
+    echo "Error: VERILATOR_INSTALL_DIR variable still undefined after running Verilator installer."
+    return
+  fi
+
+  # Verilator was set up: add Verilator paths to appropriate variables.
+  export PATH="$VERILATOR_INSTALL_DIR/bin:$PATH"
+  export C_INCLUDE_PATH="$VERILATOR_INSTALL_DIR/share/verilator/include:$C_INCLUDE_PATH"
+  export CPLUS_INCLUDE_PATH="$VERILATOR_INSTALL_DIR/share/verilator/include:$CPLUS_INCLUDE_PATH"
+
+  # Check proper Verilator installation given current $PATH.
+  echo PATH=\"$PATH\"
+  echo "Verilator version:"
+  verilator --version || { echo "Error: Verilator not in \$PATH." ; return ; }
+else
+  echo "Skipping Verilator setup on user's request (\$VERILATOR_INSTALL_DIR = \"NO\")."
 fi
-
-export PATH=$RISCV/bin:$VERILATOR_INSTALL_DIR/bin:$PATH
-export LIBRARY_PATH=$RISCV/lib
-export LD_LIBRARY_PATH=$RISCV/lib
-export C_INCLUDE_PATH=$RISCV/include:$VERILATOR_INSTALL_DIR/share/verilator/include
-export CPLUS_INCLUDE_PATH=$RISCV/include:$VERILATOR_INSTALL_DIR/share/verilator/include
-
-# Check proper Verilator installation given current $PATH.
-echo PATH=\"$PATH\"
-echo "Verilator version:"
-verilator --version || { echo "Error: Verilator not in \$PATH." ; return ; }
 
 # number of parallel jobs to use for make commands and simulation
 export NUM_JOBS=24
