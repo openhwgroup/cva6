9ff5fa88061b434ea2591a7d567f0201f7cca07f
Bruno Sa <bruno.vilaca.sa@gmail.com>
2023-04-05 16:17:50 +0100

    hyp: fix typo in lsu
    Signed-off-by: Bruno Sa <bruno.vilaca.sa@gmail.com>

--- core/load_store_unit.sv
+++ core/load_store_unit.sv
@@ -463,7 +463,7 @@ module load_store_unit import ariane_pkg::*; #(
             // check the operator to activate the right functional unit accordingly
             hs_ld_st_inst = 1'b0;
             hlvx_inst     = 1'b0;
-            case (lsu_ctrl.operator)
+            case (lsu_ctrl.operation)
                 // all loads go here
                 HLV_B, HLV_BU, HLV_H, HLV_HU,
                 HLV_W, HSV_B, HSV_H, HSV_W,
