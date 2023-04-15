402ee6fdfc13191d20b48c3da151f385a85c4c90
JeanRochCoulon <jean-roch.coulon@thalesgroup.com>
2023-04-15 10:29:45 +0200

    Add AXI parameter in cva6 config, keep AXI master structs in ariane and slave in SOC (#1182)
    Signed-off-by: Jean-Roch Coulon <jean-roch.coulon@thalesgroup.com>
    Co-authored-by: Florian Zaruba <florianzaruba@gmail.com>

--- corev_apu/tb/ariane_axi_soc_pkg.sv
+++ corev_apu/tb/ariane_axi_soc_pkg.sv
@@ -17,36 +17,17 @@
 
 package ariane_axi_soc;
 
-    // used in axi_adapter.sv
-    typedef enum logic { SINGLE_REQ, CACHE_LINE_REQ } ad_req_t;
-
-    localparam UserWidth = ariane_pkg::AXI_USER_WIDTH;
-    localparam AddrWidth = 64;
-    localparam DataWidth = 64;
+    localparam UserWidth = ariane_axi::UserWidth;
+    localparam AddrWidth = ariane_axi::AddrWidth;
+    localparam DataWidth = ariane_axi::DataWidth;
     localparam StrbWidth = DataWidth / 8;
 
-    typedef logic [ariane_soc::IdWidth-1:0]      id_t;
+    typedef logic [ariane_axi::IdWidth-1:0]  id_t;
     typedef logic [ariane_soc::IdWidthSlave-1:0] id_slv_t;
     typedef logic [AddrWidth-1:0] addr_t;
     typedef logic [DataWidth-1:0] data_t;
     typedef logic [StrbWidth-1:0] strb_t;
     typedef logic [UserWidth-1:0] user_t;
-
-    // AW Channel
-    typedef struct packed {
-        id_t              id;
-        addr_t            addr;
-        axi_pkg::len_t    len;
-        axi_pkg::size_t   size;
-        axi_pkg::burst_t  burst;
-        logic             lock;
-        axi_pkg::cache_t  cache;
-        axi_pkg::prot_t   prot;
-        axi_pkg::qos_t    qos;
-        axi_pkg::region_t region;
-        axi_pkg::atop_t   atop;
-        user_t            user;
-    } aw_chan_t;
 
     // AW Channel - Slave
     typedef struct packed {
@@ -64,42 +45,12 @@ package ariane_axi_soc;
         user_t            user;
     } aw_chan_slv_t;
 
-    // W Channel - AXI4 doesn't define a wid
-    typedef struct packed {
-        data_t data;
-        strb_t strb;
-        logic  last;
-        user_t user;
-    } w_chan_t;
-
-    // B Channel
-    typedef struct packed {
-        id_t            id;
-        axi_pkg::resp_t resp;
-        user_t          user;
-    } b_chan_t;
-
     // B Channel - Slave
     typedef struct packed {
         id_slv_t        id;
         axi_pkg::resp_t resp;
         user_t          user;
     } b_chan_slv_t;
-
-    // AR Channel
-    typedef struct packed {
-        id_t             id;
-        addr_t            addr;
-        axi_pkg::len_t    len;
-        axi_pkg::size_t   size;
-        axi_pkg::burst_t  burst;
-        logic             lock;
-        axi_pkg::cache_t  cache;
-        axi_pkg::prot_t   prot;
-        axi_pkg::qos_t    qos;
-        axi_pkg::region_t region;
-        user_t            user;
-    } ar_chan_t;
 
     // AR Channel - Slave
     typedef struct packed {
@@ -116,15 +67,6 @@ package ariane_axi_soc;
         user_t            user;
     } ar_chan_slv_t;
 
-    // R Channel
-    typedef struct packed {
-        id_t            id;
-        data_t          data;
-        axi_pkg::resp_t resp;
-        logic           last;
-        user_t          user;
-    } r_chan_t;
-
     // R Channel - Slave
     typedef struct packed {
         id_slv_t        id;
@@ -134,32 +76,10 @@ package ariane_axi_soc;
         user_t          user;
     } r_chan_slv_t;
 
-    // Request/Response structs
-    typedef struct packed {
-        aw_chan_t aw;
-        logic     aw_valid;
-        w_chan_t  w;
-        logic     w_valid;
-        logic     b_ready;
-        ar_chan_t ar;
-        logic     ar_valid;
-        logic     r_ready;
-    } req_t;
-
-    typedef struct packed {
-        logic     aw_ready;
-        logic     ar_ready;
-        logic     w_ready;
-        logic     b_valid;
-        b_chan_t  b;
-        logic     r_valid;
-        r_chan_t  r;
-    } resp_t;
-
     typedef struct packed {
         aw_chan_slv_t aw;
         logic         aw_valid;
-        w_chan_t      w;
+        ariane_axi::w_chan_t      w;
         logic         w_valid;
         logic         b_ready;
         ar_chan_slv_t ar;
