3137019ac3a8a5cb2f6638413cc84894dbcfa569
JeanRochCoulon <jean-roch.coulon@thalesgroup.com>
2023-04-28 11:48:45 +0200

    Update CVA6 hash

--- cva6/regress/install-cva6.sh
+++ cva6/regress/install-cva6.sh
@@ -57,7 +57,7 @@ export NUM_JOBS=24
 if ! [ -n "$CVA6_REPO" ]; then
   CVA6_REPO="https://github.com/openhwgroup/cva6.git"
   CVA6_BRANCH="master"
-  CVA6_HASH="75807530f26ba9a0ca501e9d3a6575ec375ed7ab"
+  CVA6_HASH="4f06aa620f75bcae369f05d0652283d45ef76a24"
   CVA6_PATCH=
 fi
 echo $CVA6_REPO
