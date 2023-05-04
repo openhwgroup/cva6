908a328d786a10a6310e82bdcbb102cde83da06b
spidugu444 <sai-krishna.pidugu@thalesgroup.com>
2023-05-04 17:54:39 +0530

    Removed patch code which will generate ecall to terminate simulation

--- cva6/regress/install-riscv-isa-sim.sh
+++ cva6/regress/install-riscv-isa-sim.sh
@@ -11,19 +11,13 @@ if ! [ -n "$RISCV_ISA_SIM" ]; then
   RISCV_ISA_SIM="https://github.com/riscv-software-src/riscv-isa-sim.git"
   RISCV_ISA_SIM_BRANCH="master"
   RISCV_ISA_SIM_HASH="b9fc8e4e9087a6064dfcc627efabbe3fd4bdc309"
-  RISCV_ISA_SIM_PATCH=""
 fi
 echo $RISCV_ISA_SIM
 echo $RISCV_ISA_SIM_BRANCH
 echo $RISCV_ISA_SIM_HASH
-echo $RISCV_ISA_SIM_PATCH
 
 if ! [ -d cva6/tests/riscv-isa-sim ]; then
   git clone $RISCV_ISA_SIM -b $RISCV_ISA_SIM_BRANCH cva6/tests/riscv-isa-sim
   cd cva6/tests/riscv-isa-sim; git checkout $RISCV_ISA_SIM_HASH;
-  if [ -n "$RISCV_ISA_SIM_PATCH" ]; then
-    git apply $RISCV_ISA_SIM_PATCH
-  fi
-  cd -
+    cd -
 fi
-
