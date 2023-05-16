865b089e5ee2536d836c084f3aedc73cbe784e44
Nils Wistoff <nwistoff@iis.ee.ethz.ch>
2023-05-16 10:47:32 +0200

    cv64a6_imafdcv: Update parameters
    Signed-off-by: Nils Wistoff <nwistoff@iis.ee.ethz.ch>

--- core/include/cv64a6_imafdcv_sv39_config_pkg.sv
+++ core/include/cv64a6_imafdcv_sv39_config_pkg.sv
@@ -5,10 +5,15 @@
 // SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0
 // You may obtain a copy of the License at https://solderpad.org/licenses/
 //
-// Original Author: Jean-Roch COULON (jean-roch.coulon@thalesgroup.com)
+// Original Author: Jean-Roch COULON - Thales
 
 
 package cva6_config_pkg;
+
+    typedef enum logic {
+      WB = 0,
+      WT = 1
+    } cache_type_t ;
 
     localparam CVA6ConfigXlen = 64;
 
@@ -21,8 +26,12 @@ package cva6_config_pkg;
     localparam CVA6ConfigCvxifEn = 0;
     localparam CVA6ConfigCExtEn = 1;
     localparam CVA6ConfigAExtEn = 1;
+    localparam CVA6ConfigBExtEn = 0;
     localparam CVA6ConfigVExtEn = 1;
 
+    localparam CVA6ConfigAxiIdWidth = 4;
+    localparam CVA6ConfigAxiAddrWidth = 64;
+    localparam CVA6ConfigAxiDataWidth = 64;
     localparam CVA6ConfigFetchUserEn = 0;
     localparam CVA6ConfigFetchUserWidth = CVA6ConfigXlen;
     localparam CVA6ConfigDataUserEn = 0;
@@ -30,16 +39,48 @@ package cva6_config_pkg;
 
     localparam CVA6ConfigRenameEn = 0;
 
+    localparam CVA6ConfigIcacheByteSize = 16384;
     localparam CVA6ConfigIcacheSetAssoc = 4;
-    localparam CVA6ConfigIcacheSets = 4096;
     localparam CVA6ConfigIcacheLineWidth = 128;
+    localparam CVA6ConfigDcacheByteSize = 16384;
     localparam CVA6ConfigDcacheSetAssoc = 4;
-    localparam CVA6ConfigDcacheSets = 4096;
     localparam CVA6ConfigDcacheLineWidth = 128;
+
+    localparam CVA6ConfigDcacheIdWidth = 1;
+    localparam CVA6ConfigMemTidWidth = 2;
+
+    localparam CVA6ConfigWtDcacheWbufDepth = 8;
 
     localparam CVA6ConfigNrCommitPorts = 2;
     localparam CVA6ConfigNrScoreboardEntries = 8;
 
     localparam CVA6ConfigFPGAEn = 0;
+
+    localparam CVA6ConfigNrLoadPipeRegs = 1;
+    localparam CVA6ConfigNrStorePipeRegs = 0;
+
+    localparam CVA6ConfigInstrTlbEntries = 16;
+    localparam CVA6ConfigDataTlbEntries = 16;
+
+    localparam CVA6ConfigRASDepth = 2;
+    localparam CVA6ConfigBTBEntries = 32;
+    localparam CVA6ConfigBHTEntries = 128;
+
+    localparam CVA6ConfigNrPMPEntries = 8;
+
+    localparam CVA6ConfigPerfCounterEn = 1;
+
+    localparam CVA6ConfigDcacheType = WT;
+
+    localparam CVA6ConfigMmuPresent = 1;
+
+    `define RVFI_PORT
+
+    // Do not modify
+    `ifdef RVFI_PORT
+       localparam CVA6ConfigRvfiTrace = 1;
+    `else
+       localparam CVA6ConfigRvfiTrace = 0;
+    `endif
 
 endpackage
