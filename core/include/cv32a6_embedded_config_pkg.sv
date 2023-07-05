5a75855ece40c309320fa4385dc8536d6aa6d1c4
Fatima Saleem <fatima.saleem@10xengineers.ai>
2023-07-05 17:08:09 +0500

    rename ZiCond config parameter

--- core/include/cv32a6_embedded_config_pkg.sv
+++ core/include/cv32a6_embedded_config_pkg.sv
@@ -27,7 +27,7 @@ package cva6_config_pkg;
     localparam CVA6ConfigAExtEn = 0;
     localparam CVA6ConfigBExtEn = 1;
     localparam CVA6ConfigVExtEn = 0;
-    localparam CVA6ConfigCondExtEn = 0;
+    localparam CVA6ConfigZiCondExtEn = 0;
 
     localparam CVA6ConfigAxiIdWidth = 4;
     localparam CVA6ConfigAxiAddrWidth = 64;
--- core/include/cv64a6_imafdc_sv39_config_pkg.sv
+++ core/include/cv64a6_imafdc_sv39_config_pkg.sv
@@ -28,7 +28,7 @@ package cva6_config_pkg;
     localparam CVA6ConfigAExtEn = 1;
     localparam CVA6ConfigBExtEn = 1;
     localparam CVA6ConfigVExtEn = 0;
-    localparam CVA6ConfigCondExtEn = 1;
+    localparam CVA6ConfigZiCondExtEn = 1;
 
     localparam CVA6ConfigAxiIdWidth = 4;
     localparam CVA6ConfigAxiAddrWidth = 64;
