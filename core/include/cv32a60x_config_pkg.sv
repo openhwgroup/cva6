834b46809638581bc67aec330fe0342eb66b4260
JeanRochCoulon <jean-roch.coulon@thalesgroup.com>
2023-07-27 16:32:59 +0200

    Enable A extension (#1323)
    Signed-off-by: Jean-Roch Coulon <jean-roch.coulon@thalesgroup.com>

--- core/include/cv32a60x_config_pkg.sv
+++ core/include/cv32a60x_config_pkg.sv
@@ -25,7 +25,7 @@ package cva6_config_pkg;
 
     localparam CVA6ConfigCvxifEn = 1;
     localparam CVA6ConfigCExtEn = 1;
-    localparam CVA6ConfigAExtEn = 0;
+    localparam CVA6ConfigAExtEn = 1;
     localparam CVA6ConfigBExtEn = 0;
     localparam CVA6ConfigVExtEn = 0;
 
