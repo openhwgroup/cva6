2a8a5c31e061155b3425c576fdd4b2e45e70003e
AileonN <noeliaoliete1a@gmail.com>
2023-09-07 19:03:22 +0200

    Write coalescing enabled (WRITE_BYTE_MASK)

--- core/include/wt_cache_pkg.sv
+++ core/include/wt_cache_pkg.sv
@@ -225,6 +225,9 @@ package wt_cache_pkg;
     logic [63:0]                       l15_data_next_entry;       // unused in Ariane (only used for CAS atomic requests)
     logic [L15_TLB_CSM_WIDTH-1:0]      l15_csm_data;              // unused in Ariane
     logic [3:0]                        l15_amo_op;                // atomic operation type
+    `ifdef WRITE_BYTE_MASK                                       
+    logic [`L15_BYTE_MASK_WIDHT-1:0]   l15_be;                    // Byte mask
+    `endif 
   } l15_req_t;
 
   typedef struct packed {
