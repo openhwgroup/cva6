3321aaf34db6cbb56ced7a402837c991958f5ee9
Cesar Fuguet <cesar.fuguettortolero@cea.fr>
2023-10-12 11:44:44 +0200

    hpdcache: use short version of the SolderPad Licence header
    Signed-off-by: Cesar Fuguet <cesar.fuguettortolero@cea.fr>

--- core/cache_subsystem/cva6_hpdcache_if_adapter.sv
+++ core/cache_subsystem/cva6_hpdcache_if_adapter.sv
@@ -1,28 +1,14 @@
-/*
- *  Copyright 2023 CEA*
- *  *Commissariat a l'Energie Atomique et aux Energies Alternatives (CEA)
- *
- *  SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
- *
- *  Licensed under the Solderpad Hardware License v 2.1 (the “License”); you
- *  may not use this file except in compliance with the License, or, at your
- *  option, the Apache License version 2.0. You may obtain a copy of the
- *  License at
- *
- *  https://solderpad.org/licenses/SHL-2.1/
- *
- *  Unless required by applicable law or agreed to in writing, any work
- *  distributed under the License is distributed on an “AS IS” BASIS, WITHOUT
- *  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- *  License for the specific language governing permissions and limitations
- *  under the License.
- */
-/*
- *  Authors       : Cesar Fuguet
- *  Creation Date : February, 2023
- *  Description   : Interface adapter for the CVA6 core
- *  History       :
- */
+// Copyright 2023 Commissariat a l'Energie Atomique et aux Energies
+//                Alternatives (CEA)
+//
+// Licensed under the Solderpad Hardware License, Version 2.1 (the “License”);
+// you may not use this file except in compliance with the License.
+// SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
+// You may obtain a copy of the License at https://solderpad.org/licenses/
+//
+// Authors: Cesar Fuguet
+// Date: February, 2023
+// Description: Interface adapter for the CVA6 core
 module cva6_hpdcache_if_adapter
 import hpdcache_pkg::*;
 
--- core/include/cv64a6_imafdc_sv39_hpdcache_config_pkg.sv
+++ core/include/cv64a6_imafdc_sv39_hpdcache_config_pkg.sv
@@ -1,13 +1,18 @@
-//  Copyright 2023 CEA*
-//  *Commissariat a l'Energie Atomique et aux Energies Alternatives (CEA)
+// Copyright 2021 Thales DIS design services SAS
 //
 // Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0
 // You may obtain a copy of the License at https://solderpad.org/licenses/
 //
+// Original Author: Jean-Roch COULON - Thales
+//
+// Copyright 2023 Commissariat a l'Energie Atomique et aux Energies
+//                Alternatives (CEA)
+//
 // Author: Cesar Fuguet - CEA
-// Creation Date: August, 2023
+// Date: August, 2023
+// Description: CVA6 configuration package using the HPDcache as cache subsystem
 
 
 package cva6_config_pkg;
