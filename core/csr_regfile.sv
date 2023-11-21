7a866ffe93d0aa8f7c128414abb20f6bc04bca85
AEzzejjari <110914586+AEzzejjari@users.noreply.github.com>
2023-11-21 18:06:13 +0100

    Update csr_regfile.sv

--- core/csr_regfile.sv
+++ core/csr_regfile.sv
@@ -1288,31 +1288,27 @@ module csr_regfile
       CSR_SET:   csr_wdata = csr_wdata_i | csr_rdata;
       CSR_CLEAR: csr_wdata = (~csr_wdata_i) & csr_rdata;
       CSR_READ:  csr_we = 1'b0;
-      SRET: begin
-        if (CVA6Cfg.RVS) begin
-          // the return should not have any write or read side-effects
-          csr_we   = 1'b0;
-          csr_read = 1'b0;
-          sret     = 1'b1;  // signal a return from supervisor mode
-        end
-      end
       MRET: begin
         // the return should not have any write or read side-effects
         csr_we   = 1'b0;
         csr_read = 1'b0;
         mret     = 1'b1;  // signal a return from machine mode
       end
-      DRET: begin
-        if (CVA6Cfg.DebugEn) begin
+      default: begin
+        if (CVA6Cfg.RVS && csr_op_i == SRET) begin
+          // the return should not have any write or read side-effects
+          csr_we   = 1'b0;
+          csr_read = 1'b0;
+          sret     = 1'b1;  // signal a return from supervisor mode
+        end else if (CVA6Cfg.DebugEn && csr_op_i == DRET) begin
           // the return should not have any write or read side-effects
           csr_we   = 1'b0;
           csr_read = 1'b0;
           dret     = 1'b1;  // signal a return from debug mode
+        end else begin
+          csr_we   = 1'b0;
+          csr_read = 1'b0;
         end
-      end
-      default: begin
-        csr_we   = 1'b0;
-        csr_read = 1'b0;
       end
     endcase
     // if we are violating our privilges do not update the architectural state
@@ -1344,7 +1340,7 @@ module csr_regfile
         privilege_violation = 1'b1;
       end
       // check access to debug mode only CSRs
-      if ((CVA6Cfg.DebugEn && csr_addr_i[11:4] == 8'h7b) || (!CVA6Cfg.DebugEn && csr_addr_i[11:4] == 8'h7b && !debug_mode_q)) begin
+      if (csr_addr_i[11:4] == 8'h7b && !debug_mode_q) begin
         privilege_violation = 1'b1;
       end
       // check counter-enabled counter CSR accesses
