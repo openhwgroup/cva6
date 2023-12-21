c8973c77d011ffea4f749e6feb6477af9a0683eb
Bruno Sa <bruno.vilaca.sa@gmail.com>
2023-12-21 12:56:36 +0000

    hyp: add keyword automatic to sv39x4 aux functions
    Signed-off-by: Bruno Sa <bruno.vilaca.sa@gmail.com>

--- core/include/ariane_pkg.sv
+++ core/include/ariane_pkg.sv
@@ -760,13 +760,13 @@ package ariane_pkg;
   // ----------------------
 
   // checks if final translation page size is 1G when H-extension is enabled
-  function logic is_trans_1G(input logic s_st_enbl, input logic g_st_enbl, input logic is_s_1G,
+  function automatic logic is_trans_1G(input logic s_st_enbl, input logic g_st_enbl, input logic is_s_1G,
                              input logic is_g_1G);
     return (((is_s_1G && s_st_enbl) || !s_st_enbl) && ((is_g_1G && g_st_enbl) || !g_st_enbl));
   endfunction : is_trans_1G
 
   // checks if final translation page size is 2M when H-extension is enabled
-  function logic is_trans_2M(input logic s_st_enbl, input logic g_st_enbl, input logic is_s_1G,
+  function automatic logic is_trans_2M(input logic s_st_enbl, input logic g_st_enbl, input logic is_s_1G,
                              input logic is_s_2M, input logic is_g_1G, input logic is_g_2M);
     return  (s_st_enbl && g_st_enbl) ? 
                 ((is_s_2M && (is_g_1G || is_g_2M)) || (is_g_2M && (is_s_1G || is_s_2M))) :
@@ -774,7 +774,7 @@ package ariane_pkg;
   endfunction : is_trans_2M
 
   // computes the paddr based on the page size, ppn and offset
-  function logic [(riscv::GPLEN-1):0] make_gpaddr(
+  function automatic logic [(riscv::GPLEN-1):0] make_gpaddr(
       input logic s_st_enbl, input logic is_1G, input logic is_2M,
       input logic [(riscv::VLEN-1):0] vaddr, input riscv::pte_t pte);
     logic [(riscv::GPLEN-1):0] gpaddr;
