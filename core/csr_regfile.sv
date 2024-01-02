89d2d74fdcb68fe50f783953d26eef34c35f1601
JeanRochCoulon <jean-roch.coulon@thalesgroup.com>
2024-01-02 23:25:50 +0100

    Fix inhibit impact on MCYCLE csr

--- core/csr_regfile.sv
+++ core/csr_regfile.sv
@@ -569,7 +569,7 @@ module csr_regfile
       instret_d = instret;
       // increment the cycle count
       if (!mcountinhibit_q[0]) cycle_d = cycle_q + 1'b1;
-      else cycle_d = instret;
+      else cycle_d = cycle_q;
     end
 
     eret_o                  = 1'b0;
