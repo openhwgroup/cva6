5cebb458ab00b43b97a0ef5e9cbbf32609369920
JeanRochCoulon <jean-roch.coulon@thalesgroup.com>
2024-01-03 17:26:19 +0100

    Update report_benchmark.py

--- .gitlab-ci/scripts/report_benchmark.py
+++ .gitlab-ci/scripts/report_benchmark.py
@@ -18,7 +18,7 @@ iterations = None
 # Will fail if the number of cycles is different from this one
 valid_cycles = {
     'dhrystone': 217900,
-    'coremark': 665193,
+    'coremark': 683117,
 }
 
 for arg in sys.argv[1:]:
