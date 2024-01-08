b2365559f7efb3e7c95980a2a2c0dc37aac3424f
Caaliph Andriamisaina <caaliph.andriamisaina@cea.fr>
2024-01-08 11:04:45 +0100

    Modify ariane_pkg.sv following issue #1726. This issue was encountered during ASIC synthesis with DC version P-2019.03.

--- core/include/ariane_pkg.sv
+++ core/include/ariane_pkg.sv
@@ -166,7 +166,7 @@ package ariane_pkg;
   // maximum instructions we can fetch on one request (we support compressed instructions)
   localparam int unsigned INSTR_PER_FETCH = RVC == 1'b1 ? (FETCH_WIDTH / 16) : 1;
   localparam int unsigned LOG2_INSTR_PER_FETCH = RVC == 1'b1 ? $clog2(
-      ariane_pkg::INSTR_PER_FETCH
+      INSTR_PER_FETCH
   ) : 1;
 
   // ---------------
