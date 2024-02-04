22ef4f764c8102242f85f3f2f8b59963a4a2d799
Jean-Roch Coulon <jean-roch.coulon@thalesgroup.com>
2024-02-04 19:43:27 +0100

    port_builder generates the table of ports

---
+++ docs/04_cv32a6_design/source/port_frontend.rst
@@ -0,0 +1,139 @@
+..
+   Copyright 2021 Thales DIS design services SAS
+   Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0
+   You may obtain a copy of the License at https://solderpad.org/licenses/
+
+   Original Author: Jean-Roch COULON - Thales
+
+.. _CV32A6_FRONTEND:
+
+.. list-table:: FRONTEND interface signals
+   :header-rows: 1
+
+   * - Signal
+     - IO
+     - connection
+     - Type
+     - Description
+
+   * - ``clk_i``
+     - in
+     - SUBSYSTEM
+     - logic
+     - Subsystem Clock
+
+   * - ``rst_ni``
+     - in
+     - SUBSYSTEM
+     - logic
+     - Asynchronous reset active low
+
+   * - ``flush_i``
+     - in
+     - CONTROLLER
+     - logic
+     - Fetch flush request
+
+   * - ``flush_bp_i``
+     - in
+     - zero
+     - logic
+     - flush branch prediction
+
+   * - ``halt_i``
+     - in
+     - none
+     - logic
+     - none
+
+   * - ``debug_mode_i``
+     - in
+     - CSR
+     - logic
+     - Debug mode state
+
+   * - ``boot_addr_i``
+     - in
+     - SUBSYSTEM
+     - logic [riscv::VLEN-1:0]
+     - Next PC when reset
+
+   * - ``resolved_branch_i``
+     - in
+     - none
+     - bp_resolve_t
+     - none
+
+   * - ``set_pc_commit_i``
+     - in
+     - CONTROLLER
+     - logic
+     - Set the PC coming from COMMIT as next PC
+
+   * - ``pc_commit_i``
+     - in
+     - COMMIT
+     - logic [riscv::VLEN-1:0]
+     - Next PC when flushing pipeline
+
+   * - ``epc_i``
+     - in
+     - CSR
+     - logic [riscv::VLEN-1:0]
+     - Next PC when returning from exception
+
+   * - ``eret_i``
+     - in
+     - CSR
+     - logic
+     - Return from exception event
+
+   * - ``trap_vector_base_i``
+     - in
+     - CSR
+     - logic [riscv::VLEN-1:0]
+     - Next PC when jumping into exception
+
+   * - ``ex_valid_i``
+     - in
+     - none
+     - logic
+     - none
+
+   * - ``set_debug_pc_i``
+     - in
+     - CSR
+     - logic
+     - Debug event
+
+   * - ``icache_dreq_o``
+     - out
+     - CACHES
+     - icache_dreq_t
+     - Handshake between CACHE and FRONTEND (fetch)
+
+   * - ``icache_dreq_i``
+     - in
+     - CACHES
+     - icache_drsp_t
+     - Handshake between CACHE and FRONTEND (fetch)
+
+   * - ``fetch_entry_o``
+     - out
+     - DECODE
+     - fetch_entry_t
+     - Handshake's data between fetch and decode
+
+   * - ``fetch_entry_valid_o``
+     - out
+     - DECODE
+     - logic
+     - Handshake's valid between fetch and decode
+
+   * - ``fetch_entry_ready_i``
+     - in
+     - DECODE
+     - logic
+     - Handshake's ready between fetch and decode
---
+++ docs/scripts/table_port_builder.py
@@ -0,0 +1,88 @@
+# Copyright 2024 Thales DIS France SAS
+# 
+# Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0
+# You may obtain a copy of the License at https://solderpad.org/licenses/
+# 
+# Original Author: Jean-Roch COULON - Thales
+
+#!/usr/bin/python3
+
+import re
+import sys
+import os
+import collections
+import argparse
+
+class portIO:
+    def __init__(
+        self,
+        name0,
+        direction0,
+        type0,
+        comment0,
+        connection0,
+    ):
+        self.name0 = name0
+        self.direction0 = direction0
+        self.type0 = type0
+        self.comment0 = comment0
+        self.connection0 = connection0
+
+
+if __name__ == "__main__":
+
+    file0 = []
+    file0.append(["FRONTEND", "../core/frontend/frontend.sv", "./04_cv32a6_design/source/port_frontend.rst"])
+
+    for i in file0:
+		[module, filein, fileout] = i
+		print("Input file " + filein)
+		print("Output file " + fileout)
+		port = []
+		with open(filein, "r") as fin:
+			comment0 = "none"
+			connection0 = "none"
+			for l1 in fin:
+				e = re.match("^ +(?:(in|out))put ([\S]*(?: [\S]*|)) ([\S]*)\n", l1)
+				d = re.match("^ +\/\/ (.*) - ([\S]*)\n", l1)
+				if d:
+					comment0 = d.group(1)
+					connection0 = d.group(2)
+				if e:
+					name = e.group(3)
+					name = name.split(",")
+					# print(l1)
+					# print(name[0], e.group(1), e.group(2), comment0, connection0)
+					port.append(portIO(name[0], e.group(1), e.group(2), comment0, connection0))
+					comment0 = "none"
+					connection0 = "none"
+
+		# for i in range(len(port)):
+			# print(port[i][0], port[i][1], port[i][2], port[i][3], port[i][4])
+			
+		with open(fileout, "w") as fout:
+			fout.write("..\n")
+			fout.write("   Copyright 2021 Thales DIS design services SAS\n")
+			fout.write("   Licensed under the Solderpad Hardware Licence, Version 2.0 (the \"License\");\n")
+			fout.write("   you may not use this file except in compliance with the License.\n")
+			fout.write("   SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0\n")
+			fout.write("   You may obtain a copy of the License at https://solderpad.org/licenses/\n\n")
+			fout.write("   Original Author: Jean-Roch COULON - Thales\n\n")
+			fout.write(".. _CV32A6_%s:\n\n" % (module))
+			fout.write(".. list-table:: %s interface signals\n" % (module))
+			fout.write("   :header-rows: 1\n")
+			fout.write("\n")
+			fout.write("   * - Signal\n")
+			fout.write("     - IO\n")
+			fout.write("     - connection\n")
+			fout.write("     - Type\n")
+			fout.write("     - Description\n")
+			for i in range(len(port)):
+				fout.write("\n")
+				fout.write("   * - ``%s``\n" % (port[i].name0))
+				fout.write("     - %s\n" % (port[i].direction0))
+				fout.write("     - %s\n" % (port[i].connection0))
+				fout.write("     - %s\n" % (port[i].type0))
+				fout.write("     - %s\n" % (port[i].comment0))
