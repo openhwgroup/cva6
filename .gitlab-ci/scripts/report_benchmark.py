336e2924f4ef89fb35479036115eef4725c5cfe8
JeanRochCoulon <jean-roch.coulon@thalesgroup.com>
2024-02-05 16:58:56 +0100

    Fine tune the Coremark expected result after PR optimization

--- .gitlab-ci/scripts/report_benchmark.py
+++ .gitlab-ci/scripts/report_benchmark.py
@@ -18,7 +18,7 @@ iterations = None
 # Will fail if the number of cycles is different from this one
 valid_cycles = {
     'dhrystone': 217900,
-    'coremark': 665193,
+    'coremark': 664736,
 }
 
 for arg in sys.argv[1:]:
