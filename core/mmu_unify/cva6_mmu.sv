80a0868ac8056578011c0030ef1ebbab3313cec9
AngelaGonzalezMarino <gonzalezmarinoangela@gmail.com>
2024-02-19 13:00:46 +0100

    linting

--- core/mmu_unify/cva6_mmu.sv
+++ core/mmu_unify/cva6_mmu.sv
@@ -400,9 +400,7 @@ module cva6_mmu
                 };
                 end
             end
-
             icache_areq_o.fetch_valid = 1'b0;
-
             // ---------
             // ITLB Hit
             // --------
