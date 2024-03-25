e4d2a917aa0685a29d02b53bad106cea914563c2
Zbigniew Chamski <zbigniew.chamski@thalesgroup.com>
2024-03-25 12:11:44 +0100

    [impl-def] Rename spec inventory Yaml files.  Update example accordingly.

--- docs/impl_defined/example.py
+++ docs/impl_defined/example.py
@@ -27,8 +27,8 @@ import variconf
 cv32a65x_conf = variconf.WConf(ImplSpecificBehaviors)
 
 # Load the 'bootstrapped' descrition of impl-defined behaviors in RV ISA spec vol. 2
-# (result of programmaticvally adding missing tags to the original file '..../implement.yaml').
-cv32a65x_conf.load_file('spec_inventory/vol2priv/implement-bootstrapped.yaml')
+# (result of programmaticvally adding missing tags to the original file '..../implement-handmade.yaml').
+cv32a65x_conf.load_file('spec_inventory/vol2priv/implement.yaml')
 
 # Print entry #15 of the 'behaviors' list as internal object (a dictionary).
 behavior_15 = cv32a65x_conf.get().behaviors[15]
