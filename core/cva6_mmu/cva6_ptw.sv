18c9efcbab338407a1cd0dd37ec3d4cb9ad5e345
AngelaGonzalezMarino <135128652+AngelaGonzalezMarino@users.noreply.github.com>
2024-03-25 20:14:15 +0100

    Update core/cva6_mmu/cva6_ptw.sv
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- core/cva6_mmu/cva6_ptw.sv
+++ core/cva6_mmu/cva6_ptw.sv
@@ -583,7 +583,7 @@ module cva6_ptw
       // if not, go back to idle
       if (((state_q inside {PTE_LOOKUP, WAIT_RVALID}) && !data_rvalid_q) || ((state_q == WAIT_GRANT) && req_port_i.data_gnt))
         state_d = WAIT_RVALID;
-      else // state_d = USE_SHARED_TLB == 1 ? LATENCY : IDLE;
+      else  // state_d = USE_SHARED_TLB == 1 ? LATENCY : IDLE;
         state_d = LATENCY;
     end
   end
