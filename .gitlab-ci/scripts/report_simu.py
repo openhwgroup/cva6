5c7ddcbcc522b4691864145f26d891963e459b49
valentinThomazic <valentin.thomazic@thalesgroup.com>
2024-04-03 18:03:47 +0200

    Fix log naming and dashboard improvements (#2001)

--- .gitlab-ci/scripts/report_simu.py
+++ .gitlab-ci/scripts/report_simu.py
@@ -15,8 +15,10 @@ import os
 with open(str(sys.argv[1]), 'r') as f:
     log = f.read()
 
+with_logs = os.environ.get("COLLECT_SIMU_LOGS") != None
+
 pattern = re.compile(
-    r'(?:\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} INFO     )Compiling (.*): .*(tests\S*)$[\s\S]*?^(?:\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} INFO     )Found matching ISS: (\S*)$[\s\S]*?^(?:\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} INFO     )Target: (\S*)$[\s\S]*?^(?:\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2}(?: INFO     ))ISA (\S*)$[\s\S]*?^(?:\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} (?:(?:INFO     )\[(\w*)\]: (\d*) matched(?:, (\d*) mismatch)?)|(?:^(?:\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2})(?: ERROR    )(\D{5})(?:.*)$))',
+    r'(?:\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} INFO     )(?:Processing regression test list : (?:.*)/testlist_(.*-.*)(?:.yaml), test: (\S*)$[\s\S]*?^(?:\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} INFO     )Compiling (.*):.*$|Compiling (.*): .*(tests\S*))$[\s\S]*?^(?:\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} INFO     )Found matching ISS: (\S*)$[\s\S]*?^(?:\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} INFO     )Target: (\S*)$[\s\S]*?^(?:\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2}(?: INFO     ))ISA (\S*)$[\s\S]*?^(?:\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2} (?:(?:INFO     )\[(\w*)\]: (\d*) matched(?:, (\d*) mismatch)?)|(?:^(?:\w{3}, \d{2} \w{3} \d{4} \d{2}:\d{2}:\d{2})(?: ERROR    )(\D{5})(?:.*)$))',
     re.MULTILINE)
 list_of_tests = pattern.findall(log)
 
@@ -24,32 +26,36 @@ metric = rb.TableStatusMetric('')
 
 metric.add_column("TARGET", "text")
 metric.add_column("ISA", "text")
-metric.add_column("TEST_TYPE", "text")
 metric.add_column("TEST", "text")
-metric.add_column("OUTPUT", "log")
-metric.add_column("TB_LOGS", "log")
-metric.add_column("DISASSEMBLY", "log")
+metric.add_column("TEST LIST", "text")
+
+if with_logs:
+    metric.add_column("OUTPUT", "log")
+    metric.add_column("TB LOGS", "log")
+    metric.add_column("DISASSEMBLY", "log")
 
 job_test_pass = 0
 job_test_total = 0
 
-logsPath = "logs/" + os.environ.get("CI_JOB_ID") + "/artifacts/logs/"
-
 for i in list_of_tests:
     job_test_total += 1
 
-    target = i[3]
-    isa = i[4]
-    testsuite = i[0]
-    test = i[1].split("/")[-1].split(".")[0]
+    target = i[6]
+    isa = i[7]
+    test = i[1] or i[4].split("/")[-1].split(".")[0]
+    testsuite = i[0] or "custom test"
+    test_type = i[2] or i[4]
 
-    output_log = logsPath + 'logfile.log.head'
-    tb_log = logsPath + test + "." + target + '.log.iss.head'
-    disassembly = logsPath + test + "." + target + '.csv.head'
+    if with_logs:
+        logsPath = "logs/" + os.environ.get("CI_JOB_ID") + "/artifacts/logs/"
+        output_log = logsPath + 'logfile.log.head'
+        tb_log = logsPath + test + "." + target + '.log.iss.head'
+        disassembly = logsPath + test + "." + target + '.csv.head'
+        col = [target, isa, test, testsuite, output_log, tb_log, disassembly]
+    else:
+        col = [target, isa, test, testsuite]
 
-    col = [target, isa, testsuite, test, output_log, tb_log, disassembly]
-
-    if i[5] == "PASSED":
+    if i[8] == "PASSED":
         metric.add_pass(*col)
         job_test_pass += 1
     else:
