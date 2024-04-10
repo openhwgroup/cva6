d6d3842341d42ce0b7915c3117fe5300cf088014
Florian Zaruba <florian@openhwgroup.org>
2024-04-10 10:44:39 +0200

    Update core/include/config_pkg.sv
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- core/include/config_pkg.sv
+++ core/include/config_pkg.sv
@@ -257,7 +257,7 @@ package config_pkg;
     int unsigned ICACHE_TAG_WIDTH;
     int unsigned ICACHE_LINE_WIDTH;
     int unsigned ICACHE_USER_LINE_WIDTH;
-    cache_type_t                 DCacheType;
+    cache_type_t DCacheType;
     int unsigned DCACHE_SET_ASSOC;
     int unsigned DCACHE_SET_ASSOC_WIDTH;
     int unsigned DCACHE_INDEX_WIDTH;
