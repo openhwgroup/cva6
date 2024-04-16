646e36f009265910c3bbb8784d1a2f92ac61c5b0
Florian Zaruba <florian@openhwgroup.org>
2024-04-16 16:51:41 +0200

    Update core/cva6_accel_first_pass_decoder_stub.sv
    Co-authored-by: Côme <124148386+cathales@users.noreply.github.com>

--- core/cva6_accel_first_pass_decoder_stub.sv
+++ core/cva6_accel_first_pass_decoder_stub.sv
@@ -10,8 +10,8 @@
 module cva6_accel_first_pass_decoder
   import ariane_pkg::*;
 #(
-    parameter type scoreboard_entry_t = logic,
-    parameter config_pkg::cva6_cfg_t CVA6Cfg = '0
+    parameter config_pkg::cva6_cfg_t CVA6Cfg = '0,
+    parameter type scoreboard_entry_t = logic
 ) (
     input  logic              [31:0] instruction_i,           // instruction from IF
     input  riscv::xs_t               fs_i,                    // floating point extension status
