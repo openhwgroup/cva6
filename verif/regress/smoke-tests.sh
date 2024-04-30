566e304dde803371258546aeb42307b222bb85b6
Coulon Jean Roch <jean-roch.coulon@thalesgroup.com>
2024-04-30 18:10:48 +0200

    Fix smoke-tests.sh

--- verif/regress/smoke-tests.sh
+++ verif/regress/smoke-tests.sh
@@ -67,7 +67,7 @@ python3 cva6.py --testlist=../tests/testlist_riscv-tests-cv32a60x-p.yaml --test 
 python3 cva6.py --testlist=../tests/testlist_riscv-arch-test-cv32a60x.yaml --test rv32im-cadd-01 --iss_yaml cva6.yaml --target cv32a65x --iss=$DV_SIMULATORS $DV_OPTS  --linker=../tests/riscv-arch-test/riscv-target/spike/link.ld
 python3 cva6.py --target cv32a65x --iss=$DV_SIMULATORS --iss_yaml=cva6.yaml --c_tests ../tests/custom/hello_world/hello_world.c --linker=../tests/custom/common/test.ld\
   --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc -I../tests/custom/env -I../tests/custom/common" $DV_OPTS
-# make -C ../.. clean
-# make clean_all
+make -C ../.. clean
+make clean_all
 
 cd -
