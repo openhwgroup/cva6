e7f7b3d024240a54642dae1f98599c85ea85e2d1
Jalali <110232072+AyoubJalali@users.noreply.github.com>
2024-05-21 10:13:46 +0000

    Fix issue #2027 Remove localparam (#2137)

--- verif/tb/core/uvma_cva6pkg_utils.sv
+++ verif/tb/core/uvma_cva6pkg_utils.sv
@@ -3,9 +3,9 @@ function st_core_cntrl_cfg cva6pkg_to_core_cntrl_cfg(st_core_cntrl_cfg base);
 
     automatic config_pkg::cva6_cfg_t CVA6Cfg = build_config_pkg::build_config(cva6_config_pkg::cva6_cfg);
 
-    $cast(base.xlen, cva6_config_pkg::CVA6ConfigXlen);
+    $cast(base.xlen, CVA6Cfg.XLEN);
 
-    base.ilen = cva6_config_pkg::CVA6ConfigXlen;
+    base.ilen = 32;
 
     base.ext_i_supported = 1;
     base.ext_a_supported = CVA6Cfg.RVA;
@@ -15,15 +15,15 @@ function st_core_cntrl_cfg cva6pkg_to_core_cntrl_cfg(st_core_cntrl_cfg base);
     base.ext_v_supported = CVA6Cfg.RVV;
     base.ext_f_supported = CVA6Cfg.RVF;
     base.ext_d_supported = CVA6Cfg.RVD;
-    base.ext_zba_supported = cva6_config_pkg::CVA6ConfigBExtEn;
-    base.ext_zbb_supported = cva6_config_pkg::CVA6ConfigBExtEn;
-    base.ext_zbc_supported = cva6_config_pkg::CVA6ConfigBExtEn;
-    base.ext_zbe_supported = cva6_config_pkg::CVA6ConfigBExtEn;
+    base.ext_zba_supported = CVA6Cfg.RVB;
+    base.ext_zbb_supported = CVA6Cfg.RVB;
+    base.ext_zbc_supported = CVA6Cfg.RVB;
+    base.ext_zbe_supported = CVA6Cfg.RVB;
     base.ext_zbf_supported = 0;
     base.ext_zbm_supported = 0;
     base.ext_zbp_supported = 0;
     base.ext_zbr_supported = 0;
-    base.ext_zbs_supported = cva6_config_pkg::CVA6ConfigBExtEn;
+    base.ext_zbs_supported = CVA6Cfg.RVB;
     base.ext_zbt_supported = 0;
     base.ext_zcb_supported = CVA6Cfg.RVZCB;
     base.ext_zifencei_supported = 1;
