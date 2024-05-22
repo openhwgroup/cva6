2271192d116c9f6c20c5c81effa357a830f08a58
Asmaa-Kassimi <asmaa.kassimi@external.thalesgroup.com>
2024-05-22 13:10:40 +0200

    Add a python script to check spyglass lint progress and display summary.rpt reference errors.
    Signed-off-by: Asmaa-Kassimi <asmaa.kassimi@external.thalesgroup.com>

--- .gitlab-ci/scripts/report_spyglass_lint.py
+++ .gitlab-ci/scripts/report_spyglass_lint.py
@@ -13,17 +13,40 @@ import os
 import re
 import sys
 import yaml
+
 import report_builder as rb
 from pprint import pprint
 
+def extract_info (summary_rpt):
+	pattern = re.compile(r'(WARNING|ERROR|INFO)\s+([\w.-]+)\s+(\d+)\s+(.+)$')
+	info_list = []
 
-log_path = str(sys.argv[1])
-with open(log_path, 'r') as f:
-    log = f.read()
+	with open(str(sys.argv[1]), "r") as f:
+		lines = f.readlines()
+
+	for line in lines:
+		match = pattern.match(line)
+		if match:
+			severity = match.group(1)
+			rule_name = match.group(2)
+			count = match.group(3)
+			short_help = match.group(4)
+
+			info_list.append((severity, rule_name, count, short_help))
+
+	return info_list
+
+def print_table(info_list):
+	print("{:<10} {:<20} {:<10} {:<50}".format('Severity','Rule Name', 'Count', 'Short Help'))
+	print("=" * 81)
+
+	for info in info_list:
+		if info[0] in ['WARNING', 'ERROR','INFO']:
+			print("{:<10} {:<20} {:<10} {:<50}".format(*info))
 
 def check_spyglass(spyglass_log_path):
 	# Read the spyglass log files
-	with open(str(sys.argv[1]), "r") as f:
+	with open(str(sys.argv[2]), "r") as f:
 		log = f.read()
 
 	# Define patterns to search for in the logs
@@ -36,14 +59,24 @@ def check_spyglass(spyglass_log_path):
 	else:
 		print("Spyglass lint check failed")
 
-if __name__ == "__main__":
-	if len(sys.argv) != 2:
-		prinf("Usage: python script <spyglass log file>")
-		sys.exit(1)
-	log_path = sys.argv[1]
 
-	if not os.path.exists(log_path):
+if __name__ == "__main__":
+	if len(sys.argv) != 3:
+		print("Usage: python script.py <summary_file_path>  <spyglass_log_file>")
+		sys.exit(1)
+	summary_rpt = sys.argv[1]
+	spyglass_log_path = sys.argv[2]
+
+	if not os.path.exists(summary_rpt):
+		print("Summary file does not exist.")
+		sys.exit(1)
+
+	if not os.path.exists(spyglass_log_path):
 		print("Spyglass log file does not exist.")
 		sys.exit(1)
 
-	check_spyglass(log_path)
+	check_spyglass(spyglass_log_path)
+
+	info_list = extract_info(summary_rpt)
+
+	print_table(info_list)
