aab8c631293fb9ff6f680b85273e5cfcb3e441ee
Mario <mario@openhwgroup.org>
2024-05-30 12:06:35 +0000

    Fix typo

--- verif/env/uvme/uvme_cva6_sb.sv
+++ verif/env/uvme/uvme_cva6_sb.sv
@@ -171,7 +171,7 @@ function void uvme_cva6_sb_c::create_sbs();
    // TODO Implement uvme_cva6_sb_c::create_sbs()
    //      Ex: egress_sb  = uvme_cva6_sb_simplex_c::type_id::create("egress_sb" , this);
    //          ingress_sb = uvme_cva6_sb_simplex_c::type_id::create("ingress_sb", this);
-    if (cfg.tandem_enable)
+    if (cfg.tandem_enabled)
         m_rvfi_scoreboard = uvmc_rvfi_scoreboard_c#(ILEN,XLEN)::type_id::create("m_rvfi_scoreboard", this);
 
 endfunction : create_sbs
