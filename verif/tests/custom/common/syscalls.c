34299c65e4ce194520cf4fa4ceab759d24fc9759
mgouttenoire <mathieu.gouttenoire@thalesgroup.com>
2024-06-11 09:16:33 +0200

    Fix sprintf type warning

--- verif/tests/custom/common/syscalls.c
+++ verif/tests/custom/common/syscalls.c
@@ -154,7 +154,7 @@ void _init(int cid, int nc)
   char* pbuf = buf;
   for (int i = 0; i < NUM_COUNTERS; i++)
     if (counters[i])
-      pbuf += sprintf(pbuf, "%s = %d\n", counter_names[i], counters[i]);
+      pbuf += sprintf(pbuf, "%s = %zu\n", counter_names[i], counters[i]);
   if (pbuf != buf)
     printstr(buf);
 
