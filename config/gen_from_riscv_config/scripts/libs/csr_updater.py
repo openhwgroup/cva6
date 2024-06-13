a452dd94d7849029a355fd9cfa18193a50313957
Abdessamii Oukalrazqou <abdessamii.oukalrazqou@external.thalesgroup.com>
2024-06-13 14:12:16 +0200

    [gen_from_riscv_config]modify csr updater.py /fix-2191 , modify csr_updater.yaml

--- config/gen_from_riscv_config/scripts/libs/csr_updater.py
+++ config/gen_from_riscv_config/scripts/libs/csr_updater.py
@@ -1,60 +1,37 @@
-# Copyright 2024 Thales DIS France SAS
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# Original Author: Oukalrazqou Abdessamii
-""" Module is used to update csr based on yaml file called  csr updater """
 import re
 import yaml
-
 
 def csr_recursive_update(original_dict, csr_update):
     """
     Gets the data of the RISC-V Config Yaml file and
-    update the value of sub key in RISC-V Config Yaml file
-    (ex: reset-val , shadow_type)
-    :param original_dict : parsed data of RISC-V Config Yaml file
-           csr_update : parsed data of  CSR updater
+    updates the value of sub key in the RISC-V Config Yaml file
+    (ex: reset-val, shadow_type)
+    :param original_dict: parsed data of the RISC-V Config Yaml file
+    :param csr_update: parsed data of the CSR updater
     :return: data of RISC-V Config Yaml file updated
     """
     for key, value in csr_update.items():
-        if key in original_dict.keys():
-            if isinstance(value, dict):
-                csr_recursive_update(original_dict[key], value)
-            elif isinstance(value, bool):
-                if isinstance(original_dict[key], dict):
-                    for k in original_dict[key]:
-                        print(k)
-                        if isinstance(original_dict[key][k], dict):
-                 
-                            for sub_key in original_dict[key][k]:
-                                original_dict[key][k][sub_key] = value
-                        else:
-                            original_dict[key][k] = value
-                else:
+        if key in original_dict:
+            if isinstance(value, dict) and isinstance(original_dict[key], dict):
+                # If both are dicts, recurse
+                if key == 'type':
+                    # Replace the entire type dictionary
                     original_dict[key] = value
+                else:
+                    csr_recursive_update(original_dict[key], value)
             else:
+                # Replace the original value with the update value
                 original_dict[key] = value
-
-
 def csr_formatter(srcfile, modifile):
-    # Read original dictionary from YAML  Source file
+    # Read original dictionary from YAML source file
     with open(srcfile, "r", encoding="utf-8") as file:
         original_dict = yaml.safe_load(file)
+
     updated_values = {}
     if modifile is not None:
         with open(modifile, "r", encoding="utf-8") as file:
             updated_values = yaml.safe_load(file)
+    
     # Update original_dict with values from updated_values recursively
     csr_recursive_update(original_dict["hart0"], updated_values)
 
