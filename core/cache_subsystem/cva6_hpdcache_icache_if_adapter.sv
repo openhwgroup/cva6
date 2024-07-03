9f663bd0b7c7c2d908480a08f79bea20b6026343
takeshiho0531 <kawadatakeshiho@gmail.com>
2024-07-03 18:13:58 +0900

    feat(&wip): add updated cva6_hpdcache_icache_if_adapter

--- core/cache_subsystem/cva6_hpdcache_icache_if_adapter.sv
+++ core/cache_subsystem/cva6_hpdcache_icache_if_adapter.sv
@@ -24,7 +24,7 @@ module cva6_hpdcache_icache_if_adapter
     parameter type fetch_drsp_t = logic,
     parameter type obi_fetch_req_t = logic,
     parameter type obi_fetch_rsp_t = logic,
-    parameter logic [CVA6Cfg.MEM_TID_WIDTH-1:0] RdTxId = 0
+    parameter logic [CVA6Cfg.MEM_TID_WIDTH-1:0] RdTxId = 0 // TODO
 )
 //  }}}
 
@@ -39,10 +39,10 @@ module cva6_hpdcache_icache_if_adapter
     input hpdcache_req_sid_t hpdcache_req_sid_i,
 
     //  Request/response ports from/to the CVA6 core
-    input fetch_dreq_t fetch_dreq_i,
-    output fetch_drsp_t fetch_dreq_o,
-    input obi_fetch_req_t obi_fetch_req_i,
-    output obi_fetch_rsp_t obi_fetch_rsp_o,
+    input fetch_dreq_t dreq_i,
+    output fetch_drsp_t dreq_o,
+    input obi_fetch_req_t fetch_obi_req_i,
+    output obi_fetch_rsp_t fetch_obi_rsp_o,
 
     //  Request port to the L1 Dcache
     output logic                        hpdcache_req_valid_o,
@@ -58,9 +58,7 @@ module cva6_hpdcache_icache_if_adapter
 );
   //  }}}
 
-  //  Internal nets and registers
-  //  {{{
-  logic hpdcache_req_is_uncacheable;
+  localparam ICACHE_OFFSET_WIDTH = $clog2(CVA6Cfg.ICACHE_LINE_WIDTH / 8);
   localparam int ICACHE_CL_SIZE = $clog2(CVA6Cfg.ICACHE_LINE_WIDTH / 8);
   localparam int ICACHE_WORD_SIZE = 3;
   localparam int ICACHE_MEM_REQ_CL_SIZE =
@@ -68,57 +66,67 @@ module cva6_hpdcache_icache_if_adapter
       $clog2(
       CVA6Cfg.AxiDataWidth / 8
   ) : ICACHE_CL_SIZE;
+
+  //  Internal nets and registers
+  //  {{{
+  logic hpdcache_req_is_uncacheable;
+  logic va_transferred_q, va_is_transferred_d;
+
+  always_ff @( posedge clk_i or negedge rst_ni) begin : transferred_state_magnegement
+    if (!rst_ni) begin
+        va_transferred_q <= '0;
+    end else begin
+        va_transferred_q <= va_is_transferred_d;
+    end
+  end
+
+  assign va_transferred_d = dreq_i.req
   //  }}}
 
   //  Request forwarding
   //  {{{
-  //  LOAD request
-  //  {{{
-
-  assign hpdcache_req_is_uncacheable = !config_pkg::is_inside_cacheable_regions(
-      CVA6Cfg,
-      {
+    assign hpdcache_req_is_uncacheable = !config_pkg::is_inside_cacheable_regions(
+          CVA6Cfg,
+          {
         {64 - CVA6Cfg.PLEN{1'b0}},
-        obi_fetch_req_i.a.addrr[CVA6Cfg.ICACHE_TAG_WIDTH+CVA6Cfg.ICACHE_INDEX_WIDTH-1:CVA6Cfg.ICACHE_INDEX_WIDTH],
+        fetch_obi_req_i.a.addr[CVA6Cfg.ICACHE_TAG_WIDTH+CVA6Cfg.ICACHE_INDEX_WIDTH-1:CVA6Cfg.ICACHE_INDEX_WIDTH],
         {CVA6Cfg.ICACHE_INDEX_WIDTH{1'b0}}
-      }
-  );
+          }
+      );
 
-  //    Request forwarding
-  assign hpdcache_req_valid_o = fetch_dreq_i.data_req,
-      hpdcache_req_o.addr_offset = fetch_dreq_i.vaddr[CVA6Cfg.ICACHE_INDEX_WIDTH-1:3],
-      hpdcache_req_o.wdata = '0,
-      hpdcache_req_o.op = hpdcache_pkg::HPDCACHE_REQ_LOAD,
-      hpdcache_req_o.be = obi_fetch_req_i.a.data_be,
-      hpdcache_req_o.size = hpdcache_req_is_uncacheable ? ICACHE_WORD_SIZE : ICACHE_MEM_REQ_CL_SIZE,
-      hpdcache_req_o.sid = '0,
-      hpdcache_req_o.tid = RdTxId,  // TODO
-      hpdcache_req_o.need_rsp = 1'b1,
-      hpdcache_req_o.phys_indexed = 1'b0,
-      hpdcache_req_o.addr_tag = '0,  // unused on virtually indexed request
-      hpdcache_req_o.pma = '0;  // unused on virtually indexed request
+      //    Request forwarding
+    assign hpdcache_req_valid_o = dreq_i.req,
+          hpdcache_req_o.addr_offset = dreq_i.vaddr[CVA6Cfg.ICACHE_INDEX_WIDTH-1:ICACHE_OFFSET_WIDTH],
+          hpdcache_req_o.wdata = '0,
+          hpdcache_req_o.op = hpdcache_pkg::HPDCACHE_REQ_LOAD,
+          hpdcache_req_o.be = fetch_obi_req_i.a.be,
+          hpdcache_req_o.size = hpdcache_req_is_uncacheable ? ICACHE_WORD_SIZE : ICACHE_MEM_REQ_CL_SIZE, // TODO
+          hpdcache_req_o.sid = '0,
+          hpdcache_req_o.tid = RdTxId, // TODO 
+          hpdcache_req_o.need_rsp = 1'b1,
+          hpdcache_req_o.phys_indexed = 1'b0,
+          hpdcache_req_o.addr_tag = '0,  // unused on virtually indexed request
+          hpdcache_req_o.pma = '0;  // unused on virtually indexed request
 
-  assign hpdcache_req_abort_o = fetch_dreq_i.kill_req,
-      hpdcache_req_tag_o = obi_fetch_req_i.a.addrr[CVA6Cfg.ICACHE_TAG_WIDTH+CVA6Cfg.ICACHE_INDEX_WIDTH-1:CVA6Cfg.ICACHE_INDEX_WIDTH],
-      hpdcache_req_pma_o.uncacheable = hpdcache_req_is_uncacheable,
-      hpdcache_req_pma_o.io = 1'b0;
+    assign hpdcache_req_abort_o = dreq_i.kill_req || !(va_transferred_q && fetch_obi_req_i.req),
+          hpdcache_req_tag_o = fetch_obi_req_i.a.addr[CVA6Cfg.ICACHE_TAG_WIDTH+CVA6Cfg.ICACHE_INDEX_WIDTH-1:CVA6Cfg.ICACHE_INDEX_WIDTH],
+          hpdcache_req_pma_o.uncacheable = hpdcache_req_is_uncacheable,
+          hpdcache_req_pma_o.io = 1'b0;
 
-  //    Response forwarding
-  assign cva6_req_o.data_rvalid = hpdcache_rsp_valid_i,
-      cva6_req_o.data_rdata = hpdcache_rsp_i.rdata,
-      cva6_req_o.data_rid = hpdcache_rsp_i.tid,
-      cva6_req_o.data_gnt = hpdcache_req_ready_i;
+      //    Response forwarding
+    assign dreq_o.ready = hpdcache_rsp_valid_i,
+          dreq_o.invalid_data = hpdcache_req_ready_i; // TODO
 
-  assign fetch_obi_rsp_o.gnt = hpdcache_req_ready_i,  // TODO
-      fetch_obi_rsp_o.gntpar = !hpdcache_req_ready_i,  // TODO
-      fetch_obi_rsp_o.rvalid = hpdcache_rsp_valid_i,  // TODO
-      fetch_obi_rsp_o.rvalidpar = !hpdcache_rsp_valid_i,  // TODO
-      fetch_obi_rsp_o.r.rid = '0,
-      fetch_obi_rsp_o.r.r_optional.exokay = '0,
-      fetch_obi_rsp_o.r.r_optional.rchk = '0,
-      fetch_obi_rsp_o.r.err = '0,
+    assign fetch_obi_rsp_o.gnt = hpdcache_req_ready_i, // TODO
+      fetch_obi_rsp_o.gntpar = !hpdcache_req_ready_i, // TODO
+      fetch_obi_rsp_o.rvalid = hpdcache_req_ready_i, // TODO
+      fetch_obi_rsp_o.rvalidpar = !hpdcache_req_ready_i, // TODO
+      fetch_obi_rsp_o.r.rid = hpdcache_rsp_i.tid,
+      fetch_obi_rsp_o.r.r_optional.exokay = '0, // TODO
+      fetch_obi_rsp_o.r.r_optional.rchk = '0, // TODO
+      fetch_obi_rsp_o.r.err = hpdcache_rsp_i.error,
       fetch_obi_rsp_o.r.rdata = hpdcache_rsp_i.rdata,
-      fetch_obi_rsp_o.r.r_optional.ruser = '0;
+      fetch_obi_rsp_o.r.r_optional.ruser = '0; // TODO
   //  }}}
   //  }}}
 
