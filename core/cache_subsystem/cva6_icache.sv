f2a6bb5896c96016fe4843b9afc87269dd885daf
takeshiho0531 <kawadatakeshiho@gmail.com>
2024-07-04 20:23:33 +0900

    refac: align style

--- core/cache_subsystem/cva6_icache.sv
+++ core/cache_subsystem/cva6_icache.sv
@@ -425,9 +425,18 @@ module cva6_icache
         if (flush_d) begin
           state_d = IDLE;
           // we have a hit or an exception output valid result
-        end else if (((|cl_hit && cache_en_q && !inv_q) || dreq_i.kill_req)) begin
+        end else if (dreq_i.kill_req) begin
+          data_valid_obi = '1;  // just don't output in this case
           state_d = IDLE;
-          data_valid_obi = '1;  // just don't output in the case of dreq_i.kill_req
+          if (!mem_rtrn_vld_i) begin
+            dreq_o.ready = 1'b1;
+            if (dreq_i.req) begin
+              state_d = READ;
+            end
+          end
+        end else if (|cl_hit && cache_en_q && !inv_q) begin
+          data_valid_obi = '1;
+          state_d = IDLE;
           // we can accept another request
           // and stay here, but only if no inval is coming in
           // note: we are not expecting ifill return packets here...
