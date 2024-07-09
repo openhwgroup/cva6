ea6b73e52fea765d72453f1383c7b2b43a1edb0c
Côme Allart <come.allart@thalesgroup.com>
2024-07-09 14:19:44 +0200

    make cv32a65x superscalar

--- .gitlab-ci/scripts/report_benchmark.py
+++ .gitlab-ci/scripts/report_benchmark.py
@@ -18,7 +18,7 @@ iterations = None
 # Will fail if the number of cycles is different from this one
 valid_cycles = {
     'dhrystone': 217900,
-    'coremark': 686072,
+    'coremark': 549055,
 }
 
 for arg in sys.argv[1:]:
