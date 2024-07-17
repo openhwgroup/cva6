fee09d7bdda6944d26629cc311cea6a9082c924e
Asmaa-Kassimi <asmaa.kassimi@external.thalesgroup.com>
2024-07-17 17:54:51 +0200

    lsu unit condition with IS_XLEN64

--- core/load_store_unit.sv
+++ core/load_store_unit.sv
@@ -541,17 +541,22 @@ module load_store_unit
     data_misaligned = 1'b0;
 
     if (lsu_ctrl.valid) begin
-      case (lsu_ctrl.operation)
-        // double word
-        LD, SD, FLD, FSD,
-                AMO_LRD, AMO_SCD,
-                AMO_SWAPD, AMO_ADDD, AMO_ANDD, AMO_ORD,
-                AMO_XORD, AMO_MAXD, AMO_MAXDU, AMO_MIND,
-                AMO_MINDU, HLV_D, HSV_D: begin
-          if (CVA6Cfg.IS_XLEN64 && lsu_ctrl.vaddr[2:0] != 3'b000) begin
-            data_misaligned = 1'b1;
+      if (CVA6Cfg.IS_XLEN64) begin
+        case (lsu_ctrl.operation)
+          // double word
+          LD, SD, FLD, FSD,
+                  AMO_LRD, AMO_SCD,
+                  AMO_SWAPD, AMO_ADDD, AMO_ANDD, AMO_ORD,
+                  AMO_XORD, AMO_MAXD, AMO_MAXDU, AMO_MIND,
+                  AMO_MINDU, HLV_D, HSV_D: begin
+            if (lsu_ctrl.vaddr[2:0] != 3'b000) begin
+              data_misaligned = 1'b1;
+            end
           end
-        end
+          default: ;
+        endcase
+      end
+      case (lsu_ctrl.operation)
         // word
         LW, LWU, SW, FLW, FSW,
                 AMO_LRW, AMO_SCW,
