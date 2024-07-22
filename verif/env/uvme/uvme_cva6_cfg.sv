3fc61462d92665ea0b5d135bcfac8877b8a4053a
Alae-Eddine Ez-Zejjari <alae-eddine.ez-zejjari@external.thalesgroup.com>
2024-07-22 13:40:49 +0200

    Enable HPDcache in the UVM config
    Signed-off-by: Alae-Eddine Ez-Zejjari <alae-eddine.ez-zejjari@external.thalesgroup.com>

--- verif/env/uvme/uvme_cva6_cfg.sv
+++ verif/env/uvme/uvme_cva6_cfg.sv
@@ -142,7 +142,7 @@ class uvme_cva6_cfg_c extends uvma_core_cntrl_cfg_c;
       dm_halt_addr_valid      == 1;
       dm_exception_addr_valid == 1;
       nmi_addr_valid          == 1;
-      HPDCache_supported      == (CVA6Cfg.DCacheType == 2);
+      HPDCache_supported      == 1;
 
       DirectVecOnly           == CVA6Cfg.DirectVecOnly;
       TvalEn                  == CVA6Cfg.TvalEn;
