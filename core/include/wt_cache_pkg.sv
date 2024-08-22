c4b0f1d69ae0ba02f0d131c9c96881bac836d2c6
Arnau Bigas <arnau.bigas@bsc.es>
2024-08-22 14:44:29 -0700

    Updating CVA6 for OpenPiton to support HEAD
    Includes the following:
    - Flist and params
    - CVA6 ported  + fts added to latest HPDC version
    - Update cache subsystem for hpdc v5 + openpiton support
    
    Co-authored-by: Jonathan Balkind <jbalkind@ucsb.edu>
    Co-authored-by: Noelia Oliete Escuin <noelia.oliete@bsc.es>

--- core/include/wt_cache_pkg.sv
+++ core/include/wt_cache_pkg.sv
@@ -15,30 +15,7 @@
 // this is needed to propagate the
 // configuration in case Ariane is
 // instantiated in OpenPiton
-`ifdef PITON_ARIANE
-`include "l15.tmp.h"
-`include "define.tmp.h"
-`endif
-
 package wt_cache_pkg;
-
-  // these parameters need to coincide with the
-  // L1.5 parameterization, do not change
-`ifdef PITON_ARIANE
-
-`ifndef CONFIG_L15_ASSOCIATIVITY
-  `define CONFIG_L15_ASSOCIATIVITY 4
-`endif
-
-`ifndef TLB_CSM_WIDTH
-  `define TLB_CSM_WIDTH 33
-`endif
-
-  localparam L15_SET_ASSOC = `CONFIG_L15_ASSOCIATIVITY;
-  localparam L15_TLB_CSM_WIDTH = `TLB_CSM_WIDTH;
-`else
-  localparam L15_TLB_CSM_WIDTH = 33;
-`endif
 
   // FIFO depths of L15 adapter
   localparam ADAPTER_REQ_FIFO_DEPTH = 2;
@@ -69,45 +46,6 @@ package wt_cache_pkg;
     ICACHE_INV_REQ,   // no ack from the core required
     ICACHE_IFILL_ACK
   } icache_in_t;
-
-  // taken from iop.h in openpiton
-  // to l1.5 (only marked subset is used)
-  typedef enum logic [4:0] {
-    L15_LOAD_RQ    = 5'b00000,  // load request
-    L15_IMISS_RQ   = 5'b10000,  // instruction fill request
-    L15_STORE_RQ   = 5'b00001,  // store request
-    L15_ATOMIC_RQ  = 5'b00110,  // atomic op
-    //L15_CAS1_RQ     = 5'b00010, // compare and swap1 packet (OpenSparc atomics)
-    //L15_CAS2_RQ     = 5'b00011, // compare and swap2 packet (OpenSparc atomics)
-    //L15_SWAP_RQ     = 5'b00110, // swap packet (OpenSparc atomics)
-    L15_STRLOAD_RQ = 5'b00100,  // unused
-    L15_STRST_RQ   = 5'b00101,  // unused
-    L15_STQ_RQ     = 5'b00111,  // unused
-    L15_INT_RQ     = 5'b01001,  // interrupt request
-    L15_FWD_RQ     = 5'b01101,  // unused
-    L15_FWD_RPY    = 5'b01110,  // unused
-    L15_RSVD_RQ    = 5'b11111   // unused
-  } l15_reqtypes_t;
-
-  // from l1.5 (only marked subset is used)
-  typedef enum logic [3:0] {
-    L15_LOAD_RET               = 4'b0000,  // load packet
-    // L15_INV_RET                = 4'b0011, // invalidate packet, not unique...
-    L15_ST_ACK                 = 4'b0100,  // store ack packet
-    //L15_AT_ACK                 = 4'b0011, // unused, not unique...
-    L15_INT_RET                = 4'b0111,  // interrupt packet
-    L15_TEST_RET               = 4'b0101,  // unused
-    L15_FP_RET                 = 4'b1000,  // unused
-    L15_IFILL_RET              = 4'b0001,  // instruction fill packet
-    L15_EVICT_REQ              = 4'b0011,  // eviction request
-    L15_ERR_RET                = 4'b1100,  // unused
-    L15_STRLOAD_RET            = 4'b0010,  // unused
-    L15_STRST_ACK              = 4'b0110,  // unused
-    L15_FWD_RQ_RET             = 4'b1010,  // unused
-    L15_FWD_RPY_RET            = 4'b1011,  // unused
-    L15_RSVD_RET               = 4'b1111,  // unused
-    L15_CPX_RESTYPE_ATOMIC_RES = 4'b1110   // custom type for atomic responses
-  } l15_rtrntypes_t;
 
   // swap endianness in a 64bit word
   function automatic logic [63:0] swendian64(input logic [63:0] in);
