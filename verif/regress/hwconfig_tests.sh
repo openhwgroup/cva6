1d0076eec34c4d5a794669c9d1ed7eb82ca0b8ec
Jean-Roch Coulon <jean-roch.coulon@thalesgroup.com>
2024-10-17 23:20:53 +0200

    smoke-hwconfig: run with vcs-uvm and use return0 test to speed-up CI light stage timing execution

--- verif/regress/hwconfig_tests.sh
+++ verif/regress/hwconfig_tests.sh
@@ -21,12 +21,42 @@ source verif/regress/install-riscv-tests.sh
 source ./verif/sim/setup-env.sh
 
 if ! [ -n "$DV_SIMULATORS" ]; then
-  DV_SIMULATORS=veri-testharness,spike
+  DV_SIMULATORS=vcs-uvm
 fi
 
-cd verif/sim/
-python3 cva6.py --testlist=../tests/testlist_hwconfig.yaml --iss_yaml cva6.yaml --target hwconfig --isa=rv32imac --hwconfig_opts="$DV_HWCONFIG_OPTS" --iss=$DV_SIMULATORS
-make -C ../.. clean
-make clean_all
+if ! [ -n "$DV_TARGET" ]; then
+  DV_TARGET=cv32a65x
+fi
+
+make clean
+make -C verif/sim clean_all
+
+cd verif/sim
+
+srcA=(
+        ../tests/custom/common/syscalls.c
+        ../tests/custom/common/crt.S
+)
+cflags=(
+        -static
+        -mcmodel=medany
+        -fvisibility=hidden
+        -nostartfiles
+        -Oz -fno-inline
+        -Wno-implicit-function-declaration
+        -Wno-implicit-int
+        -I../tests/custom/env
+        -I../tests/custom/common
+        -I../tests/custom/dhrystone/
+        -DNOPRINT
+)
+
+python3 cva6.py \
+        --target "$DV_TARGET" \
+        --hwconfig_opts="$DV_HWCONFIG_OPTS" \
+        --iss="$DV_SIMULATORS" \
+        --iss_yaml=cva6.yaml \
+        --c_tests "../tests/custom/return0/return0.c" \
+        --gcc_opts "${srcA[*]} ${cflags[*]}"
 
 cd -
