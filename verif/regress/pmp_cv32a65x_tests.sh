792403071cd908888b91811f92118b7ac3a92bda
Olivier Betschi <olivier.betschi@fr.bosch.com>
2024-12-05 16:59:55 +0100

    Modify PMP scripts to be included into CI : add the script in gitlab-ci, update the sh script to run all the tests and add the missing parameter in cva6.py

--- verif/regress/pmp_cv32a65x_tests.sh
+++ verif/regress/pmp_cv32a65x_tests.sh
@@ -18,11 +18,6 @@ fi
 source ./verif/regress/install-verilator.sh
 source ./verif/regress/install-spike.sh
 
-# Install the required test suites
-source ./verif/regress/install-riscv-compliance.sh
-source ./verif/regress/install-riscv-tests.sh
-source ./verif/regress/install-riscv-arch-test.sh
-
 # Setup sim env
 source ./verif/sim/setup-env.sh
 
@@ -49,8 +44,7 @@ SIMULATOR_VCS=`echo $DV_SIMULATORS | grep -c vcs`
 SIMULATOR_XRUN=`echo $DV_SIMULATORS | grep -c xrun`
 
 export DV_OPTS="$DV_OPTS"
-export DV_OPTS="$DV_OPTS --iss_timeout 0"
-export DV_OPTS="$DV_OPTS --c_timeout 0 --asm_timeout 0 --elf_timeout 0"
+export DV_OPTS="$DV_OPTS --iss_timeout 2000"
 
 if [ $SIMULATOR_VCS == 1 ]; then
     export ISSCOMP_OPTS="$ISSCOMP_OPTS -debug_access+r"
@@ -67,19 +61,6 @@ export SPIKE_PARAMS="--help"
 
 export DEBUG_FILE=""
 
-
-# Select the test to run here:
-  TEST_NUMBER="#1#"
-# TEST_NUMBER="#2#"
-# TEST_NUMBER="#3#"
-# TEST_NUMBER="#4#"
-# TEST_NUMBER="#5#"
-# TEST_NUMBER="#6#"
-# TEST_NUMBER="#7#"
-# TEST_NUMBER="#8#"
-# TEST_NUMBER="#9#"
-
-
 TEST_TARGET="#cv32a65x#"
 
 cd verif/sim/
@@ -88,253 +69,228 @@ cd verif/sim/
 # ------------------------------------------------------------------------------
 #1# pmp_cv32a65x_granularity_test.S on cv32a65x
 
-if [ $TEST_NUMBER == "#1#" ] ; then
-    
-    echo "running #1# pmp_cv32a65x_granularity_test.S on cv32a65x with $DV_SIMULATORS"
-    
-    python3 cva6.py \
-            --target cv32a65x \
-            --iss_yaml=cva6.yaml \
-            --iss=$DV_SIMULATORS \
-            --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_granularity_test.S \
-            --uvm_test uvmt_cva6_pmp_test_c \
-            --linker=../tests/custom/common/test.ld \
-            --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
-                        -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
-                        -I../tests/custom/env -I../tests/custom/common" \
-            $DV_OPTS \
-            --debug="$DEBUG_FILE" \
-            --spike_params="$SPIKE_PARAMS" \
-            --issrun_opts="$ISSRUN_OPTS" \
-            --isscomp_opts="$ISSCOMP_OPTS"
+echo "running #1# pmp_cv32a65x_granularity_test.S on cv32a65x with $DV_SIMULATORS"
 
-    make -C ../.. clean
-    make clean_all
+python3 cva6.py \
+        --target cv32a65x \
+        --iss_yaml=cva6.yaml \
+        --iss=$DV_SIMULATORS \
+        --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_granularity_test.S \
+        --uvm_test uvmt_cva6_pmp_test_c \
+        --linker=../../config/gen_from_riscv_config/cv32a65x/linker/link.ld \
+        --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
+                    -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
+                    -I../tests/custom/env -I../tests/custom/common" \
+        $DV_OPTS \
+        --debug="$DEBUG_FILE" \
+        --spike_params="$SPIKE_PARAMS" \
+        --issrun_opts="$ISSRUN_OPTS" \
+        --isscomp_opts="$ISSCOMP_OPTS"
+
+make -C ../.. clean
+make clean_all
     
-fi
 
 # ------------------------------------------------------------------------------
 #2# pmp_cv32a65x_exact_csrr_test.S on cv32a65x
 
-if [ $TEST_NUMBER == "#2#" ] ; then
-    
-    echo "running #2# pmp_cv32a65x_exact_csrr_test.S on cv32a65x with $DV_SIMULATORS"
-    
-    python3 cva6.py \
-            --target cv32a65x \
-            --iss_yaml=cva6.yaml \
-            --iss=$DV_SIMULATORS \
-            --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_exact_csrr_test.S \
-            --uvm_test uvmt_cva6_pmp_test_c \
-            --linker=../tests/custom/common/test.ld \
-            --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
-                        -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
-                        -I../tests/custom/env -I../tests/custom/common" \
-            $DV_OPTS \
-            --debug="$DEBUG_FILE" \
-            --spike_params="$SPIKE_PARAMS" \
-            --issrun_opts="$ISSRUN_OPTS" \
-            --isscomp_opts="$ISSCOMP_OPTS"
+echo "running #2# pmp_cv32a65x_exact_csrr_test.S on cv32a65x with $DV_SIMULATORS"
 
-    make -C ../.. clean
-    make clean_all
-    
-fi
+python3 cva6.py \
+        --target cv32a65x \
+        --iss_yaml=cva6.yaml \
+        --iss=$DV_SIMULATORS \
+        --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_exact_csrr_test.S \
+        --uvm_test uvmt_cva6_pmp_test_c \
+        --linker=../../config/gen_from_riscv_config/cv32a65x/linker/link.ld \
+        --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
+                    -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
+                    -I../tests/custom/env -I../tests/custom/common" \
+        $DV_OPTS \
+        --debug="$DEBUG_FILE" \
+        --spike_params="$SPIKE_PARAMS" \
+        --issrun_opts="$ISSRUN_OPTS" \
+        --isscomp_opts="$ISSCOMP_OPTS"
 
+make -C ../.. clean
+make clean_all
+    
 # ------------------------------------------------------------------------------
 #3# pmp_cv32a65x_lsu_tor_test.S on cv32a65x
 
-if [ $TEST_NUMBER == "#3#" ] ; then
-    
-    echo "running #3# pmp_cv32a65x_lsu_tor_test.S on cv32a65x with $DV_SIMULATORS"
-    
-    python3 cva6.py \
-            --target cv32a65x \
-            --iss_yaml=cva6.yaml \
-            --iss=$DV_SIMULATORS \
-            --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_lsu_tor_test.S \
-            --uvm_test uvmt_cva6_pmp_test_c \
-            --linker=../tests/custom/common/test.ld \
-            --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
-                        -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
-                        -I../tests/custom/env -I../tests/custom/common" \
-            $DV_OPTS \
-            --debug="$DEBUG_FILE" \
-            --spike_params="$SPIKE_PARAMS" \
-            --issrun_opts="$ISSRUN_OPTS" \
-            --isscomp_opts="$ISSCOMP_OPTS" 
+echo "running #3# pmp_cv32a65x_lsu_tor_test.S on cv32a65x with $DV_SIMULATORS"
 
-    make -C ../.. clean
-    make clean_all
-    
-fi
+python3 cva6.py \
+        --target cv32a65x \
+        --iss_yaml=cva6.yaml \
+        --iss=$DV_SIMULATORS \
+        --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_lsu_tor_test.S \
+        --uvm_test uvmt_cva6_pmp_test_c \
+        --linker=../../config/gen_from_riscv_config/cv32a65x/linker/link.ld \
+        --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
+                    -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
+                    -I../tests/custom/env -I../tests/custom/common" \
+        $DV_OPTS \
+        --debug="$DEBUG_FILE" \
+        --spike_params="$SPIKE_PARAMS" \
+        --issrun_opts="$ISSRUN_OPTS" \
+        --isscomp_opts="$ISSCOMP_OPTS" 
+
+make -C ../.. clean
+make clean_all
+
+
 
 # ------------------------------------------------------------------------------
 #4# pmp_cv32a65x_lsu_napot_test.S on cv32a65x
 
-if [ $TEST_NUMBER == "#4#" ] ; then
-    
-    echo "running #4# pmp_cv32a65x_lsu_napot_test.S on cv32a65x with $DV_SIMULATORS"
-    
-    python3 cva6.py \
-            --target cv32a65x \
-            --iss_yaml=cva6.yaml \
-            --iss=$DV_SIMULATORS \
-            --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_lsu_napot_test.S \
-            --uvm_test uvmt_cva6_pmp_test_c \
-            --linker=../tests/custom/common/test.ld \
-            --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
-                        -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
-                        -I../tests/custom/env -I../tests/custom/common" \
-            $DV_OPTS \
-            --debug="$DEBUG_FILE" \
-            --spike_params="$SPIKE_PARAMS" \
-            --issrun_opts="$ISSRUN_OPTS" \
-            --isscomp_opts="$ISSCOMP_OPTS"
+echo "running #4# pmp_cv32a65x_lsu_napot_test.S on cv32a65x with $DV_SIMULATORS"
 
-    make -C ../.. clean
-    make clean_all
-    
-fi
+python3 cva6.py \
+        --target cv32a65x \
+        --iss_yaml=cva6.yaml \
+        --iss=$DV_SIMULATORS \
+        --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_lsu_napot_test.S \
+        --uvm_test uvmt_cva6_pmp_test_c \
+        --linker=../../config/gen_from_riscv_config/cv32a65x/linker/link.ld \
+        --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
+                    -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
+                    -I../tests/custom/env -I../tests/custom/common" \
+        $DV_OPTS \
+        --debug="$DEBUG_FILE" \
+        --spike_params="$SPIKE_PARAMS" \
+        --issrun_opts="$ISSRUN_OPTS" \
+        --isscomp_opts="$ISSCOMP_OPTS"
+
+make -C ../.. clean
+make clean_all
+
+
 
 # ------------------------------------------------------------------------------
 #5# pmp_cv32a65x_decreasing_entries_test.S on cv32a65x
 
-if [ $TEST_NUMBER == "#5#" ] ; then
-    
-    echo "running #5# pmp_cv32a65x_decreasing_entries_test.S on cv32a65x with $DV_SIMULATORS"
-    
-    python3 cva6.py \
-            --target cv32a65x \
-            --iss_yaml=cva6.yaml \
-            --iss=$DV_SIMULATORS \
-            --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_decreasing_entries_test.S \
-            --uvm_test uvmt_cva6_pmp_test_c \
-            --linker=../tests/custom/common/test.ld \
-            --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
-                        -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
-                        -I../tests/custom/env -I../tests/custom/common" \
-            $DV_OPTS \
-            --debug="$DEBUG_FILE" \
-            --spike_params="$SPIKE_PARAMS" \
-            --issrun_opts="$ISSRUN_OPTS" \
-            --isscomp_opts="$ISSCOMP_OPTS" 
+echo "running #5# pmp_cv32a65x_decreasing_entries_test.S on cv32a65x with $DV_SIMULATORS"
 
-    make -C ../.. clean
-    make clean_all
-    
-fi
+python3 cva6.py \
+        --target cv32a65x \
+        --iss_yaml=cva6.yaml \
+        --iss=$DV_SIMULATORS \
+        --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_decreasing_entries_test.S \
+        --uvm_test uvmt_cva6_pmp_test_c \
+        --linker=../../config/gen_from_riscv_config/cv32a65x/linker/link.ld \
+        --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
+                    -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
+                    -I../tests/custom/env -I../tests/custom/common" \
+        $DV_OPTS \
+        --debug="$DEBUG_FILE" \
+        --spike_params="$SPIKE_PARAMS" \
+        --issrun_opts="$ISSRUN_OPTS" \
+        --isscomp_opts="$ISSCOMP_OPTS" 
+
+make -C ../.. clean
+make clean_all
 
 # ------------------------------------------------------------------------------
 #6# pmp_cv32a65x_defined_matches_test.S on cv32a65x
 
-if [ $TEST_NUMBER == "#6#" ] ; then
-    
-    echo "running #6# pmp_cv32a65x_defined_matches_test.S on cv32a65x with $DV_SIMULATORS"
-    
-    python3 cva6.py \
-            --target cv32a65x \
-            --iss_yaml=cva6.yaml \
-            --iss=$DV_SIMULATORS \
-            --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_defined_matches_test.S \
-            --uvm_test uvmt_cva6_pmp_test_c \
-            --linker=../tests/custom/common/test.ld \
-            --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
-                        -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
-                        -I../tests/custom/env -I../tests/custom/common" \
-            $DV_OPTS \
-            --debug="$DEBUG_FILE" \
-            --spike_params="$SPIKE_PARAMS" \
-            --issrun_opts="$ISSRUN_OPTS" \
-            --isscomp_opts="$ISSCOMP_OPTS" 
+echo "running #6# pmp_cv32a65x_defined_matches_test.S on cv32a65x with $DV_SIMULATORS"
 
-    make -C ../.. clean
-    make clean_all
-    
-fi
+python3 cva6.py \
+        --target cv32a65x \
+        --iss_yaml=cva6.yaml \
+        --iss=$DV_SIMULATORS \
+        --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_defined_matches_test.S \
+        --uvm_test uvmt_cva6_pmp_test_c \
+        --linker=../../config/gen_from_riscv_config/cv32a65x/linker/link.ld \
+        --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
+                    -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
+                    -I../tests/custom/env -I../tests/custom/common" \
+        $DV_OPTS \
+        --debug="$DEBUG_FILE" \
+        --spike_params="$SPIKE_PARAMS" \
+        --issrun_opts="$ISSRUN_OPTS" \
+        --isscomp_opts="$ISSCOMP_OPTS" 
+
+make -C ../.. clean
+make clean_all
+
+
 
 # ------------------------------------------------------------------------------
 #7# pmp_cv32a65x_double_entries_test.S on cv32a65x
 
-if [ $TEST_NUMBER == "#7#" ] ; then
-    
-    echo "running #7# pmp_cv32a65x_double_entries_test.S on cv32a65x with $DV_SIMULATORS"
-    
-    python3 cva6.py \
-            --target cv32a65x \
-            --iss_yaml=cva6.yaml \
-            --iss=$DV_SIMULATORS \
-            --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_double_entries_test.S \
-            --uvm_test uvmt_cva6_pmp_test_c \
-            --linker=../tests/custom/common/test.ld \
-            --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
-                        -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
-                        -I../tests/custom/env -I../tests/custom/common" \
-            $DV_OPTS \
-            --debug="$DEBUG_FILE" \
-            --spike_params="$SPIKE_PARAMS" \
-            --issrun_opts="$ISSRUN_OPTS" \
-            --isscomp_opts="$ISSCOMP_OPTS" 
+echo "running #7# pmp_cv32a65x_double_entries_test.S on cv32a65x with $DV_SIMULATORS"
 
-    make -C ../.. clean
-    make clean_all
-    
-fi
+python3 cva6.py \
+        --target cv32a65x \
+        --iss_yaml=cva6.yaml \
+        --iss=$DV_SIMULATORS \
+        --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_double_entries_test.S \
+        --uvm_test uvmt_cva6_pmp_test_c \
+        --linker=../../config/gen_from_riscv_config/cv32a65x/linker/link.ld \
+        --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
+                    -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
+                    -I../tests/custom/env -I../tests/custom/common" \
+        $DV_OPTS \
+        --debug="$DEBUG_FILE" \
+        --spike_params="$SPIKE_PARAMS" \
+        --issrun_opts="$ISSRUN_OPTS" \
+        --isscomp_opts="$ISSCOMP_OPTS" 
+
+make -C ../.. clean
+make clean_all
+
+
 
 # ------------------------------------------------------------------------------
 #8# pmp_cv32a65x_locked_outside_napot_test.S on cv32a65x
 
-if [ $TEST_NUMBER == "#8#" ] ; then
-    
-    echo "running #8# pmp_cv32a65x_locked_outside_napot_test.S on cv32a65x with $DV_SIMULATORS"
-    
-    python3 cva6.py \
-            --target cv32a65x \
-            --iss_yaml=cva6.yaml \
-            --iss=$DV_SIMULATORS \
-            --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_locked_outside_napot_test.S \
-            --uvm_test uvmt_cva6_pmp_test_c \
-            --linker=../tests/custom/common/test.ld \
-            --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
-                        -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
-                        -I../tests/custom/env -I../tests/custom/common" \
-            $DV_OPTS \
-            --debug="$DEBUG_FILE" \
-            --spike_params="$SPIKE_PARAMS" \
-            --issrun_opts="$ISSRUN_OPTS" \
-            --isscomp_opts="$ISSCOMP_OPTS" 
+echo "running #8# pmp_cv32a65x_locked_outside_napot_test.S on cv32a65x with $DV_SIMULATORS"
 
-    make -C ../.. clean
-    make clean_all
-    
-fi
+python3 cva6.py \
+        --target cv32a65x \
+        --iss_yaml=cva6.yaml \
+        --iss=$DV_SIMULATORS \
+        --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_locked_outside_napot_test.S \
+        --uvm_test uvmt_cva6_pmp_test_c \
+        --linker=../../config/gen_from_riscv_config/cv32a65x/linker/link.ld \
+        --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
+                    -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
+                    -I../tests/custom/env -I../tests/custom/common" \
+        $DV_OPTS \
+        --debug="$DEBUG_FILE" \
+        --spike_params="$SPIKE_PARAMS" \
+        --issrun_opts="$ISSRUN_OPTS" \
+        --isscomp_opts="$ISSCOMP_OPTS" 
+
+make -C ../.. clean
+make clean_all
 
 # ------------------------------------------------------------------------------
 #9# pmp_cv32a65x_locked_outside_tor_test.S on cv32a65x
 
-if [ $TEST_NUMBER == "#9#" ] ; then
-    
-    echo "running #9# pmp_cv32a65x_locked_outside_tor_test.S on cv32a65x with $DV_SIMULATORS"
-    
-    python3 cva6.py \
-            --target cv32a65x \
-            --iss_yaml=cva6.yaml \
-            --iss=$DV_SIMULATORS \
-            --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_locked_outside_tor_test.S \
-            --uvm_test uvmt_cva6_pmp_test_c \
-            --linker=../tests/custom/common/test.ld \
-            --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
-                        -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
-                        -I../tests/custom/env -I../tests/custom/common" \
-            $DV_OPTS \
-            --debug="$DEBUG_FILE" \
-            --spike_params="$SPIKE_PARAMS" \
-            --issrun_opts="$ISSRUN_OPTS" \
-            --isscomp_opts="$ISSCOMP_OPTS" 
+echo "running #9# pmp_cv32a65x_locked_outside_tor_test.S on cv32a65x with $DV_SIMULATORS"
 
-    make -C ../.. clean
-    make clean_all
-    
-fi
+python3 cva6.py \
+        --target cv32a65x \
+        --iss_yaml=cva6.yaml \
+        --iss=$DV_SIMULATORS \
+        --asm_tests ../tests/custom/pmp_cv32a65x/pmp_cv32a65x_locked_outside_tor_test.S \
+        --uvm_test uvmt_cva6_pmp_test_c \
+        --linker=../../config/gen_from_riscv_config/cv32a65x/linker/link.ld \
+        --gcc_opts="-static -mcmodel=medany -fvisibility=hidden -nostdlib -nostartfiles \
+                    -g ../tests/custom/common/syscalls.c ../tests/custom/common/crt.S -lgcc \
+                    -I../tests/custom/env -I../tests/custom/common" \
+        $DV_OPTS \
+        --debug="$DEBUG_FILE" \
+        --spike_params="$SPIKE_PARAMS" \
+        --issrun_opts="$ISSRUN_OPTS" \
+        --isscomp_opts="$ISSCOMP_OPTS" 
+
+make -C ../.. clean
+make clean_all
+
+
 
 cd -
