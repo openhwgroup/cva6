3fbea892ea586db938716ddfdeb8a1275790abd8
Riccardo Tedeschi <riccardo.tedeschi6@unibo.it>
2025-02-21 11:29:29 +0100

    cv*_config_pkg.sv: separately parametrize RVF and RVD

--- core/include/cv32a6_imafc_sv32_config_pkg.sv
+++ core/include/cv32a6_imafc_sv32_config_pkg.sv
@@ -13,6 +13,7 @@ package cva6_config_pkg;
   localparam CVA6ConfigXlen = 32;
 
   localparam CVA6ConfigRVF = 1;
+  localparam CVA6ConfigRVD = 0;
   localparam CVA6ConfigF16En = 0;
   localparam CVA6ConfigF16AltEn = 0;
   localparam CVA6ConfigF8En = 0;
@@ -85,7 +86,7 @@ package cva6_config_pkg;
       MemTidWidth: unsigned'(CVA6ConfigMemTidWidth),
       NrLoadBufEntries: unsigned'(CVA6ConfigNrLoadBufEntries),
       RVF: bit'(CVA6ConfigRVF),
-      RVD: bit'(CVA6ConfigRVF),
+      RVD: bit'(CVA6ConfigRVD),
       XF16: bit'(CVA6ConfigF16En),
       XF16ALT: bit'(CVA6ConfigF16AltEn),
       XF8: bit'(CVA6ConfigF8En),
