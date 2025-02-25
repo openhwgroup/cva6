bac134b7b5114a8dd0db4df7a6dcb6d83a4d67d8
Riccardo Tedeschi <58978462+ricted98@users.noreply.github.com>
2025-02-25 07:13:29 +0100

    cv*_config_pkg.sv: separately parametrize RVF and RVD (#2786)
    Use separate parametrization for RVF and RVD support. In the cv32a6_imafc_sv32 configuration RVD is currently enabled, leading to compilation errors.
    
    Co-authored-by: JeanRochCoulon <jean-roch.coulon@thalesgroup.com>

--- core/include/cv32a6_imac_sv32_config_pkg.sv
+++ core/include/cv32a6_imac_sv32_config_pkg.sv
@@ -13,6 +13,7 @@ package cva6_config_pkg;
   localparam CVA6ConfigXlen = 32;
 
   localparam CVA6ConfigRVF = 0;
+  localparam CVA6ConfigRVD = 0;
   localparam CVA6ConfigF16En = 0;
   localparam CVA6ConfigF16AltEn = 0;
   localparam CVA6ConfigF8En = 0;
@@ -84,7 +85,7 @@ package cva6_config_pkg;
       MemTidWidth: unsigned'(CVA6ConfigMemTidWidth),
       NrLoadBufEntries: unsigned'(CVA6ConfigNrLoadBufEntries),
       RVF: bit'(CVA6ConfigRVF),
-      RVD: bit'(CVA6ConfigRVF),
+      RVD: bit'(CVA6ConfigRVD),
       XF16: bit'(CVA6ConfigF16En),
       XF16ALT: bit'(CVA6ConfigF16AltEn),
       XF8: bit'(CVA6ConfigF8En),
