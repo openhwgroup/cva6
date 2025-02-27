e6567e224b086b5e2ee81c630045c81d68e6fae7
Jean-Roch Coulon <jean-roch.coulon@thalesgroup.com>
2025-02-27 17:16:56 +0100

    Improve CC

--- core/csr_regfile.sv
+++ core/csr_regfile.sv
@@ -863,7 +863,7 @@ module csr_regfile
           // -> last bit of pmpaddr must be set 0/1 based on the mode:
           // NA4, NAPOT: 1
           // TOR, OFF:   0
-          if (pmpcfg_q[index].addr_mode[1] == 1'b1)
+          if (CVA6Cfg.PMPNapotEn && pmpcfg_q[index].addr_mode[1] == 1'b1)
             csr_rdata = {pmpaddr_q[index][CVA6Cfg.PLEN-3:1], 1'b1};
           else csr_rdata = {pmpaddr_q[index][CVA6Cfg.PLEN-3:1], 1'b0};
         end
