1c25bede5cd90d39d1aa5cc1f96bd0829a3c2128
André Sintzoff <61976467+ASintzoff@users.noreply.github.com>
2025-03-31 12:05:34 +0200

    csr_regfile.sv: verible
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- core/csr_regfile.sv
+++ core/csr_regfile.sv
@@ -2571,9 +2571,7 @@ module csr_regfile
                               ? 1'b1
                               : 1'b0;
   end else if (CVA6Cfg.RVS) begin
-    assign en_translation_o = (satp_mode_is_sv && priv_lvl_o != riscv::PRIV_LVL_M)
-                             ? 1'b1
-                             : 1'b0;
+    assign en_translation_o   = (satp_mode_is_sv && priv_lvl_o != riscv::PRIV_LVL_M) ? 1'b1 : 1'b0;
     assign en_g_translation_o = 1'b0;
   end else begin
     assign en_translation_o = 1'b0;
