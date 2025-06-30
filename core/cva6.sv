openhwgroup/cva6

commit d04ef660496080ec2f95669b5669a83bbd25b26a

Fix typos (#3029)
I ran spellchecker and fixed some typos. Also I converted CVX_Interface_Coprocessor.rst file that had mixed line endings to LF line endings.

---------

Signed-off-by: Nazar Kazakov <nazar.kazakov@codethink.co.uk>

core/cva6.sv
@@ -179,7 +179,7 @@ module cva6
       logic [CVA6Cfg.ICACHE_SET_ASSOC_WIDTH-1:0] way;  // way to replace
       logic [CVA6Cfg.PLEN-1:0] paddr;  // physical address
       logic nc;  // noncacheable
-      logic [CVA6Cfg.MEM_TID_WIDTH-1:0] tid;  // threadi id (used as transaction id in Ariane)
+      logic [CVA6Cfg.MEM_TID_WIDTH-1:0] tid;  // thread id (used as transaction id in Ariane)
     },
     localparam type icache_rtrn_t = struct packed {
       wt_cache_pkg::icache_in_t rtype;  // see definitions above
@@ -782,7 +782,7 @@ module cva6
   assign wt_valid_ex_id[FPU_WB] = fpu_valid_ex_id;
 
   if (CVA6Cfg.CvxifEn) begin
-    always_comb begin : gen_cvxif_input_assignement
+    always_comb begin : gen_cvxif_input_assignment
       x_compressed_ready = cvxif_resp_i.compressed_ready;
       x_compressed_resp  = cvxif_resp_i.compressed_resp;
       x_issue_ready      = cvxif_resp_i.issue_ready;
@@ -792,7 +792,7 @@ module cva6
       x_result           = cvxif_resp_i.result;
     end
 
-    always_comb begin : gen_cvxif_output_assignement
+    always_comb begin : gen_cvxif_output_assignment
       cvxif_req.compressed_valid = x_compressed_valid;
       cvxif_req.compressed_req   = x_compressed_req;
       cvxif_req.issue_valid      = x_issue_valid;
@@ -1307,13 +1307,13 @@ module cva6
   // -------------------
 
   // Acc dispatcher and store buffer share a dcache request port.
-  // Store buffer always has priority access over acc dipsatcher.
+  // Store buffer always has priority access over acc dispatcher.
   dcache_req_i_t [NumPorts-1:0] dcache_req_to_cache;
   dcache_req_o_t [NumPorts-1:0] dcache_req_from_cache;
 
   // D$ request
-  // Since ZCMT is only enable for embdeed class so MMU should be disable. 
-  // Cache port 0 is being ultilize in implicit read access in ZCMT extension.
+  // Since ZCMT is only enabled for embedded class so MMU should be disabled.
+  // Cache port 0 is being utilized in implicit read access in ZCMT extension.
   if (CVA6Cfg.RVZCMT & ~(CVA6Cfg.MmuPresent)) begin
     assign dcache_req_to_cache[0] = dcache_req_ports_id_cache;
   end else begin
@@ -1325,8 +1325,8 @@ module cva6
                                                                           dcache_req_ports_acc_cache[1];
 
   // D$ response
-  // Since ZCMT is only enable for embdeed class so MMU should be disable.
-  // Cache port 0 is being ultilized in implicit read access in ZCMT extension.
+  // Since ZCMT is only enabled for embedded class so MMU should be disabled.
+  // Cache port 0 is being utilized in implicit read access in ZCMT extension.
   if (CVA6Cfg.RVZCMT & ~(CVA6Cfg.MmuPresent)) begin
     assign dcache_req_ports_cache_id = dcache_req_from_cache[0];
     assign dcache_req_ports_cache_ex[0] = '0;

core/load_store_unit.sv
@@ -158,7 +158,7 @@ module load_store_unit
     // PMP address - CSR_REGFILE
     input logic           [avoid_neg(CVA6Cfg.NrPMPEntries-1):0][CVA6Cfg.PLEN-3:0] pmpaddr_i,
 
-    // RVFI inforamtion - RVFI
+    // RVFI information - RVFI
     output lsu_ctrl_t                    rvfi_lsu_ctrl_o,
     // RVFI information - RVFI
     output logic      [CVA6Cfg.PLEN-1:0] rvfi_mem_paddr_o

commit 4594c5d76986b354aeaf1d573c8b72a60d943efa

Continue Typo fixing (#3034)
rename hartit_t
remove unused signal
To continue typo fixing started by #3029

core/cva6.sv
@@ -300,7 +300,7 @@ module cva6
     parameter type hartid_t = `HARTID_T(CVA6Cfg),
     parameter type x_compressed_req_t = `X_COMPRESSED_REQ_T(CVA6Cfg, hartid_t),
     parameter type x_compressed_resp_t = `X_COMPRESSED_RESP_T(CVA6Cfg),
-    parameter type x_issue_req_t = `X_ISSUE_REQ_T(CVA6Cfg, hartit_t, id_t),
+    parameter type x_issue_req_t = `X_ISSUE_REQ_T(CVA6Cfg, hartid_t, id_t),
     parameter type x_issue_resp_t = `X_ISSUE_RESP_T(CVA6Cfg, writeregflags_t, readregflags_t),
     parameter type x_register_t = `X_REGISTER_T(CVA6Cfg, hartid_t, id_t, readregflags_t),
     parameter type x_commit_t = `X_COMMIT_T(CVA6Cfg, hartid_t, id_t),

core/include/cvxif_types.svh
@@ -16,7 +16,7 @@
     logic accept; /*Is the offloaded compressed instruction (id) accepted by the coprocessor?*/ \
 }
 
-`define X_ISSUE_REQ_T(Cfg, hartit_t, id_t) struct packed { \
+`define X_ISSUE_REQ_T(Cfg, hartid_t, id_t) struct packed { \
     logic [31:0] instr; /*Offloaded instruction*/ \
     hartid_t hartid; /*Identification of the hart offloading the instruction*/ \
     id_t id; /*Identification of the offloaded instruction*/ \

core/load_store_unit.sv
@@ -209,7 +209,7 @@ module load_store_unit
   logic                    st_hs_ld_st_inst;
   logic                    st_hlvx_inst;
   logic translation_req, cva6_translation_req, acc_translation_req;
-  logic translation_valid, cva6_translation_valid, acc_translataion_valid;
+  logic translation_valid, cva6_translation_valid;
   logic [CVA6Cfg.VLEN-1:0] mmu_vaddr, cva6_mmu_vaddr, acc_mmu_vaddr;
   logic [CVA6Cfg.PLEN-1:0] mmu_paddr, cva6_mmu_paddr, acc_mmu_paddr, lsu_paddr;
   logic [31:0] mmu_tinst;

corev_apu/src/ariane.sv
@@ -29,7 +29,7 @@ module ariane import ariane_pkg::*; #(
   localparam type hartid_t            = `HARTID_T(CVA6Cfg),
   localparam type x_compressed_req_t  = `X_COMPRESSED_REQ_T(CVA6Cfg, hartid_t),
   localparam type x_compressed_resp_t = `X_COMPRESSED_RESP_T(CVA6Cfg),
-  localparam type x_issue_req_t       = `X_ISSUE_REQ_T(CVA6Cfg, hartit_t, id_t),
+  localparam type x_issue_req_t       = `X_ISSUE_REQ_T(CVA6Cfg, hartid_t, id_t),
   localparam type x_issue_resp_t      = `X_ISSUE_RESP_T(CVA6Cfg, writeregflags_t, readregflags_t),
   localparam type x_register_t        = `X_REGISTER_T(CVA6Cfg, hartid_t, id_t, readregflags_t),
   localparam type x_commit_t          = `X_COMMIT_T(CVA6Cfg, hartid_t, id_t),

verif/tb/uvmt/cva6_tb_wrapper.sv
@@ -59,7 +59,7 @@ module cva6_tb_wrapper import uvmt_cva6_pkg::*; #(
   localparam type hartid_t            = `HARTID_T(CVA6Cfg),
   localparam type x_compressed_req_t  = `X_COMPRESSED_REQ_T(CVA6Cfg, hartid_t),
   localparam type x_compressed_resp_t = `X_COMPRESSED_RESP_T(CVA6Cfg),
-  localparam type x_issue_req_t       = `X_ISSUE_REQ_T(CVA6Cfg, hartit_t, id_t),
+  localparam type x_issue_req_t       = `X_ISSUE_REQ_T(CVA6Cfg, hartid_t, id_t),
   localparam type x_issue_resp_t      = `X_ISSUE_RESP_T(CVA6Cfg, writeregflags_t, readregflags_t),
   localparam type x_register_t        = `X_REGISTER_T(CVA6Cfg, hartid_t, id_t, readregflags_t),
   localparam type x_commit_t          = `X_COMMIT_T(CVA6Cfg, hartid_t, id_t),