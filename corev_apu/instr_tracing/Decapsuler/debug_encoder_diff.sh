be405f40f0218cfe3ebc5075ecdcc184d6f0cbeb
Maxime COLSON <maxime.colson@thalesgroup.com>
2025-07-29 15:01:06 +0200

    add headers

--- corev_apu/instr_tracing/Decapsuler/debug_encoder_diff.sh
+++ corev_apu/instr_tracing/Decapsuler/debug_encoder_diff.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+# Copyright 2025 Thales DIS design services SAS
+#
+# Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0
+# You may obtain a copy of the License at https://solderpad.org/licenses/
+#
+# Author: Maxime Colson - Thales
 
 exit_error() {
     echo "$1" >&2
--- verif/regress/Instr_tracing_test.sh
+++ verif/regress/Instr_tracing_test.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-#Usage ./compare_iti_trace.sh <executable>
-#Exit 0 if iti traces match, 1 otherwise.
+# Copyright 2025 Thales DIS design services SAS
+#
+# Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0
+# You may obtain a copy of the License at https://solderpad.org/licenses/
+#
+# Author: Maxime Colson - Thales
+
+# Usage ./compare_iti_trace.sh <executable>
+# Exit 0 if iti traces match, 1 otherwise.
 
 exit_error() {
     echo "$1" >&2
