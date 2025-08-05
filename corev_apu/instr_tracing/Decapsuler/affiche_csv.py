021a84d6533eeb10b3b5239499968735b14d0bce
Maxime COLSON <maxime.colson@thalesgroup.com>
2025-08-05 15:58:26 +0200

    remove unused files for submodule integration

--- corev_apu/instr_tracing/Decapsuler/affiche_csv.py
+++ corev_apu/instr_tracing/Decapsuler/affiche_csv.py
@@ -1,3 +1,12 @@
+# Copyright 2025 Thales DIS design services SAS
+#
+# Licensed under the Solderpad Hardware Licence, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# SPDX-License-Identifier: Apache-2.0 WITH SHL-2.0
+# You may obtain a copy of the License at https://solderpad.org/licenses/
+#
+# Author: Maxime Colson - Thales
+# Script used to print a csv file in a more readable way
 import pandas as pd
 import argparse
 import os
