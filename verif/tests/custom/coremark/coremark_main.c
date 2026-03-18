33eff7462a4b69c638d493261a95a8a19c089086
JeanRochCoulon <jean-roch.coulon@thalesgroup.com>
2026-03-18 12:11:29 +0100

    Update coremark_main.c

--- verif/tests/custom/coremark/coremark_main.c
+++ verif/tests/custom/coremark/coremark_main.c
@@ -281,7 +281,7 @@ for (i = 0; i < MULTITHREAD; i++)
             divisor = 1;
         results[0].iterations *= 1 + 10 / divisor;
     }
-    iterate(&results[0]);
+    iterate(1);
     /* perform actual benchmark */
     start_time();
 #if (MULTITHREAD > 1)
