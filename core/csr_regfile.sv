ea5627a5e9aa6bb22d750f22b00c0c0cf3923651
Huacheng <huanghuacheng@e.gzhu.edu.cn>
2026-09-04 15:57:50 +0800

    Update core/csr_regfile.sv
    Co-authored-by: JeanRochCoulon <jean-roch.coulon@thalesgroup.com>

--- core/csr_regfile.sv
+++ core/csr_regfile.sv
@@ -1453,9 +1453,6 @@ module csr_regfile
                 2'b00:   scbie_d = riscv::CBIE_ILLEGAL;
                 2'b01:   scbie_d = riscv::CBIE_FLUSH;
                 2'b11:   scbie_d = riscv::CBIE_INVAL;
-                // WARL: reject the reserved encoding 2'b10 by
-                // reverting to the previously held legal value
-                // instead of silently storing it (#3511).
                 default: scbie_d = scbie_q;
               endcase
               scbcfe_d = csr_wdata[6];
