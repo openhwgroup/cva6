21f92e24f659fae2d96bcbe0760a0350fcf8c37d
Udaya Raj Subedi <075bei047.udaya@pcampus.edu.np>
2026-09-15 15:25:37 +0200

    Changing the linux boot script to accept the bitsream file argument, and path should be absolute

--- corev_apu/fpga/scripts/program_genesys2.tcl
+++ corev_apu/fpga/scripts/program_genesys2.tcl
@@ -8,13 +8,17 @@
 # Original Author: Guillaume CHAUVON (guillaume.chauvon@thalesgroup.com)
 
 # Program Genesys2 FPGA board connected to $HW_SERVER_URL with bitstream ../work-fpga/ariane_xilinx.bit
+set bitstream_path "../work-fpga/ariane_xilinx.bit"
 
+if {[info exists ::env(BITSTREAM)]} {
+    set bitstream_path $::env(BITSTREAM)
+}
 open_hw_manager
 # Connect to an HW_SERVER connected to the FPGA.
 # It is recommended to launch the hw_server before using this script to specify its URL.
 connect_hw_server -url $env(HW_SERVER_URL)
 open_hw_target
 current_hw_device [get_hw_devices xc7k325t_0]
-set_property PROGRAM.FILE {../work-fpga/ariane_xilinx.bit} [get_hw_device xc7k325t_0]
+set_property PROGRAM.FILE $bitstream_path [get_hw_device xc7k325t_0]
 program_hw_devices [get_hw_devices xc7k325t_0]
 refresh_hw_device [lindex [get_hw_devices xc7k325t_0] 0]
